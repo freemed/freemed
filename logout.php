13616d54429d5eb45064dba41434082157fddaf9
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2001-01-25 19:21:37 +0000

    updated to use defines
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@430 6507a214-7cff-0310-bd63-e3842a560bc8

--- logout.php
+++ logout.php
@@ -13,7 +13,7 @@
   SetCookie("default_facility", "0", time()+0);
   SetCookie("current_patient",  "0", time()+0);
 
-  # Header("Location: $complete_url"); // 19990610 - header instead
+  # Header("Location: ".COMPLETE_URL); // 19990610 - header instead
   include ("lib/API.php");
 
   if (strlen($_URL)>0) $__url_part = "?_URL=".urlencode($_URL);
@@ -21,7 +21,7 @@
     <HTML>
     <HEAD>
      <TITLE>"._("Logout of")." ".PACKAGENAME."</TITLE>
-     <META HTTP-EQUIV=\"REFRESH\" CONTENT=\"0;URL=$base_url$__url_part\">
+     <META HTTP-EQUIV=\"REFRESH\" CONTENT=\"0;URL=".BASE_URL."$__url_part\">
     </HEAD>
     <BODY BGCOLOR=#ffffff>
   ";
@@ -31,7 +31,7 @@
       <P>
       <CENTER>
         <B>"._("If your browser does not support the REFRESH tag")."
-        <A HREF=\"$base_url\">"._("click here")."</A>.</B>
+        <A HREF=\"".BASE_URL."\">"._("click here")."</A>.</B>
       </CENTER>
       <P>
   ";
