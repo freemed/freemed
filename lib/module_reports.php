1325321c0c598336028bb9030fb2c42d46463357
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2001-01-31 15:16:39 +0000

    forgot to commit the master report module. oops!
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@435 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/module_reports.php
@@ -0,0 +1,74 @@
+<?php
+ // $Id$
+ // desc: module prototype for reports
+ // lic : GPL, v2
+
+if (!defined("__MODULE_REPORTS_PHP__")) {
+
+define (__MODULE_REPORTS_PHP__, true);
+
+// class freemedReportsModule
+class freemedReportsModule extends freemedModule {
+
+	// override variables
+	var $CATEGORY_NAME = "Reports";
+	var $CATEGORY_VERSION = "0.1";
+
+	// vars to be passed from child modules
+	var $form_vars;
+
+	// user
+	var $this_user;
+
+	// contructor method
+	function freemedEMRModule () {
+		// call parent constructor
+		$this->freemedModule();
+	} // end function freemedEMRModule
+
+	// override check_vars method
+	function check_vars ($nullvar = "") {
+		global $module, $LoginCookie;
+		if (!isset($module)) return false;
+		// FIXME!!: check access to facility
+		//if (!freemed_check_access_for_patient($LoginCookie, $patient)) return false;
+		return true;
+	} // end function check_vars
+
+	// function main
+	// - generic main function
+	function main ($nullvar = "") {
+		global $action, $patient, $LoginCookie;
+
+		if (!isset($this_user))
+			$this->this_user    = new User ($LoginCookie);
+
+		switch ($action) {
+
+			case "display";
+				$this->display();
+				break;
+
+			case "view":
+			default:
+				$this->view();
+				break;
+		} // end switch action
+
+	} // end function main
+
+	// ********************** MODULE SPECIFIC ACTIONS *********************
+
+	// function display
+	// by default, a wrapper for view
+	function display () { $this->view(); }
+
+	// function view
+	// - view stub
+	function view () { }
+
+} // end class freemedReportsModule
+
+} // end if not defined
+
+?>
