17779229dccab18d2668f6e5abb1841a3325bd36
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2001-03-15 23:56:22 +0000

    more error reporting fixes
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@464 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/error_handler.php
+++ lib/error_handler.php
@@ -5,15 +5,18 @@
 
 if (!defined("__ERROR_HANDLER_PHP__")) {
 
-define (__ERROR_HANDLER_PHP__, true);
+define ('__ERROR_HANDLER_PHP__', true);
 
 function freemed_standard_error_handler ($no, $str, $file, $line, $context) {
 	switch ($no) {
+		case E_CORE_ERROR:
+		case E_CORE_WARNING:
 		case E_USER_ERROR:
 		case E_USER_WARNING:
 			$error =
 				"Package : ".PACKAGENAME."\n".
 				"Version : ".VERSION."\n".
+				"phpwebtools Version : ".WEBTOOLS_VERSION."\n".
 				"Installation : ".INSTALLATION."\n".
 				"IP : ".$GLOBALS["SERVER_NAME"]."\n".
 				"Timestamp : ".date("D M d Y h:i a")."\n".
@@ -23,6 +26,19 @@ function freemed_standard_error_handler ($no, $str, $file, $line, $context) {
 
 			// currently, show error
 			echo "<PRE>\n$error\n</PRE>\n";
+			if (BUG_TRACKER) {
+				echo "
+				<P>
+				<CENTER>
+				<FORM ACTION=\"http://www.freemed.org/report_bug.php\" METHOD=POST
+				TARGET=\"bug_report\">
+				<INPUT TYPE=HIDDEN NAME=\"report\" VALUE=\"".
+				prepare($error)."\">
+				<INPUT TYPE=SUBMIT VALUE=\""._("Submit Bug Report")."\">
+				</FORM>
+				</CENTER>
+				";
+			} // end if BUG_TRACKER
 	
 			if (function_exists("freemed_display_box_bottom"))	
 				freemed_display_box_bottom ();
@@ -34,7 +50,7 @@ function freemed_standard_error_handler ($no, $str, $file, $line, $context) {
 } // end function freemed_standard_error_handler
 
 // set as default error handler
-//error_reporting ( E_USER_ERROR );
+error_reporting ( 0 );
 $original_error_handler = set_error_handler("freemed_standard_error_handler");
 
 } // end checking for __ERROR_HANDLER_PHP__
