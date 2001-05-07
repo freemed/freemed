d0a3144416c7442e1d13193804b4eaf8aee7635c
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2001-05-07 14:10:31 +0000

    cleanups, which may be moot, since nothing uses this (yet)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@529 6507a214-7cff-0310-bd63-e3842a560bc8

--- patient_image_handler.php
+++ patient_image_handler.php
@@ -4,33 +4,39 @@
  // lic : GPL
 
  // obligatory initialization garbage
- $page_name  = "patient_image_handler.php";
- $table_name = "patimg";
- include ("lib/freemed.php");
- include ("lib/API.php");
+$page_name = "patient_image_handler.php";
+include ("lib/freemed.php");
+include ("lib/API.php");
+
+// set image table properly
+define ('TABLE_NAME', "patimg");
 
  // authenticate user cookie
- freemed_open_db ($LoginCookie);
+freemed_open_db ($LoginCookie);
 
  // determine what we are getting, and grab it
- if ($id>0) {
-   $result = $sql->query ("SELECT * FROM ".$table_name."
-                         WHERE id='".addslashes($id)."'");
-   $proper = $sql->fetch_array ($result);
- } elseif ($patient>0) {
-   // gets latest picture of patient
-   $result = $sql->query ("SELECT * FROM ".$table_name."
-                         WHERE pipatient='".addslashes($patient)."'
-                         ORDER BY pidate DESC");
-   $proper = $sql->fetch_array ($result);
- } // end if...then determining type
+if ($id > 0) {
+	$result = $sql->query ("SELECT * FROM ".TABLE_NAME." ".
+		"WHERE id='".addslashes($id)."'");
+	if (!$sql->results($result))
+		die("No image found.");
+	$proper = $sql->fetch_array ($result);
+} elseif ($patient > 0) {
+	// gets latest picture of patient
+	$result = $sql->query ("SELECT * FROM ".TABLE_NAME." ".
+		"WHERE pipatient='".addslashes($patient)."' ".
+		"ORDER BY pidate DESC");
+	if (!$sql->results($result))
+		die("No image found.");
+	$proper = $sql->fetch_array ($result);
+} // end if...then determining type
 
- // display header for content type
- Header ("Content-Type: image/jpeg");
+// display header for content type
+Header ("Content-Type: image/jpeg");
 
- // display the actual image data
- echo stripslashes($proper["pidata"]);
+// display the actual image data
+echo stripslashes($proper["pidata"]);
 
- // close database connection
- freemed_close_db ();
+// close database connection
+freemed_close_db ();
 ?>
