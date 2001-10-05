5944222eedcb991d0c35e0b91f7a919bd9d1d752
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2001-10-05 14:46:33 +0000

    echo is *supposed* to use echo/print, not display_buffer ...
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@584 6507a214-7cff-0310-bd63-e3842a560bc8

--- echo.php
+++ echo.php
@@ -3,8 +3,7 @@
  // desc: echos text/plain file from input
  // lic : GPL, v2
 
-Header ("Content-type: ".
-     ( empty($type) ? "text/plain" : $type ) );
-$display_buffer .= $text;
+Header ("Content-type: ".( empty($type) ? "text/plain" : $type ) );
+print $text;
 
 ?>
