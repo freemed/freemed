15abee9d7174d9ee0a10c62270df34422a0df2df
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-04-05 19:07:36 +0000

    html_form::-ification and implemented Cancel
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@734 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/authorizations.emr.module.php
+++ modules/authorizations.emr.module.php
@@ -67,7 +67,10 @@ class authorizationsModule extends freemedEMRModule {
          template_display ();
        }
        $r = freemed::get_link_rec ($id, $this->table_name);
-	   while(list($k,$v)=each($this->variables)) global $$v;
+	foreach ($r AS $k => $v) {
+		global ${$k};
+		${$k} = stripslashes($v);
+	}
        extract ($r);
        break; // end internal modform
      } // end internal action switch
@@ -85,155 +88,67 @@ class authorizationsModule extends freemedEMRModule {
        <INPUT TYPE=HIDDEN NAME=\"authpatient\" VALUE=\"".prepare($patient)."\">
        <INPUT TYPE=HIDDEN NAME=\"module\"  VALUE=\"".prepare($module)."\">
 
-       <TABLE BORDER=0 CELLSPACING=0 CELLPADDING=3
-        VALIGN=MIDDLE ALIGN=CENTER>
-        
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Starting Date")." :
-        </TD>
-        <TD ALIGN=LEFT>
-         ".date_entry("authdtbegin")."
-        </TD>
-       </TR>
+	";
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Ending Date")." :
-        </TD>
-        <TD ALIGN=LEFT>
-         ".date_entry("authdtend")."
-        </TD>
-       </TR>
+	$display_buffer .= html_form::form_table(array(
+		_("Starting Date") =>
+		date_entry("authdtbegin"),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Authorization Number")." :
-        </TD>
-        <TD ALIGN=LEFT>
-         <INPUT TYPE=TEXT NAME=\"authnum\" SIZE=30
-          MAXLENGTH=25 VALUE=\"".prepare($authnum)."\">
-        </TD>
-       </TR>
+		_("Ending Date") =>
+		date_entry("authdtend"),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Authorization Type")." : 
-        </TD>
-        <TD ALIGN=LEFT>
-         <SELECT NAME=\"authtype\">
-          <OPTION VALUE=\"0\" ".
-          ( ($authtype <  1) ? "SELECTED" : "" ).">"._("NONE SELECTED")."
-          <OPTION VALUE=\"1\" ".
-          ( ($authtype == 1) ? "SELECTED" : "" ).">physician
-          <OPTION VALUE=\"2\" ".
-          ( ($authtype == 2) ? "SELECTED" : "" ).">insurance company
-          <OPTION VALUE=\"3\" ".
-          ( ($authtype == 3) ? "SELECTED" : "" )."
-           >certificate of medical neccessity
-          <OPTION VALUE=\"4\" ".
-          ( ($authtype == 4) ? "SELECTED" : "" ).">surgical
-          <OPTION VALUE=\"5\" ".
-          ( ($authtype == 5) ? "SELECTED" : "" ).">worker's compensation
-          <OPTION VALUE=\"6\" ".
-          ( ($authtype == 6) ? "SELECTED" : "" ).">consulation
-         </SELECT>
-        </TD>
-       </TR>
-     ";
+		_("Authorization Number") =>
+		html_form::text_widget("authnum", 25),
 
-     $phys_q="SELECT * FROM physician ORDER BY phylname,phyfname";
-     $phys_r=$sql->query($phys_q);
-     $ins_q="SELECT * FROM insco ORDER BY insconame,inscostate,inscocity";
-     $ins_r=$sql->query($ins_q);
-     
-     $display_buffer .= "
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Authorizing Provider")." :
-        </TD>
-        <TD ALIGN=LEFT>
-     ".
-     freemed_display_selectbox ($phys_r, "#phylname#, #phyfname#", "authprov")
-     ."
-        </TD>
-       </TR>
+		_("Authorization Type") =>
+		html_form::select_widget(
+			"authtype",
+			array(
+				_("NONE SELECTED") => "0",
+				_("physician") => "1",
+				_("insurance company") => "2",
+				_("certificate of medical neccessity") => "3",
+				_("surgical") => "4",
+				_("worker's compensation") => "5",
+				_("consulatation") => "6"
+			)
+		),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Provider Identifier")." :
-        </TD>
-        <TD ALIGN=LEFT>
-         <INPUT TYPE=TEXT NAME=\"authprovid\" SIZE=20 MAXLENGTH=15
-          VALUE=\"".prepare($authprovid)."\">
-         </SELECT>
-        </TD>
-       </TR>
+		_("Authorizing Provider") =>
+		freemed_display_selectbox (
+		$sql->query("SELECT * FROM physician ORDER BY phylname,phyfname"),
+		"#phylname#, #phyfname#", "authprov"),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Authorizing Insurance Company")." :
-        </TD>
-        <TD ALIGN=LEFT>
-     ".
-     freemed_display_selectbox ($ins_r, 
-       "#insconame# (#inscocity#,#inscostate#)", "authinsco")
-     ."
-        </TD>
-       </TR>
+		_("Provider Identifier") =>
+		html_form::text_widget("authprovid", 20, 15),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Number of Visits")." :
-        </TD>
-        <TD ALIGN=LEFT>
-     ".fm_number_select ("authvisits", 0, 100)."
-        </TD>
-       </TR>
+		_("Authorizing Insurance Company") =>
+		freemed_display_selectbox ( 
+		$sql->query("SELECT * FROM insco ORDER BY insconame,inscostate,inscocity"),
+		"#insconame# (#inscocity#,#inscostate#)", "authinsco"),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Used Visits")." :
-        </TD>
-        <TD ALIGN=LEFT>
-     ".fm_number_select ("authvisitsused", 0, 100)."
-        </TD>
-       </TR>
+		_("Number of Visits") =>
+		fm_number_select ("authvisits", 0, 100),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Remaining Visits")." :
-        </TD>
-        <TD ALIGN=LEFT>
-     ".fm_number_select ("authvisitsremain", 0, 100)."
-        </TD>
-       </TR>
+		_("Used Visits") =>
+		fm_number_select ("authvisitsused", 0, 100),
 
-       <TR>
-        <TD ALIGN=RIGHT>
-         "._("Comment")." :
-        </TD>
-        <TD ALIGN=LEFT>
-         <INPUT TYPE=TEXT NAME=\"authcomment\" SIZE=30 MAXLENGTH=100
-          VALUE=\"".prepare($authcomment)."\">
-        </TD>
-       </TR>
+		_("Remaining Visits") =>
+		fm_number_select ("authvisitsremain", 0, 100),
+
+		_("Comment") =>
+		html_form::text_widget("authcomment", 30, 100)
+
+	));
  
-       </TABLE>
-
+	$display_buffer .= "
        <CENTER>
        <INPUT TYPE=SUBMIT VALUE=\"  ".
          ( ($action=="addform") ? _("Add") : _("Modify"))."  \">
        <INPUT TYPE=RESET  VALUE=\" "._("Clear")." \">
+	<INPUT TYPE=\"SUBMIT\" NAME=\"submit\" VALUE=\"Cancel\">
        </CENTER>
        </FORM>
-
-       <CENTER>
-        <A HREF=\"$this->page_name?module=$module&patient=$patient\"
-         >". 
-	  ( ($action=="addform") ? _("Abandon Addition") :
-	    _("Abandon Modification") )."</A>
-       </CENTER>
      ";
 	} // end function authorizationsModule->form()
 
