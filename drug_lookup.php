8ba2e89f44672e9b3fe42fe338fa26f42722c69d
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-07-08 15:18:51 +0000

    Added drug_lookup.php to provide popup pick list for RxList formulary.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@780 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ drug_lookup.php
@@ -0,0 +1,122 @@
+<?php
+	// $Id$
+	// $Author$
+
+$page_name = "drug_lookup.php";
+include_once("lib/freemed.php");
+include_once("lib/class.rxlist.php");
+
+//----- Open database, authenticate, etc
+freemed_open_db ();
+$this_user = new User ();
+
+//----- Check for process
+if ($action==_("Search")) {
+	$display_buffer .= "<BODY onLoad=\"process(); return true;\">\n";
+}
+
+//----- Form header
+$display_buffer .= "<CENTER><FORM NAME=\"lookup\" ACTION=\"".$page_name."\" ".
+	"METHOD=\"POST\">\n";
+
+//----- Master action switch
+switch ($action) {
+	case _("Search"):
+
+	// Perform query
+	unset($list);
+	if (!empty($drug)) {
+		$list = RxList::get_list($drug);
+	}
+
+	// If no results, die right here
+	if (!isset($drug) or (count($list) < 1)) {
+		$display_buffer .= _("No drugs found with that criteria!");
+		break;
+	}
+
+	// Handle immediate passing and closing
+	if (count($list) == 1) {
+		$display_buffer .= "
+		<SCRIPT LANGUAGE=\"Javascript\">
+		function process () {
+			var our_value = '".prepare($drug[0])."'
+
+			// Pass the variable
+			opener.document.".prepare($formname).".".
+			prepare($varname).".value = our_value
+			
+			// Submit name to null
+			opener.document.".prepare($formname).".".prepare($submitname).
+			".value = ''
+			// Submit the form
+			opener.document.forms.".prepare($formname).".submit();
+			
+			// Close the window
+			window.self.close()
+		}
+		</SCRIPT>
+		We should be '".prepare($drug[0])."'.
+		";
+		
+		// Add to pick list
+		$pick_list = $list;
+	} else { // end handling only one result
+		unset($pick_list);
+		$pick_list = $list;
+	}
+
+	$display_buffer .= "
+		<SCRIPT LANGUAGE=\"Javascript\">
+		function my_process () {
+			// Pass the variable
+			opener.document.".prepare($formname).".".prepare($varname).
+			".value = document.lookup.list.value
+
+			// Submit name to null
+			opener.document.".prepare($formname).".".prepare($submitname).
+			".value = ''
+
+			// Submit the form
+			opener.document.".prepare($formname).".submit()
+			
+			// Close the window
+			window.self.close()
+		}
+		</SCRIPT>
+		<DIV ALIGN=\"CENTER\">
+		".html_form::select_widget(
+			"list",	$pick_list
+		)."
+		</DIV>
+	";
+	$display_buffer .= "<INPUT TYPE=\"BUTTON\" NAME=\"select\" ".
+		"VALUE=\"Select\" onClick=\"my_process(); return true;\">\n";
+	break;
+
+	default:
+	$display_buffer .= "
+		<INPUT TYPE=\"HIDDEN\" NAME=\"varname\" VALUE=\"".prepare($varname)."\">
+		<INPUT TYPE=\"HIDDEN\" NAME=\"formname\" VALUE=\"".prepare($formname)."\">
+		<INPUT TYPE=\"HIDDEN\" NAME=\"submitname\" VALUE=\"".prepare($submitname)."\">
+		<CENTER>
+		".html_form::form_table(array(
+			_("Drug") =>
+			html_form::text_widget(
+				"drug", 20
+			)
+		))."
+		</CENTER>
+	";
+	$display_buffer .= "<INPUT TYPE=\"SUBMIT\" NAME=\"action\" ".
+		"VALUE=\""._("Search")."\">\n";
+	break;
+} // end switch
+
+//----- End of form
+$display_buffer .= "</FORM>\n";
+
+//----- KLUDGE: Dump display_buffer
+print $display_buffer;
+
+?>
