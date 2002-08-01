adcb7a8f456a9d8689a35d809475346fe012fe11
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-08-01 13:16:20 +0000

    XHTML compliance, support for stylesheet & proper html in no_template_display
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@800 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/template/default/template.php
+++ lib/template/default/template.php
@@ -13,73 +13,73 @@ if (isset($refresh)) {
 if (!$no_template_display) {
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-<HTML>
-<HEAD>
-	<TITLE><?php print prepare(PACKAGENAME) . " v". VERSION . " - " .
+<html>
+<head>
+	<title><?php print prepare(PACKAGENAME) . " v". VERSION . " - " .
 		( !empty($page_title) ? $page_title." - " : "" ) .
-		prepare(INSTALLATION); ?></TITLE>
-	<META HTTP-EQUIV="Content-Type" 
+		prepare(INSTALLATION); ?></title>
+	<meta HTTP-EQUIV="Content-Type" 
 		CONTENT="text/html; CHARSET=<?php print $__ISO_SET__; ?>">
 <?php
 //----- Handle refresh
 if (isset($refresh)) {
 ?>
-	<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=<?php print $refresh; ?>">
+	<meta HTTP-EQUIV="REFRESH" CONTENT="0;URL=<?php print $refresh; ?>">
 <?php
 } else if (isset($automatic_refresh)) { // handle automatic refreshes
 ?>
-	<META HTTP-EQUIV="REFRESH" CONTENT="<?php print $automatic_refresh; ?>;URL=<?php print basename($REQUEST_URI); ?>">
+	<meta HTTP-EQUIV="REFRESH" CONTENT="<?php print $automatic_refresh; ?>;URL=<?php print basename($REQUEST_URI); ?>">
 <?php
 } // end handle refresh
 ?>
-	<LINK REL="StyleSheet" TYPE="text/css"
-		HREF="lib/template/default/stylesheet.css">
-</HEAD>
+	<link REL="StyleSheet" TYPE="text/css"
+		HREF="lib/template/default/stylesheet.css" />
+</head>
 
-<BODY BGCOLOR="#ffffff" TEXT="#555555"
+<body BGCOLOR="#ffffff" TEXT="#555555"
  ALINK="#000000" VLINK="#000000" LINK="#000000"
  MARGINWIDTH="0" MARGINHEIGHT="0" LEFTMARGIN="0" RIGHTMARGIN="0">
 
 <!-- define main table -->
 
-<TABLE WIDTH="100%" CELLSPACING="0" CELLPADDING="2"
+<table WIDTH="100%" CELLSPACING="0" CELLPADDING="2"
  VALIGN="MIDDLE" ALIGN="CENTER">
 
 <!-- top/header bar -->
 
-<TR>
-	<TD COLSPAN="2" ALIGN="LEFT" VALIGN="TOP">
+<tr>
+	<td COLSPAN="2" ALIGN="LEFT" VALIGN="TOP">
 		<!-- <I>Banner goes here.</I> -->
-		<IMG SRC="lib/template/default/banner.<?php
+		<img SRC="lib/template/default/banner.<?php
 		print IMAGE_TYPE; ?>"
-		 WIDTH="300" HEIGHT="40" ALT="freemed">
-	</TD>
-</TR>
+		 WIDTH="300" HEIGHT="40" ALT="freemed" />
+	</td>
+</tr>
 
-<TR>
-	<TD COLSPAN="1" VALIGN="TOP" ALIGN="RIGHT" WIDTH="20%">
+<tr>
+	<td COLSPAN="1" VALIGN="TOP" ALIGN="RIGHT" WIDTH="20%">
 
 	<!-- menu bar -->
 <?php
 //----- Check to see if we skip displaying this
 if (!$no_menu_bar) {
 ?>
-	<TABLE WIDTH="100%" CELLSPACING="0" CELLPADDING="2"
+	<table WIDTH="100%" CELLSPACING="0" CELLPADDING="2"
 	 CLASS="menubar" VALIGN="TOP" ALIGN="CENTER">
-	<TR><TD VALIGN="TOP" ALIGN="CENTER" CLASS="menubar_title">
-		<B><?php print INSTALLATION; ?></B>
-		<BR>
-		<SMALL><?php print PACKAGENAME." v".VERSION; ?></SMALL>
+	<tr><td VALIGN="TOP" ALIGN="CENTER" CLASS="menubar_title">
+		<b><?php print INSTALLATION; ?></b>
+		<br>
+		<small><?php print PACKAGENAME." v".VERSION; ?></small>
 <?php
 //----- Add page title text if it exists
 if (isset($page_title)) {
 	print "
-		<BR>
+		<br>
 		".prepare($page_title)."
 	";
 } // end isset page_title
 ?>
-	</TD></TR>
+	</td></tr>
 <?php
 //----- Create user object if it doesn't exist and we're logged in
 if (freemed_verify_auth() and !is_object($this_user)) {
@@ -88,57 +88,65 @@ if (freemed_verify_auth() and !is_object($this_user)) {
 
 //----- Generate session information portion of the bar
 if (is_object($this_user)) {
-	print "<TR><TD VALIGN=\"TOP\" ALIGN=\"LEFT\" CLASS=\"menubar_info\">\n";
-	print "<CENTER>\n";
+	print "<tr><td VALIGN=\"TOP\" ALIGN=\"LEFT\" CLASS=\"menubar_info\">\n";
+	print "<center>\n";
 	print _("User")." : ".$this_user->getDescription()."\n";
-	print "</CENTER>\n";
-	print "</TD></TR>\n";
+	print "</center>\n";
+	print "</td></tr>\n";
 } // end checking if this_user exists
 ?>
-	<TR><TD VALIGN="TOP" ALIGN="LEFT" CLASS="menubar_items">
+	<tr><td VALIGN="TOP" ALIGN="LEFT" CLASS="menubar_items">
 
 <?php
 	// Include menubar items
 	include_once("lib/template/default/menubar.php");
 
 ?>
-	</TD></TR></TABLE>
+	</td></tr></table>
 
 <?php } else { /* if there is *no* menu bar */ ?>
 
 &nbsp;
 
 <?php } /* end of checking for no menu bar */ ?>
-	</TD> <TD COLSPAN="1" VALIGN="TOP" ALIGN="CENTER">
+	</td> <td COLSPAN="1" VALIGN="TOP" ALIGN="CENTER">
 
 	<!-- body -->
 
-		<TABLE WIDTH="100%" CELLSPACING="0" CELLPADDING="3"
+		<table WIDTH="100%" CELLSPACING="0" CELLPADDING="3"
 		 CLASS="mainbox">
-		<TR><TD VALIGN="MIDDLE" ALIGN="CENTER">
+		<tr><td VALIGN="MIDDLE" ALIGN="CENTER">
 <?php
 	// Actual content display
 	print $display_buffer;
 ?>
 		&nbsp;
-		</TD></TR></TABLE>
-	</TD>
-</TR>
+		</td></tr></table>
+	</td>
+</tr>
 
 <!-- master table end -->
-</TABLE>
+</table>
 
 <!-- copyright notice -->
-<P>
-<DIV NAME="copyright" ALIGN="CENTER">
+<p/>
+<div NAME="copyright" ALIGN="CENTER">
 	&copy; 1999-<?php print date("Y"); ?> by the FreeMED Software Foundation
-</DIV>
+</div>
 
-</BODY>
-</HTML>
+</body>
+</html>
 <?php
 } else {
 	// Show what we have, if that's what we're doing
+	print "<html>\n".
+		"<head>\n".
+		"<link REL=\"StyleSheet\" TYPE=\"text/css\" ".
+		"HREF=\"lib/template/default/stylesheet.css\"/>\n".
+		"</head>\n".
+		"<body>\n";
 	print $display_buffer;
+	print "</body>\n".
+		"</html>\n";
 } // end checking for "no_template_display"
 ?>
