864b4b04220f6da4a9822d0647386cd07b9dc187
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-08-06 14:13:34 +0000

    rewritten with djvu support
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@817 6507a214-7cff-0310-bd63-e3842a560bc8

--- patient_image_handler.php
+++ patient_image_handler.php
@@ -6,6 +6,7 @@
  // obligatory initialization garbage
 $page_name = "patient_image_handler.php";
 include ("lib/freemed.php");
+define ('RESIZE', 800);
 
  // authenticate user cookie
 freemed_open_db ();
@@ -15,37 +16,49 @@ $patient = freemed::secure_filename($patient);
 $id      = freemed::secure_filename($id     );
 
 //----- Assemble proper file name
-$imagefilename = "img/store/".$patient.".".$id.".jpg";
+$imagefilename = "img/store/".$patient.".".$id.".djvu";
 
-//----- Set RESIZE
-define ('RESIZE', 800);
+//----- Use browser detect to determine what kind of image this should be
+$browser = new browser_detect();
+$type = "djvu";
+if (!freemed::support_djvu($browser)) { $type = "jpeg"; }
 
-//----- Load image or form error string if unloadable
-$image = @ImageCreateFromJpeg($imagefilename);
-if (!$image) {
-	$im = ImageCreate(150, 30);
-	$bgc = ImageColorAllocate($im, 255, 255, 255);
-	$tc  = ImageColorAllocate($im, 0, 0, 0);
-	ImageFilledRectangle($im, 0, 0, 150, 30, $bgc);
-	ImageString($im, 1, 5, 5, "Error loading $id", $tc);
-} else {
-	// Check to see if it's over 600 pixels wide
-	if ((ImageSX($image) > RESIZE) and !$no_resize) {
-		$old = $image;
-		$image = @ImageCreate(RESIZE, ((int) RESIZE * (ImageSY($old) / ImageSX($old) )));
-		ImageCopyResized($image, $old, 0, 0, 0, 0,
-			RESIZE, ((int) RESIZE * (ImageSY($old) / ImageSX($old) )),
-			ImageSX($old), ImageSY($old)
-		);
+switch ($type) {
+	case "jpeg":
+	// Create temporary file name
+	$tempname = tempnam("/tmp", "fmjpeg");
+
+	//----- Load image or form error string if unloadable
+	$image = @ImageCreateFromJpeg($imagefilename);
+	if (!$image) {
+		$im = ImageCreate(150, 30);
+		$bgc = ImageColorAllocate($im, 255, 255, 255);
+		$tc  = ImageColorAllocate($im, 0, 0, 0);
+		ImageFilledRectangle($im, 0, 0, 150, 30, $bgc);
+		ImageString($im, 1, 5, 5, "Error loading $id", $tc);
+	} else {
+		// Check to see if it's over 600 pixels wide
+		if ((ImageSX($image) > RESIZE) and !$no_resize) {
+			$old = $image;
+			$image = @ImageCreate(RESIZE, ((int) RESIZE * (ImageSY($old) / ImageSX($old) )));
+			ImageCopyResized($image, $old, 0, 0, 0, 0,
+				RESIZE, ((int) RESIZE * (ImageSY($old) / ImageSX($old) )),
+				ImageSX($old), ImageSY($old)
+			);
+		}
 	}
-}
 
-// display header for content type
-Header ("Content-Type: image/jpeg");
+	// display header for content type
+	Header ("Content-Type: image/jpeg");
 
-// display the actual image data
-//print readfile($imagefilename);
-ImageJpeg($image, '', 20);
-ImageDestroy($image);
+	// display the actual image data
+	ImageJpeg($image, '', 20);
+	ImageDestroy($image);
+	break; // end jpeg
+
+	case "djvu": default:
+	Header ("Content-Type: image/x.djvu");
+	readfile($imagefilename);
+} // end switch for image type
 
 ?>
