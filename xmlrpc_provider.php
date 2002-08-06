62b0e5aba790bb2db62d347549f003a58b80b693
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-08-06 14:11:11 +0000

    XMLRPC services now working with basic authentication and methods
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@814 6507a214-7cff-0310-bd63-e3842a560bc8

--- xmlrpc_provider.php
+++ xmlrpc_provider.php
@@ -2,6 +2,9 @@
  // $Id$
  // $Author$
  // $Log$
+ // Revision 1.2  2002/08/06 14:11:11  rufustfirefly
+ // XMLRPC services now working with basic authentication and methods
+ //
  // Revision 1.1  2001/12/14 16:35:38  rufustfirefly
  // renamed from soap_* to xmlrpc_* (since it's really XMLRPC, not SOAP)
  //
@@ -10,12 +13,52 @@
  //
 
 //----- Load neccesary headers
+define ('SESSION_DISABLE', true);
 include_once ("lib/freemed.php");
+
+//----- Check for XML-RPC support in PHP build
+if (!is_array($_xmlrpcs_listMethods_sig))
+	die("There is no XML-RPC support in this build of phpwebtools!");
+
+//----- Create XMLRPC_METHODS
+unset ($XMLRPC_METHODS);
+
+//----- Register services
 include_once ("lib/xmlrpc_services.php");
 
-//----- Add services (done in lib/soap_services.php)
+//----- Check for authentication
+$headers = getallheaders(); $authed = false;
+if (ereg('Basic', $headers['Authorization'])) {
+	// Parse headers
+	$tmp = $headers['Authorization'];
+	$tmp = ereg_replace(' ', '', $tmp);
+	$tmp = ereg_replace('Basic', '', $tmp);
+	$auth = base64_decode(trim($tmp));
+	list ($user, $pass) = split(':', $auth);
 
-//----- Run SOAP (XML RPC) server
-$XMLRPC_SERVER = new xmlrpc_server( $XMLRPC_METHODS );
+	// Check for username/password
+	$query = "SELECT username, userpassword FROM user ".
+		"WHERE username='".addslashes($user)."' AND ".
+		"userpassword='".addslashes($pass)."'";
+	$result = $sql->query($query);
+
+	if (@$sql->num_rows($result) == 1) {
+		$authed = true;
+	}
+} else {
+	// Otherwise return fault for no authorization
+	$authed = false;
+}
+
+//----- Return fault for no authorization
+if (!$authed) {
+	$XMLRPC_SERVER = new xmlrpc_server ( $XMLRPC_METHODS, false );
+	$XMLRPC_SERVER->returnFault('auth_error');
+	die();
+}
+
+//----- Run XML-RPC server
+Header("Content-Type: text/xml");
+$XMLRPC_SERVER = new xmlrpc_server ( $XMLRPC_METHODS );
 
 ?>
