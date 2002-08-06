6b0a1e292d383257921e3a1dcbedb768376fd9da
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-08-06 14:12:49 +0000

    rewrote button support with GD/TTF in mind
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@816 6507a214-7cff-0310-bd63-e3842a560bc8

--- generate_button.php
+++ generate_button.php
@@ -2,14 +2,34 @@
  // $Id$
  // desc: script from px.skylar.com to generate custom buttons
  // code: Rasmus Lerdorf <rasmus@lerdorf.on.ca>
+ // (http://www.phpbuilder.com/columns/rasmus19990124.php3)
 
-Header( "Content-type: image/gif" );
-$string = implode( $argv, " " );
-$im = ImageCreateFromGif( "img/button-50x30.gif" );
-$orange = ImageColorAllocate($im, 220, 210, 60);
-$px = ( imagesx($im) - 7.5 * strlen($string) ) / 2;
-ImageString( $im, 3, $px, 8, $string, $orange );
-ImageGif( $im );
+Header( "Content-type: image/png" );
+
+// Set size
+if (!isset($size)) $size = 11;
+$my_size = @ImageTTFBBox($size, 0, "./img/font.ttf", $text);
+$dx = abs($my_size[2] - $my_size[0]);
+$dy = abs($my_size[5] - $my_size[3]);
+$xpad = 5;
+$ypad = 5;
+
+$im = ImageCreate($dx+$xpad, $dy+$ypad);
+$blue  = ImageColorAllocate($im, 0x2c, 0x6D, 0xAF);
+$black = ImageColorAllocate($im, 0x00, 0x00, 0x00);
+$white = ImageColorAllocate($im, 0xFF, 0xFF, 0xFF);
+
+ImageRectangle($im, 0, 0, $dx+$xpad-1, $dy+$ypad-1, $black);
+ImageRectangle($im, 0, 0, $dx+$xpad, $dy+$ypad, $white);
+
+@ImageTTFText($im, $size, 0, (int)($xpad/2)+1, $dy+(int)($ypad/2), $black,
+	"./img/font.ttf", $text);
+
+@ImageTTFText($im, $size, 0, (int)($xpad/2), $dy+(int)($ypad/2)-1, $white,
+	"./img/font.ttf", $text);
+
+// Show and destroy image
+ImagePng( $im );
 ImageDestroy( $im );
 
 ?>
