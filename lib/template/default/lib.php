afd703d1612650eabc8332e01187834fa0510228
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-08-06 14:20:32 +0000

    moved template-specific images into lib/template/default/img/
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@818 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/template/default/lib.php
+++ lib/template/default/lib.php
@@ -9,22 +9,22 @@ class template {
 
 	function summary_delete_link($class, $url) {
 		$buffer .= "<A HREF=\"".$url."\" ".
-			"><IMG SRC=\"lib/template/default/summary_delete.png\"
+			"><IMG SRC=\"lib/template/default/img/summary_delete.png\"
 			BORDER=\"0\" ALT=\""._("Delete")."\"></A>";
 		return $buffer;
 	} // end function summary_delete_link
 
 	function summary_modify_link($class, $url) {
 		$buffer .= "<A HREF=\"".$url."\" ".
-			"><IMG SRC=\"lib/template/default/summary_modify.png\"
+			"><IMG SRC=\"lib/template/default/img/summary_modify.png\"
 			BORDER=\"0\" ALT=\""._("Modify")."\"></A>";
 		return $buffer;
 	} // end function summary_modify_link
 
 	function summary_view_link($class, $url, $newwindow = false) {
 		$buffer .= "<A HREF=\"".$url."\" ".(
-			$newwindow ? "TARGET=\"_view\"" : ""
-			)."><IMG SRC=\"lib/template/default/summary_view.png\"
+			$newwindow ? "TARGET=\"".$class."_view\"" : ""
+			)."><IMG SRC=\"lib/template/default/img/summary_view.png\"
 			BORDER=\"0\" ALT=\""._("View")."\"></A>";
 		return $buffer;
 	} // end function summary_modify_link
