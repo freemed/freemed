e8e881e77fb1d55f4ec4e662c429132adbdf1a13
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-11-27 20:39:57 +0000

    New Login class, for method FreeMED.Login.check
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@866 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/xmlrpc/class.Login.php
@@ -0,0 +1,51 @@
+<?php
+ // $Id$
+ // $Author$
+ // $Log$
+ // Revision 1.1  2002/11/27 20:39:57  rufustfirefly
+ // New Login class, for method FreeMED.Login.check
+ //
+
+class Login {
+
+	function check () {
+		global $sql;
+		//----- Check for authentication
+		$headers = getallheaders(); $authed = false;
+		if (ereg('Basic', $headers['Authorization'])) {
+			// Parse headers
+			$tmp = $headers['Authorization'];
+			$tmp = ereg_replace(' ', '', $tmp);
+			$tmp = ereg_replace('Basic', '', $tmp);
+			$auth = base64_decode(trim($tmp));
+			list ($user, $pass) = split(':', $auth);
+		
+			// Check for username/password
+			$query = "SELECT username, userpassword, userrealphy, id FROM user ".
+				"WHERE username='".addslashes($user)."' AND ".
+				"userpassword='".addslashes($pass)."'";
+			$result = $sql->query($query);
+	
+			if (@$sql->num_rows($result) == 1) {
+				$authed = true;
+				$r = $sql->fetch_array($result);
+				$GLOBALS['__freemed']['basic_auth_id'] = $r['id'];
+				$GLOBALS['__freemed']['basic_auth_phy'] = $r['userrealphy'];
+			} else {
+				// Clear basic auth id
+				$authed = false;
+				$GLOBALS['__freemed']['basic_auth_id'] = 0;
+				$GLOBALS['__freemed']['basic_auth_phy'] = 0;
+			}
+		} else {
+			// Otherwise return fault for no authorization
+			$authed = false;
+			$GLOBALS['__freemed']['basic_auth_id'] = 0;
+			$GLOBALS['__freemed']['basic_auth_phy'] = 0;
+		}
+		return $authed;
+	} // function check
+
+} // end class Login
+
+?>
