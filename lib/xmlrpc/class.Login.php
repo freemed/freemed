f9dd5dd677899d0bc65d7c3c8599fe4a2f5bb657
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2002-12-29 14:23:46 +0000

    Fixed scope problem in FreeMED.Login.* namespace.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@891 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/xmlrpc/class.Login.php
+++ lib/xmlrpc/class.Login.php
@@ -2,6 +2,9 @@
  // $Id$
  // $Author$
  // $Log$
+ // Revision 1.2  2002/12/29 14:23:46  rufustfirefly
+ // Fixed scope problem in FreeMED.Login.* namespace.
+ //
  // Revision 1.1  2002/11/27 20:39:57  rufustfirefly
  // New Login class, for method FreeMED.Login.check
  //
@@ -9,7 +12,6 @@
 class Login {
 
 	function check () {
-		global $sql;
 		//----- Check for authentication
 		$headers = getallheaders(); $authed = false;
 		if (ereg('Basic', $headers['Authorization'])) {
@@ -24,11 +26,11 @@ class Login {
 			$query = "SELECT username, userpassword, userrealphy, id FROM user ".
 				"WHERE username='".addslashes($user)."' AND ".
 				"userpassword='".addslashes($pass)."'";
-			$result = $sql->query($query);
+			$result = $GLOBALS['sql']->query($query);
 	
-			if (@$sql->num_rows($result) == 1) {
+			if (@$GLOBALS['sql']->num_rows($result) == 1) {
 				$authed = true;
-				$r = $sql->fetch_array($result);
+				$r = $GLOBALS['sql']->fetch_array($result);
 				$GLOBALS['__freemed']['basic_auth_id'] = $r['id'];
 				$GLOBALS['__freemed']['basic_auth_phy'] = $r['userrealphy'];
 			} else {
