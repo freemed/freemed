4b309e260dc5e0e3ebed4a08bc98b6400b1cc8c1
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2003-03-28 04:58:43 +0000

    Simple conversion script to new image store format.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@994 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ scripts/image2md5scheme.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+# $Id$
+# $Author$
+#
+# Stupid little script to move images from old repository type to new
+# MD5-type repository. This only generates the shell commands necessary
+# to do this, and doesn't actually perform the moves.
+
+use Digest::MD5  qw(md5 md5_hex md5_base64);
+
+my $pid, $record, $type, $original;
+
+while ($original = shift) {
+
+	# Split into components
+	( $pid, $record, $type ) = split(/\./, $original, 3);
+
+	# Determine md5 hash
+	my $md5hash = md5_hex($pid);
+
+	# Create output filename
+	my $dir = substr($md5hash, 0, 2) . '/' .
+		substr($md5hash, 2, 2). '/' .
+		substr($md5hash, 4, 2). '/' .
+		substr($md5hash, 6, 2);
+	
+	my $filename = substr($md5hash, 8, 24). '.' .
+		$record . '.' .
+		$type;
+
+	print "mkdir -p $dir\n";
+	print "mv -v $original $dir/$filename\n";
+}
