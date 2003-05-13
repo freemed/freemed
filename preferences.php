977f46a91a320d512f128fcbe34fb8f39d470309
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2003-05-13 20:18:52 +0000

    MD5 changes for password changing.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1099 6507a214-7cff-0310-bd63-e3842a560bc8

--- preferences.php
+++ preferences.php
@@ -6,7 +6,7 @@ $page_name = basename($_SERVER["PHP_SELF"]);
 include_once ("lib/freemed.php");
 
 //----- Login/authenticate
-freemed_open_db ();
+freemed::connect ();
 
 //----- Set page title
 $page_title = __("Preferences");
@@ -33,7 +33,10 @@ switch ($action) {
 		html_form::password_widget("current_password"),
 
 		__("New Password") =>
-		html_form::password_widget("new_password")
+		html_form::password_widget("new_password1"),
+
+		__("New Password Again") =>
+		html_form::password_widget("new_password2"),
 	))."
 	</div>
 	<div align=\"center\">
@@ -45,18 +48,50 @@ switch ($action) {
 	break; // end passwordform
 
 	case "password":
-	// Check for password being correct
-	if (!$this_user->checkPassword($current_password) or
-			($this_user->user_number == 1)) {
-		// Do nothing
-	} else {
-		// Change password
-		$result = $sql->query($sql->update_query(
-			"user",
-			array("userpassword" => $new_password),
-			array("id" => $this_user->user_number)
-		));
-	}
+
+//	$display_buffer = $_SESSION['authdata']['user'];
+	$uid=$_SESSION['authdata']['user'];
+
+	$result = $sql->query ("SELECT * FROM user ".
+		"WHERE id = '".addslashes($uid)."'");
+	
+		$r = $sql->fetch_array ($result);
+
+			$current_md5=md5($current_password);
+
+			$db_pass=$r['userpassword'];	
+
+
+		
+
+		if ($current_md5 == $r['userpassword']) 	
+		{
+			if($new_password1==$new_password2)
+			{
+				$display_buffer=$display_buffer."This worked";
+				$this_user = CreateObject('FreeMED.User');
+				$this_user->setPassword($new_password1,$uid);
+				
+// change the password
+			}
+			else
+			{
+				// print error passwords dont match
+				$display_buffer= "The new passwords you entered dont match<BR>";
+			}
+			
+
+		}
+		else
+		{
+//------HIPAA Logging
+$user_to_log=$_SESSION['authdata']['user'];
+if((LOGLEVEL<1)||LOG_HIPAA){syslog(LOG_INFO,"preferences.php|user $user_to_log attempt to change password failed.");}	
+
+
+			$display_buffer = "The password you entered was incorrect";
+		}
+
 	$refresh = $page_name;
 	template_display();
 	break;
