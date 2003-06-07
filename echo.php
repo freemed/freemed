d6d84ebd96a8230af4583d6d526e7095767cca17
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2003-06-07 15:45:52 +0000

    Fixed under register globals.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1132 6507a214-7cff-0310-bd63-e3842a560bc8

--- echo.php
+++ echo.php
@@ -3,7 +3,7 @@
  // desc: echos text/plain file from input
  // lic : GPL, v2
 
-Header ("Content-type: ".( empty($type) ? "text/plain" : $type ) );
-print $text;
+Header ("Content-type: ".( empty($_POST['type']) ? "text/plain" : $_POST['type'] ) );
+print $_POST['text'];
 
 ?>
