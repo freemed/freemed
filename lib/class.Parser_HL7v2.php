cd1a89d4db44623d6d71bfc751e9d2d1ebd5c19d
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2003-11-19 06:49:53 +0000

    Reworked HL7 v2.3 parsing engine. Added global macros for messages in parser. (Jeff)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1233 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/class.Parser_HL7v2.php
+++ lib/class.Parser_HL7v2.php
@@ -3,6 +3,43 @@
 	// $Author$
 	// HL7 Parser
 
+// TODO --- NaturalDocs all of the macros here (using Macro)
+
+// Field position definition list
+define ('HL7v2_PID_ID', 3);
+define ('HL7v2_PID_NAME', 5);
+	define ('HL7v2_PID_NAME_LAST', 0);
+	define ('HL7v2_PID_NAME_FIRST', 1);
+	define ('HL7v2_PID_NAME_MIDDLE', 2);
+define ('HL7v2_PID_DATEOFBIRTH', 7);
+define ('HL7v2_PID_GENDER', 8);
+define ('HL7v2_PID_SOCIALSECURITY', 19);
+define ('HL7v2_PID_PHONE_HOME', 13);
+define ('HL7v2_PID_PHONE_WORK', 14);
+define ('HL7v2_PID_ADDRESS', 11);
+	define ('HL7v2_PID_ADDRESS_LINE1', 0);
+	define ('HL7v2_PID_ADDRESS_LINE2', 1);
+	define ('HL7v2_PID_ADDRESS_CITY', 2);
+	define ('HL7v2_PID_ADDRESS_STATE', 3);
+	define ('HL7v2_PID_ADDRESS_ZIPCODE', 4);
+
+// PD1
+define ('HL7v2_PD1_PROVIDER', 4);
+	define ('HL7v2_PD1_PROVIDER_ID', 0);
+	define ('HL7v2_PD1_PROVIDER_LASTNAME', 1);
+	define ('HL7v2_PD1_PROVIDER_FIRSTNAME', 2);
+
+// SCH
+define ('HL7v2_SCH_DURATION', 9);
+define ('HL7v2_SCH_UNIT', 10);
+define ('HL7v2_SCH_NOTE', 8);
+	define ('HL7v2_SCH_NOTE_SHORT', 0);
+	define ('HL7v2_SCH_NOTE_LONG', 1);
+
+// Class: _FreeMED.Parser_HL7v2
+//
+//	HL7 v2.3 compatible generic parser
+//
 class Parser_HL7v2 {
 
 	var $field_separator;
@@ -13,6 +50,15 @@ class Parser_HL7v2 {
 	var $MSH;
 	var $EVN;
 
+	// Method: Parser_HL7v2 constructor
+	//
+	// Parameters:
+	//
+	//	$message - Text of HL7 v2.3 message
+	//
+	//	$options - (optional) Additional options to be passed
+	//	to the parser. This is an associative array.
+	//
 	function Parser_HL7v2 ( $message, $_options = NULL ) {
 		// Assume separator is a pipe
 		$this->field_separator = '|';
@@ -54,6 +100,7 @@ class Parser_HL7v2 {
 				break;
 
 				default:
+				// Parse all other segments
 				$this->__default_segment_parser($segment);
 				$this->map[$count]['type'] = $type;
 				$this->map[$count]['position'] = count($this->message[$type]);
@@ -62,17 +109,28 @@ class Parser_HL7v2 {
 		}
 
 		// Depending on message type, handle differently
+		/*
 		switch ($this->message_type) {
 			default:
 			print ('Message type '.$this->message_type.' is '.
 				'currently unhandled'."<br/>\n");
 			break;
 		} // end switch
+		*/
 	} // end constructor Parser_HL7v2
-
+	
+	// Method: Parser_HL7v2->Handle
+	//
+	//	Method to be called by other parts of the program to execute
+	//	the action associated with the provided message type.
+	//
+	// Returns:
+	//
+	//	Output of the specified handler.
+	//
 	function Handle() {
 		// Set to handle current method
-		$type = str_replace('^', '_', $this->MSH['message_type']);
+		list ($top_level, $type) = explode ('^', $this->MSH['message_type']);
 
 		// Check for an appropriate handler
 		$handler = CreateObject('_FreeMED.Handler_HL7v2_'.$type, $this);
@@ -103,12 +161,19 @@ class Parser_HL7v2 {
 		}
 
 		list (
+			$__garbage,
 			$this->EVN['event_type_code'],
 			$this->EVN['event_datetime'],
 			$this->EVN['event_planned'],
 			$this->EVN['event_reason'],
 			$this->EVN['operator_id']
 		) = $composites;
+
+		if ($this->options['debug']) {
+			print "EVN Segment verbose:<hr/><pre>\n";
+			print_r ( $this->EVN );
+			print "</pre><hr/>\n";
+		}
 	} // end method _EVN
 
 	function _MSH ($segment) {
@@ -145,6 +210,12 @@ class Parser_HL7v2 {
 
 		// TODO: Extract $this->MSH['encoding_characters'] and use
 		// it instead of assuming the defaults.
+
+		if ($this->options['debug']) {
+			print "MSH Segment verbose:<hr/><pre>\n";
+			print_r ( $this->MSH );
+			print "</pre><hr/>\n";
+		}
 	} // end method _MSH
 
 	//----- Truly internal functions
