f0a6e06934b97189dd0efe34893e778f986a35a0
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2003-11-19 06:49:00 +0000

    Basic HL7 v2.3 A04 and A08 message handlers added. (Volker)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1232 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/class.Handler_HL7v2_A04.php
@@ -0,0 +1,45 @@
+<?php
+	// $Id$
+	// $Author$
+
+LoadObjectDependency('_FreeMED.Handler_HL7v2');
+
+class Handler_HL7v2_A04 extends Handler_HL7v2 {
+
+	function Handle () {
+		if (!is_object($this->parser)) {
+			die('Handler_HL7v2_A04: parser object not present');
+		}
+
+		// Loop for each PID
+		foreach ($this->parser->message['PID'] AS $k => $v) {
+			$query = $GLOBALS['sql']->insert_query(
+				'patient',
+				array (
+					'ptlname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_LAST],
+					'ptfname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_FIRST],
+					'ptmname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_MIDDLE],
+					'ptdob' => $this->_ConvertDate($v[HL7v2_PID_DATEOFBIRTH]),
+					'ptsex' => strtolower($v[HL7v2_PID_GENDER]),
+					'ptaddr1' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_LINE1],
+					'ptaddr2' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_LINE2],
+					'ptcity' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_CITY],
+					'ptstate' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_STATE],
+					'ptzip' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_ZIPCODE],
+					//'ptcountry' => $this->parser->PID['patient']['country'],
+					'pthphone' => $this->_StripToNumeric($v[HL7v2_PID_PHONE_HOME]),
+					'ptwphone' => $this->_StripToNumeric($v[HL7v2_PID_PHONE_WORK]),
+					'ptssn' => $this->_StripToNumeric($v[HL7v2_PID_SOCIALSECURITY]),
+					'ptid' => $v[HL7v2_PID_ID]
+				)
+			);
+			//print "query = $query<br>\n";
+			$result = $GLOBALS['sql']->query($query);
+		}
+	} // end method Handle
+
+	function Type () { return 'ADT'; }
+
+} // end class Handler_HL7v2_A04
+
+?>
