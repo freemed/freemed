98a8754fcc0ecf16c9438da8e5a34456796b99c8
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2003-12-29 20:03:42 +0000

    Initial commit of the FreeMED / FreeB v1 billing transport. (Jeff)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1259 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/class.FreeB_v1.php
@@ -0,0 +1,233 @@
+<?php
+	// $Id$
+	// $Author$
+
+// Class: FreeB_v1
+//
+//	Provides an interface to the FreeB billing system, so that all
+//	calls can be abstracted to this class. All of the XML-RPC
+//	server side calls are handled by FreeMED's internal XML-RPC
+//	server, and they are located in lib/xmlrpc/FreeB/
+//
+class FreeB_v1 {
+
+	// Method: FreeB_v1 constructor
+	function FreeB_v1 ( ) {
+		// Try to get configuration from FreeMED configuration
+		// database
+		$this->server = freemed::config_value('freeb_server');
+		$this->path = freemed::config_value('freeb_path');
+		$this->port = freemed::config_value('freeb_port');
+		$this->protocol = freemed::config_value('freeb_protocol');
+
+		// If this isn't set, default to localhost parameters
+		if (empty($this->server)) {
+			$this->server = 'localhost';
+			$this->port = '18081';
+			$this->path = '/RPC2';
+			$this->protocol = 'http';
+		}
+
+		// Create proper constructor
+		$this->xmlrpc = CreateObject (
+			'PHP.xmlrpc_client',
+			$this->path,
+			$this->server,
+			$this->port
+		);
+	} // end constructor FreeB_v1
+
+	// Method: FormatList
+	//
+	//	Retrieve the list of formats that the connected FreeB
+	//	server has available.
+	//
+	// Returns:
+	//
+	//	Array containing the available formats.
+	//
+	function FormatList ( ) {
+		return $this->_call ( 'FreeB.Format.list', NULL );
+	} // end method FormatList
+
+	// Method: ProcessBill
+	//
+	//	Executes FreeB.Bill.process method on the server with the
+	//	specified Bill Key. Since FreeMED does not store a Bill
+	//	Key database, this is just serialized data which is
+	//	passed around.
+	//
+	// Parameters:
+	//
+	//	$key - Serialized data regarding procedures and patients
+	//	to be billed.
+	//
+	//	$format - Format for the current queue of bills
+	//
+	//	$target - Target for the current queue of bills
+	//
+	// Returns:
+	//
+	//	Result code.
+	//
+	function ProcessBill ( $key, $format, $target ) {
+		return $this->_call (
+			'FreeB.Bill.process', 
+			array (
+				CreateObject('PHP.xmlrpcval', $key, 'string'),
+				CreateObject('PHP.xmlrpcval', $format, 'string'),
+				CreateObject('PHP.xmlrpcval', $target, 'string'),
+				CreateObject('PHP.xmlrpcval', date("Ymd"), 'string')
+			)
+		);
+	} // end method ProcessBill
+
+	// Method: ProtocolVersion
+	//
+	//	Determines the version of the FreeB protocol that is being
+	//	used by the specified server.
+	//
+	// Returns:
+	//
+	//	FreeB protocol version used by the specified server. If this
+	//	is >= 2, FreeB_v1 will not properly handle this.
+	//
+	function ProtocolVersion ( ) {
+		return $this->_call ( 'FreeB.Protocol.version', NULL );
+	} // end method ProtocolVersion
+
+	// Method: TargetList
+	//
+	//	Retrieve the list of targets that the connected FreeB
+	//	server has available.
+	//
+	// Returns:
+	//
+	//	Array containing the available targets.
+	//
+	function TargetList ( ) {
+		return $this->_call ( 'FreeB.Target.list', NULL );
+	} // end method TargetList
+
+	// Method: _autoserialize
+	//
+	//	Automagically determines what kind of resource this is
+	//	supposed to be and creates a PHP.xmlrpcval object to
+	//	wrap it in.
+	//
+	// Parameters:
+	//
+	//	$mixed - Original object, any type
+	//
+	// Returns:
+	//
+	//	PHP.xmlrpcval object
+	//
+	function _autoserialize ( $mixed ) {
+		// Handle already serialized
+		if (is_object($mixed) and method_exists($mixed, 'serialize')) {
+			return $mixed;
+		}
+
+		if (is_array($mixed)) {
+			// If dealing with an array, recursively figure out
+			@reset($mixed);
+			while(list($k, $v) = @each($mixed)) {
+				$ele[$k] = $this->_autoserialize($v);
+			}
+			// Determine if struct or array as we return it
+			return CreateObject (
+				'PHP.xmlrpcval',
+				$ele,
+				$this->_is_struct($ele) ? 'struct' : 'array'
+			);
+		} else {
+			// Otherwise, use PHP auto-typing
+			// FIXME: Needs to figure out if it is binary to make base64 type things work correctly
+			$type = (is_integer($mixed) ? 'int' : gettype($mixed));
+			return CreateObject (
+				'PHP.xmlrpcval',
+				$mixed,
+				$type
+			);
+		}
+	} // end method _autoserialize
+
+	// Method: _call
+	//
+	//	Call FreeB server with the specified parameters. This
+	//	should only be used by internal FreeB_v1 methods to
+	//	complete abstraction.
+	//
+	// Parameters:
+	//
+	//	$method - Method on the FreeB server to call
+	//
+	//	$parameters - (optional) Array of parameters to call
+	//	$method with. Defaults to NULL.
+	//
+	//	$debug - (optional) Whether debug code should be shown.
+	//	Defaults to false.
+	//
+	// Returns:
+	//
+	//	Reply to call as PHP variable.
+	//
+	function _call ( $method, $parameters = NULL, $debug = false ) {
+		// Form proper message object
+		if ($parameters != NULL) {
+			$message = CreateObject(
+				'PHP.xmlrpcmsg',
+				$method,
+				( is_array($parameters) ? $parameters : array($parameters) )
+			);
+		} else {
+			$message = CreateObject(
+				'PHP.xmlrpcmsg',
+				$method
+			);
+		}
+
+		// If we're debugging, we set the debug flag
+		$this->xmlrpc->setDebug ( $debug );
+
+		// Dispatch message to server
+		$response = $this->xmlrpc->send (
+			$message,
+			0,
+			$this->protocol
+		);
+
+		// Deserialize response
+		return $response->deserialize();
+	} // end method _call
+
+	// Method: _is_struct
+	//
+	//	Determines if in an array is a structure (associative array)
+	//	or a regular array.
+	//
+	// Parameters:
+	//
+	//	$var - Variable to be typed
+	//
+	// Returns:
+	//
+	//	Boolean, true if $var is an associative array, false if it
+	//	is not.
+	//
+	function _is_struct ( $var ) {
+		// Catch non-array instance
+		if (!is_array($var)) return false;
+
+		// If there are non-numeric keys, it is a structure, otherwise
+		// default to false.
+		foreach ($var AS $k => $v) {
+			if (!is_integer($k)) return true;
+		}
+		return false;
+	} // end method _is_struct
+
+} // end class FreeB_v1
+
+?>
---
+++ modules/freeb.billing.module.php
@@ -0,0 +1,435 @@
+<?php
+	// $Id$
+	// $Author$
+
+LoadObjectDependency('FreeMED.BillingModule');
+
+class FreeBBillingTransport extends BillingModule {
+
+	var $MODULE_NAME = "FreeB Billing System";
+	var $MODULE_VERSION = "0.1";
+	var $MODULE_AUTHOR = "jeff@ourexchange.net";
+
+	var $MODULE_FILE = __FILE__;
+
+	function FreeBBillingTransport ( ) {
+		// GettextXML:
+		//	__("FreeB Billing System")
+
+		// Set configuration variables (username/password)
+		$this->_SetMetaInformation('global_config_vars', array (
+			'freeb_server', 'freeb_port', 'freeb_protocol'
+		));
+		$this->_SetMetaInformation('global_config', array (
+			__("FreeB Server Hostname") =>
+			'html_form::text_widget("freeb_server", 20, 50)',
+			__("FreeB Server Port") =>
+			'html_form::password_widget("freeb_port", 6)',
+			__("Transport Protocol") =>
+			'html_form::select_widget("freeb_protocol", array ( '.
+				'"http" => "http", '.
+				'"https" => "https" ))'
+		));
+
+		// Set appropriate associations
+		$this->_SetHandler('BillingFunctions', 'transport');
+		$this->_SetMetaInformation('BillingFunctionName', __("FreeB Billing System"));
+
+		// Call parent constructor
+		$this->BillingModule();
+	} // end constructor FreeBBillingTransport
+
+	function transport () {
+		global $display_buffer, $billing_action;
+		
+		// Handle "Cancel" submit action by returning to previous
+		// menu through refresh.
+		if ($__submit == __("Cancel")) {
+			global $refresh;
+			$refresh = "billing_functions.php";
+			return '';
+		}
+
+		// Act as a switchboard for everything
+		switch ($billing_action) {
+			case 'billing':
+				return $this->billing();
+				break;
+
+			// By default, we show the form with information
+			// regarding what is going on.
+			default:
+				return $this->menu();
+				break;
+		}
+	} // end method transport
+
+	
+	function billing ( ) {
+		global $type;
+		extract ($_REQUEST);
+
+		print "__submit = ".$__submit."<br/>\n";
+		// Handle super submit
+		if ($__submit == __("Submit Claims")) {
+			print "__submit is Submit Claims<br/>\n";
+			return $this->process();
+		}
+
+		// Handle single claim submit
+		if ($__submit_alone) {
+			print "__submit_alone = ".$__submit_alone."<br/>\n";
+			return $this->process($__submit_alone);
+		}
+
+		// Get FreeB formats and information
+		$freeb = CreateObject('FreeMED.FreeB_v1');
+		$freeb_formats = $freeb->FormatList();
+		$freeb_targets = $freeb->TargetList();
+
+		// Start master form
+		$buffer .= "
+		<form action=\"billing_functions.php\" method=\"post\">
+		<input type=\"hidden\" name=\"type\" value=\"".get_class($this)."\" />
+		<input type=\"hidden\" name=\"billing_action\" value=\"".$_REQUEST['billing_action']."\" />
+		<input type=\"hidden\" name=\"action\" value=\"type\" />
+		<input type=\"hidden\" name=\"been_here\" value=\"1\" />
+		";
+
+		// Determine list of procedures / patients to bill
+		$patients_to_bill = $this->PatientsToBill();
+
+		// If there are none, decide where to go from here
+		if (count($patients_to_bill) < 1) {
+			$buffer .= "
+			<div align=\"center\">
+			".__("There are no patients to bill.")."
+			</div>
+			";
+			return false;
+		}	
+
+		// Show billing table header
+		$buffer .= "
+		<table width=\"100%\" cellspacing=\"1\" cellpadding=\"0\" border=\"1\">
+		<thead><tr>
+			<td class=\"DataHead\" width=\"15%\">".__("Patient")."</td>
+			<td class=\"DataHead\" width=\"35%\">&nbsp;</td>
+			<td class=\"DataHead\" width=\"15%\" align=\"center\">
+				<input type=\"checkbox\" onClick=\"alert('FIXME!'); return true;\"/>".__("Select None")."</td>
+			<td class=\"DataHead\" width=\"15%\" align=\"center\">
+				<input type=\"checkbox\" onClick=\"alert('FIXME!'); return true;\"/>".__("Select All")."</td>
+			<td class=\"DataHead\" width=\"20%\" align=\"center\">
+				<input type=\"submit\" name=\"__submit\" value=\"".__("Submit Claims")."\" /></td>
+		</td></thead>
+		</table>
+		";
+
+		// Loop for patients
+		foreach ($patients_to_bill AS $__garbage => $p) {
+			// Default on first (!been_here) to check patient...
+			if (!$been_here) {
+				$patients[$p] = 1; // not sure what this does
+				$bill[$p] = 1;
+			}
+
+			// Hide patient for billing and pass value depending
+			// on whether or not it is passed already
+			$buffer .= "<input type=\"hidden\" ".
+				"name=\"patients[".$p."]\" value=\"".
+				( isset($patients[$p]) ? $patients[$p] : '1' ).
+				"\" />\n";
+
+			// Get patient object
+			unset($this_patient);
+			$this_patient = CreateObject('FreeMED.Patient', $p);
+
+			// Determine number of claims from global table, set
+			// by PatientsToBill (clever, but hacky)
+			$number_of_claims = $this->number_of_claims[$p];
+
+			// Create master table
+			$buffer .= "
+			<table width=\"100%\" cellspacing=\"1\" cellpadding=\"0\" border=\"1\">
+			<tbody><tr>
+			<td class=\"Data\" width=\"15%\"><a href=\"manage.php?id=".urlencode($p)."\"
+				>".prepare($this_patient->fullName())."</a></td>
+			<td class=\"Data\" width=\"20%\"><input type=\"checkbox\" name=\"expand[".$p."]\" ".( $expand[$p]==1 ? 'checked="checked"' : '' )." value=\"1\" onChange=\"this.form.submit(); return true;\" />".__("expand individual claims")."</td>
+			<td class=\"Data\" width=\"10%\">".$number_of_claims." ".__("claims")."</td>
+			<td class=\"Data\" width=\"25%\">&nbsp;</td>
+			<td class=\"Data\" width=\"20%\"><input type=\"checkbox\" name=\"bill[".$p."]\" ".( $bill[$p]==1 ? 'checked="checked"' : '' )." value=\"1\" />".__("Submit Patient?")."</td>
+			</tr>
+			</tbody></table>
+			";
+
+			// Get claims for this patient
+			$these_claims = $this->ProceduresToBill($p);
+			//print "these claims = "; print_r($these_claims); print "<br/>\n";
+
+			// Decide if we're expanding
+			if ($expand[$p]) {
+				// If expanding, display all procedures
+				$buffer .= "
+				<table width=\"100%\" cellspacing=\"0\" cellpadding=\"1\" border=\"1\">
+				<tbody>
+				";
+
+				// Loop through claims
+				foreach ($these_claims AS $__garbage => $c) {
+					// Get claim record information from procrec
+					$_record = freemed::get_link_rec($c, 'procrec');
+
+					// Display
+					//print "processing claim $c<br/>\n";
+					$buffer .= "
+					<tr>
+					<td class=\"Data\" width=\"15%\" align=\"right\">
+					<input type=\"checkbox\" name=\"claim[".$c."]\" ".( $claim[$c]==1 ? 'checked="checked"' : '' )." value=\"1\" />".__("Submit Claim?")."
+					</td>
+					<td class=\"Data\" width=\"25%\">
+					<a href=\"module_loader.php?module=proceduremodule&patient=".urlencode($p)."&action=modform&id=".urlencode($c)."\"
+					>".__("DOS:")." ".$_record['procdt']."</a>
+					</td>
+
+					<td class=\"Data\" width=\"25%\">
+					<select name=\"format[".$c."]\">
+					";
+					// Format selection widget from FreeB server
+					foreach ($freeb_formats AS $k => $v) {
+						$buffer .= "<option value=\"".prepare($v)."\" ".( $format[$c]==$v ? 'selected' : '' ).">".prepare($v)."</option>\n";
+					}
+
+					$buffer .= "
+					</select>
+					<select name=\"target[".$c."]\">
+					";
+					// Target selection widget from FreeB server
+					foreach ($freeb_targets AS $k => $v) {
+						$buffer .= "<option value=\"".prepare($v)."\" ".( $target[$c]==$v ? 'selected' : '' ).">".prepare($v)."</option>\n";
+					}
+
+					$buffer .= "
+					</select>
+					</td>
+					<td class=\"Data\" width=\"35%\">
+					<select name=\"coverage[".$c."]\">
+					";
+
+					// Depending on coverage from procedure,
+					// we put together a list of everything
+					$coverages = $this->PatientCoverages($c, &$curcov);
+					//print "coverages ($c) = "; print_r($coverages); print "<br/>\n";
+					foreach ($coverages AS $__garbage => $cov) {
+						$this_coverage = CreateObject('FreeMED.Coverage', $cov);
+						$buffer .= "<option value=\"".prepare($cov)."\" ".( $cov == $curcov ? 'selected' : '' ).">".$this_coverage->covinsco->insconame." ".
+						( $this_coverage->covtype==1 ? __("(Primary)") : '' ).
+						( $this_coverage->covtype==2 ? __("(Secondary)") : '' ).
+						"</option>\n";
+					}
+					
+					$buffer .= "
+					</select>
+					<input type=\"checkbox\" name=\"__submit_alone\" value=\"".prepare($c)."\" onChange=\"this.form.submit(); return true;\" />".__("Submit Alone")."
+					</td>
+					</tr>
+					";
+				}
+	
+				// End display table			
+				$buffer .= "
+				</tbody></table>
+				";
+			} else {
+				// ... otherwise set hidden attributes for them,
+				// depending on what was passed
+				foreach ($these_claims AS $__garbage => $c) {
+					// On first load, assume that all claims
+					// are being submitted...
+					if (!$been_here) {
+						$claim[$c] = 1;
+						// HACK! Should not always be X12
+						$format[$c] = 'x12';
+						$target[$c] = 'txt';
+						$__dummy = $this->PatientCoverages($c, &$curcov);
+						$coverage[$c] = $curcov;
+					}
+
+					// ... and notebook-style hide their
+					// data so it can be expanded upon
+					$buffer .= "
+					<input type=\"hidden\" name=\"claim[".$c."]\" value=\"".prepare($claim[$c])."\" />
+					<input type=\"hidden\" name=\"format[".$c."]\" value=\"".prepare($format[$c])."\" />
+					<input type=\"hidden\" name=\"target[".$c."]\" value=\"".prepare($target[$c])."\" />
+					<input type=\"hidden\" name=\"coverage[".$c."]\" value=\"".prepare($coverage[$c])."\" />
+					";
+				}
+			}
+		} // end looping for patients
+
+		// End master form
+		$buffer .= "
+		</form>
+		";
+
+		return $buffer;
+	} // end method billing
+
+	function menu () {
+		$buffer .= "
+		<table border=\"0\">
+
+		<tr>
+			<td class=\"DataHead\" width=\"15%\">".__("Function")."</td>
+			<td class=\"DataHead\">".__("Description")."</td>
+		</tr>
+
+		<tr>
+		<td>
+		<a href=\"billing_functions.php?type=".get_class($this).
+			"&billing_action=billing&action=type\"
+			>".__("Perform Billing")."</a></td>
+		<td>
+		Perform FreeB billing runs.
+		</td>
+		</tr>
+		</table>
+		";
+		return $buffer;
+	} // end method menu
+
+	function process ( $single = NULL ) {
+		$buffer .= __("Submitting data to FreeB server")." ... <br/>\n"; 		
+		// Create new FreeB instance
+		$freeb = CreateObject('FreeMED.FreeB_v1');
+
+		// Create Bill Key
+		extract($_REQUEST);
+
+		// Make sure we don't have someone passing us bupkus
+		unset($billkey);
+
+		// If we're doing a single claim, handle seperately
+		if ($single != NULL) {
+			// Create single array of stuff
+			$billkey['procedures'][] = $single;
+			$result = $freeb->ProcessBill($key, $format[$single], $target[$single]);
+		}
+
+		// Add patients to be billed
+		foreach ($bill AS $patient => $to_bill) {
+			if ($to_bill == 1) {
+				// Add this key to the patients
+				$billkey['patients'][] = $patient;	
+			}
+		}
+
+		// For now, we ignore the type of bill, and focus more
+		// on the procedures and coverages
+		foreach ($claim AS $claim => $to_bill) {
+			if ($to_bill == 1) {
+				// Add this key to the procedures
+				$billkey['procedures'][] = $claim;
+			}
+		}
+
+		// Lastly, we serialize the bill key
+		$key = serialize($billkey);
+
+		// ... and send it to FreeB, to see what we get for a result
+		$result = $freeb->ProcessBill($key, 'hcfa', 'pdf');
+
+		// DEBUG: Show what we got
+		print "DEBUG: "; print_r($result); print "<br/>\n";
+	} // end method process
+
+
+	//--------------------------------------------------------------------
+	// Helper and other internal functions
+	//--------------------------------------------------------------------
+
+	function PatientCoverages ( $claim, $default_coverage ) {
+		// Get all patient coverages associated with a procedure
+		$rec = freemed::get_link_rec($claim, 'procrec');
+		for ($i=1; $i<=4; $i++) {
+			if ($rec['proccov'.$i] > 0) {
+				$coverage[] = $rec['proccov'.$i];
+			}
+		}
+
+		// Get the "default" coverage, and set $default_coverage to it
+		$default_coverage = $rec['proccurcovid'];
+
+		// Return coverages
+		return $coverage;
+	} // end method PatientCoverages
+
+	function PatientsToBill ( ) {
+		// This is a *huge* select hack so that the query returns
+		// patients in a quasi-alphabetical order. Any better
+		// suggestions are welcomed.  - Jeff
+		$query = "SELECT DISTINCT(a.procpatient) AS patient, ".
+			// Hack to get global claim counts
+			"COUNT(a.id) AS claims ".
+			"FROM procrec AS a, patient AS b ".
+			// Needs to have a balance of over 0
+			"WHERE a.procbalcurrent > '0' AND ".
+			// Make sure the association is made
+			"a.procpatient = b.id AND ".
+			// Needs to be billable (at all) -- examine this one!
+			//"a.procbillable = '0' AND ".
+			// (Not sure) Needs not to be billed already
+			"a.procbilled = '0' ".
+			// Last little bit of the global claim count hack
+			"GROUP BY a.procpatient ".
+			// Here's the ordering magic, using patient table:
+			"ORDER BY b.ptlname,b.ptfname,b.ptmname,b.ptdob";
+
+		$result = $GLOBALS['sql']->query($query);
+		
+		// Simple hack to make sure that no results return no answers
+		if (!$GLOBALS['sql']->results($result)) {
+			return array ();
+		}
+
+		$return = array ();
+		while ($r = $GLOBALS['sql']->fetch_array($result)) {
+			$return[] = $r['patient'];
+			$this->number_of_claims[$r['patient']] = $r['claims'];
+		}
+
+		return $return;
+	} // end method PatientsToBill
+
+	function ProceduresToBill ( $patient ) {
+		$query = "SELECT id FROM procrec ".
+			// Needs to have a balance of over 0
+			"WHERE procbalcurrent > '0' AND ".
+			// And patient must jive
+			"procpatient = '".addslashes($patient)."' AND ".
+			// Needs to be billable (at all) -- examine this one!
+			//"procbillable = '0' AND ".
+			// (Not sure) Needs not to be billed already
+			"procbilled = '0' ".
+			// Order by date of procedure
+			"ORDER BY procdt";
+
+		$result = $GLOBALS['sql']->query($query);
+		
+		// Simple hack to make sure that no results return no answers
+		if (!$GLOBALS['sql']->results($result)) {
+			return array ();
+		}
+
+		$return = array ();
+		while ($r = $GLOBALS['sql']->fetch_array($result)) {
+			$return[] = $r['id'];
+		}
+
+		return $return;
+	} // end method ProceduresToBill
+
+} // end class FreeBBillingTransport
+
+register_module('FreeBBillingTransport');
+
+?>
