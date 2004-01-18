50488c850f691842e39036a2bc09058c2c411042
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2004-01-18 03:48:17 +0000

    Initial commit of new Hylafax automatic fax import system. (for Volker)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1289 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ data/config/.htaccess
@@ -0,0 +1,3 @@
+<Files *>
+deny from all
+</Files>
---
+++ scripts/fax_import/import_all_hylafax.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# $Id$
+# $Author$
+
+# This is the auto-import bash script, which loops through all entries in
+# the Hylafax receive queue and forces them to be imported into FreeMED,
+# first by being OCR'd and converted to DJVU, then by being transported
+# via XML-RPC (using the XMLRPC::Lite / SOAP::Lite toolkit) into FreeMED.
+
+( \
+	cd /usr/share/freemed/scripts/fax_import/; \
+	for f in /var/spool/hylafax/recvq/*.tif; do \
+		mv $f .; \
+		./import_fax.pl `basename $f`; \
+	done \
+)
+	
