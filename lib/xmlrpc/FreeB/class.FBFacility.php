1afd2cb2cb46247c6b3e6d8b78dca6aa46e6d03e
synseer <synseer@6507a214-7cff-0310-bd63-e3842a560bc8>
2004-01-27 04:56:23 +0000

    Added NaturalDocs documentation.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1303 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/xmlrpc/FreeB/class.FBFacility.php
+++ lib/xmlrpc/FreeB/class.FBFacility.php
@@ -1,40 +1,93 @@
 <?php
 	// $Id$
 	// $Author$
+/*      Class: FBFacility
 
+	FreeMEDs implementation of FreeB::FBFacility.
+
+*/
 class FBFacility {
 
+/*      Function: Name
+
+        Returns:
+
+        psrname record from the facility table
+
+*/ 
 	function Name ( $key ) {
 		$f = freemed::get_link_rec($key, 'facility');
 		return $f['psrname'];
 	} // end method Name
 
+/*      Function: StreetAddress
+
+        Returns:
+
+        psraddr1 record from the facility table
+
+*/ 
 	function StreetAddress ( $key ) {
 		$f = freemed::get_link_rec($key, 'facility');
 		return $f['psraddr1'];
 	} // end method StreetAddress
 
+/*      Function: City
+
+        Returns:
+
+        psrcity record from the facility table
+
+*/ 
 	function City ( $key ) {
 		$f = freemed::get_link_rec($key, 'facility');
 		return $f['psrcity'];
 	} // end method City
 
+/*      Function: State
+
+        Returns:
+
+        psrstate record from the facility table.
+
+*/ 
 	function State ( $key ) {
 		$f = freemed::get_link_rec($key, 'facility');
 		return $f['psrstate'];
 	} // end method State
 
+/*      Function: Zipcode
+
+        Returns:
+
+        psrzip record from the facility table.
+
+*/ 
 	function Zipcode ( $key ) {
 		$f = freemed::get_link_rec($key, 'facility');
 		return $f['psrzip'];
 	} // end method Zipcode
 
+/*      Function: PhoneCountry
+
+        Returns:
+
+        BROKEN
+
+*/ 
 	function PhoneCountry ( $key ) {
 		//$f = freemed::get_link_field($key, 'facility', 'psrphone');
 		// TODO: Broken behavior
 		return '';
 	} // end method PhoneCountry
 
+/*      Function: PhoneArea
+
+        Returns:
+
+        returns the first three digits of the psrphone field from the facility table
+
+*/ 
 	function PhoneArea ( $key ) {
 		// TODO: Need to handle i18n with areas
 		$f = freemed::get_link_field($key, 'facility', 'psrphone');
@@ -42,6 +95,13 @@ class FBFacility {
 		return substr($f, 0, 3);
 	} // end method PhoneArea
 
+/*      Function: PhoneNumber
+
+        Returns:
+
+        the third to the seventh digit in the psrphone field from the facility table. 
+
+*/ 
 	function PhoneNumber ( $key ) {
 		// TODO: Need to handle i18n with rest of number
 		$f = freemed::get_link_field($key, 'facility', 'psrphone');
@@ -49,6 +109,13 @@ class FBFacility {
 		return substr($f, 3, 7);
 	} // end method PhoneNumber
 
+/*      Function: PhoneExtension
+
+        Returns:
+
+        returns the last four digits of the psrphone field from the facility table.
+
+*/ 
 	function PhoneExtension ( $key ) {
 		// TODO: Need to handle i18n with rest of number
 		$f = freemed::get_link_field($key, 'facility', 'psrphone');
@@ -56,6 +123,13 @@ class FBFacility {
 		return substr($f, 10, 4);
 	} // end method PhoneExtension
 
+/*      Function: HCFACode
+
+        Returns:
+
+          uses the psrpos field from the facility table to get the posname field from the pos table. 
+
+*/ 
 	function HCFACode ( $fac, $pay ) {
 		// TODO: This is probably wrong
 		$code = freemed::get_link_field($fac, 'facility', 'psrpos');
@@ -63,6 +137,13 @@ class FBFacility {
 		return freemed::get_link_field($code, 'pos', 'posname');
 	} // end method HCFACode
 
+/*      Function: X12Code
+
+        Returns:
+
+        uses the psrpos field from the facility table to get the posname field from the pos table. 
+
+*/ 
 	function X12Code ( $fac, $pay ) {
 		// TODO: This is probably wrong
 		$code = freemed::get_link_field($fac, 'facility', 'psrpos');
