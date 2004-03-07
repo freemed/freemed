9d3e7417e65da14df8e846369faa10c500222033
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2004-03-07 14:55:23 +0000

    A04 now acts like A08 and checks for existing records first. (Volker)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1456 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/class.Handler_HL7v2_A04.php
+++ lib/class.Handler_HL7v2_A04.php
@@ -9,36 +9,55 @@ class Handler_HL7v2_A04 extends Handler_HL7v2 {
 	function Handle () {
 		syslog(LOG_INFO, 'HL7 parser| Entered A04 parser');
 		if (!is_object($this->parser)) {
-
 			die('Handler_HL7v2_A04: parser object not present');
 		}
 
 		// Loop for each PID
 		foreach ($this->parser->message['PID'] AS $k => $v) {
-			$query = $GLOBALS['sql']->insert_query(
-				'patient',
-				array (
-					'ptlname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_LAST],
-					'ptfname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_FIRST],
-					'ptmname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_MIDDLE],
-					'ptdob' => $this->_ConvertDate($v[HL7v2_PID_DATEOFBIRTH]),
-					'ptsex' => strtolower($v[HL7v2_PID_GENDER]),
-					'ptaddr1' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_LINE1],
-					'ptaddr2' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_LINE2],
-					'ptcity' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_CITY],
-					'ptstate' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_STATE],
-					'ptzip' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_ZIPCODE],
-					//'ptcountry' => $this->parser->PID['patient']['country'],
-					'pthphone' => $this->_StripToNumeric($v[HL7v2_PID_PHONE_HOME]),
-					'ptwphone' => $this->_StripToNumeric($v[HL7v2_PID_PHONE_WORK]),
-					'ptssn' => $this->_StripToNumeric($v[HL7v2_PID_SOCIALSECURITY]),
-					'ptarchive' => '0',
-					'ptid' => $v[HL7v2_PID_ID]
-				)
+			// Understanding that this breaks true HL7 compliance,
+			// this will check to see if the PID exists in the
+			// system and generate the equivalent of an A04 call
+			// if it does not.
+			$exist_query = $GLOBALS['sql']->query(
+				"SELECT * FROM patient WHERE ptid='".
+				addslashes($v[HL7v2_PID_ID])."'"
 			);
+			// Create array of variables
+			$variables = array (
+				'ptlname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_LAST],
+				'ptfname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_FIRST],
+				'ptmname' => $v[HL7v2_PID_NAME][HL7v2_PID_NAME_MIDDLE],
+				'ptdob' => $this->_ConvertDate($v[HL7v2_PID_DATEOFBIRTH]),
+				'ptsex' => strtolower($v[HL7v2_PID_GENDER]),
+				'ptaddr1' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_LINE1],
+				'ptaddr2' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_LINE2],
+				'ptcity' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_CITY],
+				'ptstate' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_STATE],
+				'ptzip' => $v[HL7v2_PID_ADDRESS][HL7v2_PID_ADDRESS_ZIPCODE],
+				//'ptcountry' => $this->parser->PID['patient']['country'],
+				'pthphone' => $this->_StripToNumeric($v[HL7v2_PID_PHONE_HOME]),
+				'ptwphone' => $this->_StripToNumeric($v[HL7v2_PID_PHONE_WORK]),
+				'ptssn' => $this->_StripToNumeric($v[HL7v2_PID_SOCIALSECURITY]),
+				'ptarchive' => '0',
+				'ptid' => $v[HL7v2_PID_ID]
+			);
+			if ($GLOBALS['sql']->results($exist_query)) {
+				// Exists, proceed with A08
+				$query = $GLOBALS['sql']->update_query(
+					'patient',
+					$variables,
+					array ('ptid' => $v[HL7v2_PID_ID])
+				);
+			} else {
+				// Otherwise use A04 type query to add patient
+				$query = $GLOBALS['sql']->insert_query(
+					'patient',
+					$variables
+				);
+			}
 			//print "query = $query<br>\n";
 			syslog(LOG_INFO, 'HL7 parser| query = '.$query);
-			return ( $result = $GLOBALS['sql']->query($query) );
+			$result = $GLOBALS['sql']->query($query);
 		}
 	} // end method Handle
 
