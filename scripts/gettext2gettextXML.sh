e27a2fd3e9433d0dc8ef36c843a69fe43fc5da1a
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2004-04-17 17:30:36 +0000

    Remove triplicate slashes patch (very old).
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1531 6507a214-7cff-0310-bd63-e3842a560bc8

--- scripts/gettext2gettextXML.sh
+++ scripts/gettext2gettextXML.sh
@@ -6,5 +6,7 @@
 for i in *.php; do
 	echo -n "Processing $i ... "
 	perl -pi -e "s/\_\(\"/__(\"/g;" "$i"
+	# Remove triplicate slashes, in the event of an overzealous run
+	perl -pi -e "s/\_\_\_\(\"/__(\"/g;" "$i"
 	echo "[done]"
 done
