abb5ccae8d315af5f766be057d59f2f7227e85b5
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2004-04-19 17:37:59 +0000

    Fixed detection of faxed documents. (Volker)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1532 6507a214-7cff-0310-bd63-e3842a560bc8

--- debian/cron.d
+++ debian/cron.d
@@ -3,4 +3,4 @@
 
 # ----- FreeMED Fax import ----------------------------------------------------
 # Examine files every 5 minutes
-05,10,15,20,25,30,35,40,45,50,55 *	* * *	root	test -f /var/spool/hylafax/recvq/*.tif || test -f /var/spool/hylafax/recvq/*.tiff && /usr/share/freemed/scripts/fax_import/import_all_hylafax.sh
+05,10,15,20,25,30,35,40,45,50,55 *	* * *	root	test -f "`ls -1 /var/spool/hylafax/recvq/*.tif* 2>&1 | head -1`" && /var/www/freemedtest/freemed/scripts/fax_import/import_all_hylafax.sh
