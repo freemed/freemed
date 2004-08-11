e2b6855665c8643b43ba4ed5647a79b67f7edad9
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2004-08-11 21:42:59 +0000

    Initial commit of annotations module into stable branch.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1648 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ modules/annotations.emr.module.php
@@ -0,0 +1,127 @@
+<?php
+	// $Id$
+	// $Author$
+	// lic : GPL, v2
+
+LoadObjectDependency('FreeMED.EMRModule');
+
+class Annotations extends EMRModule {
+
+	var $MODULE_NAME = "Annotations";
+	var $MODULE_AUTHOR = "jeff b (jeff@ourexchange.net)";
+	var $MODULE_VERSION = "0.1";
+	var $MODULE_FILE = __FILE__;
+
+	var $PACKAGE_MINIMUM_VERSION = '0.7.0';
+
+	var $record_name = "Annotations";
+	var $table_name = 'annotations';
+	var $patient_field = 'apatient';
+
+	function Annotations () {
+		$this->table_definition = array (
+			'atimestamp' => SQL__TIMESTAMP(14),
+			'apatient' => SQL__INT_UNSIGNED(0),
+			'amodule' => SQL__VARCHAR(150),
+			'atable' => SQL__VARCHAR(150),
+			'aid' => SQL__INT_UNSIGNED(0),
+			'auser' => SQL__INT_UNSIGNED(0),
+			'annotation' => SQL__TEXT,
+			'id' => SQL__SERIAL
+		);
+
+		global $this_user;
+		if (!is_object($this_user)) { $this_user = CreateObject('_FreeMED.User'); }
+		$this->variables = array (
+			'atimestamp' => SQL__NOW,
+			'apatient' => $_REQUEST['patient'],
+			'amodule',
+			'atable',
+			'aid',
+			'auser' => $_SESSION['authdata']['user'],
+			'annotation'
+		);
+
+		$this->summary_vars = array (
+			__("Date/Time") => 'atimestamp',
+			__("Table") => 'atable'
+		);
+		$this->summary_options = SUMMARY_VIEW | SUMMARY_DELETE;
+
+		$this->form_hidden = array (
+			'amodule',
+			'atable',
+			'aid'
+		);
+
+		// call parent constructor
+		$this->EMRModule();
+	} // end constructor Annotations
+
+	function addform () {
+		// Display parent form
+		$this->form();
+
+		// Display all past annotations, if present
+		global $display_buffer;
+		$q = "SELECT ".
+			"DATE_FORMAT(atimestamp, '%d %M %Y %H:%i') AS ts,".
+				"annotation,auser ".
+			"FROM ".$this->table_name." ".
+			"WHERE aid='".addslashes($_REQUEST['aid'])."' AND ".
+			"atable='".addslashes($_REQUEST['atable'])."' AND ".
+			"apatient='".addslashes($_REQUEST['patient'])."' ".
+			"ORDER BY atimestamp DESC";
+		$a = $GLOBALS['sql']->query($q);
+		while ($r = $GLOBALS['sql']->fetch_array($a)) {
+			$display_buffer .=
+			"<div class=\"thinbox_noscroll\" width=\"60%\">".
+			"<i>".$r['ts']."</i> ".__("by")." <b>".freemed::get_link_field($r['auser'], 'user', 'username')."</b>".
+			"<br/>\n".
+			prepare($r['annotation'])."</div>\n";
+		}
+	}
+
+	function form_table ( ) {
+		return array (
+			__("Annotation") =>
+			html_form::text_area('annotation')
+		);
+	} // end method form_table
+
+	function view ( ) {
+		global $sql; global $display_buffer; global $patient;
+		$display_buffer .= freemed_display_itemlist (
+			$sql->query("SELECT DATE(atimestamp) AS ts, ".
+				"amodule, auser FROM ".$this->table_name." ".
+				"WHERE apatient='".addslashes($patient)."' ".
+				freemed::itemlist_conditions(false)." ".
+				"ORDER BY atimestamp DESC"),
+			$this->page_name,
+			array(
+				__("Date") => 'ts',
+				__("Module") => 'amodule',
+				__("User") => 'auser'
+			),
+			array('', __("Not specified")), //blanks
+			array(
+				"",
+				"",
+				"user" => "username"
+			)
+		);
+	} // end method view
+
+	// Update
+	function _update ( ) {
+		global $sql;
+		$version = freemed::module_version($this->MODULE_NAME);
+		//if (!version_check($version, '0.2')) {
+		//}	
+	} // end method _update
+
+} // end class Annotations
+
+register_module ("Annotations");
+
+?>
