09f2e1572473c480a35817ac359b8c5733ca4c8c
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2005-02-27 02:06:07 +0000

    Finally got around to fixing and updating room equipment table to work.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1888 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/room_equipment.db.module.php
+++ modules/room_equipment.db.module.php
@@ -4,77 +4,44 @@
 
 LoadObjectDependency('_FreeMED.MaintenanceModule');
 
-class RoomEquipmentMaintenance extends MaintenanceModule {
+class RoomEquipment extends MaintenanceModule {
 
-	var $MODULE_NAME    = "Room Equipment Maintenance";
+	var $MODULE_NAME    = "Room Equipment";
 	var $MODULE_AUTHOR  = "jeff b (jeff@ourexchange.net)";
 	var $MODULE_VERSION = "0.1";
 	var $MODULE_FILE    = __FILE__;
 
-	var $PACKAGE_MINIMUM_VERSION = '0.6.0';
+	var $PACKAGE_MINIMUM_VERSION = '0.7.0';
 
 	var $table_name     = "roomequip";
 	var $record_name    = "Room Equipment";
 	var $order_field    = "reqname,reqdescrip";
-	var $variables      = array (
-		"reqname",
-		"reqdescrip",
-		"reqdateadd",
-		"reqdatemod"
-	);
 
-	function RoomEquipmentMaintenance () {
-		global $reqdatemod;
-		$reqdatemod = date("Y-m-d");
+	function RoomEquipment () {
+		$this->table_definition = array (
+			'reqname' => SQL__VARCHAR(50),
+			'reqdescrip' => SQL__VARCHAR(150),
+			'reqmovable' => SQL__INT_UNSIGNED(0),
+			'id' => SQL__SERIAL
+		);
+
+		$this->variables = array (
+			"reqname",
+			"reqdescrip"
+		);
+
 		$this->MaintenanceModule();
-	} // end constructor RoomEquipmentMaintenance
+	} // end constructor
 
-	function form () {
-		global $display_buffer;
-		foreach ($GLOBALS AS $k => $v) { global ${$k}; }
-		switch ($action) {
-			case "addform":
-				break;
-			case "modform":
-				$r = freemed::get_link_rec ($id, $db_name);
-				extract ($r);
-				break;
-		} // end switch
- 
-		$display_buffer .= "
-    <P>
-    <FORM ACTION=\"".$this->page_name."\" METHOD=\"POST\">
-    <INPUT TYPE=HIDDEN NAME=\"action\" VALUE=\"".
-     ( ($action=="addform") ? "add" : "mod" )."\"> 
-    <INPUT TYPE=HIDDEN NAME=\"id\" VALUE=\"".prepare($id)."\"  >
-    <INPUT TYPE=HIDDEN NAME=\"module\" VALUE=\"".prepare($module)."\"  >
-    <INPUT TYPE=HIDDEN NAME=\"reqdateadd\" VALUE=\"".prepare($reqdateadd)."\"  >
+	function generate_form ( ) {
+		return array (
+			__("Name") =>
+			html_form::text_widget('reqname', 20, 100),
 
-  ".html_form::form_table ( array (
-    __("Name") =>
-    html_form::text_widget('reqname', 20, 100),
-
-    __("Description") =>
-    html_form::text_widget('reqdescrip', 30)
-   ) )."  
-
-    <P>
-    <CENTER>
-    <INPUT TYPE=SUBMIT VALUE=\" ".( ($action=="addform") ? __("Add") :
-      __("Modify") )." \"  >
-    <INPUT TYPE=RESET  VALUE=\" ".__("Clear")." \">
-    </CENTER></FORM>
-  ";
-
-  $display_buffer .= "
-    <P>
-    <CENTER>
-    <A HREF=\"$page_name\"
-     >".__("Abandon ".( ($action=="addform") ? "Addition" : "Modification" )).
-      "</A>
-    </CENTER>
-  ";
-	} // end function RoomEquipmentMaintenance->form()
+			__("Description") =>
+			html_form::text_widget('reqdescrip', 30)
+		);
+	} // end method generate_form
 
 	function view () {
 		global $display_buffer;
@@ -95,10 +62,10 @@ class RoomEquipmentMaintenance extends MaintenanceModule {
 				"", __("NO DESCRIPTION")
 			)
 		);
-	} // end function RoomEquipmentMaintenance->view()
+	} // end method view
 
-} // end class RoomEquipmentMaintenance
+} // end class RoomEquipment
 
-register_module ("RoomEquipmentMaintenance");
+register_module ("RoomEquipment");
 
 ?>
