97d260817d6c4bcd98a0f8d05d623cce1c60fad7
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2005-03-22 15:02:39 +0000

    Typo in the path for fax import, critical bug for faxing.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@1962 6507a214-7cff-0310-bd63-e3842a560bc8

--- debian/cron.d
+++ debian/cron.d
@@ -1,9 +1,11 @@
+# $Id$
+# $Author$
 # /etc/cron.d/freemed crontab fragment
 # Syntax: m h dom mon dow user command
 
 # ----- FreeMED Fax import ----------------------------------------------------
 # Examine files every 5 minutes
-05,10,15,20,25,30,35,40,45,50,55 *	* * *	root	test -f "`ls -1 /var/spool/hylafax/recvq/*.tif* 2>&1 | head -1`" && /usr/share/freemed/freemed/scripts/fax_import/import_all_hylafax.sh
+05,10,15,20,25,30,35,40,45,50,55 *	* * *	root	test -f "`ls -1 /var/spool/hylafax/recvq/*.tif* 2>&1 | head -1`" && /usr/share/freemed/scripts/fax_import/import_all_hylafax.sh
 
 # ----- FreeMED Tickler -------------------------------------------------------
 # Tickler for every year
