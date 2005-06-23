8b54c28bf85d2644a1e245bbed434ccb0e748bc1
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2005-06-23 20:23:32 +0000

    IE/Gecko compatible replaceSelection code (thanks to javascript.faqts!)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2132 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/template/default/ajax.php
+++ lib/template/default/ajax.php
@@ -33,6 +33,54 @@ ob_start();
 <!-- Common functions -->
 <script language="javascript">
 <?php $ajax->show_javascript(); ?>
+
+// replaceSelection functions
+function setSelectionRange(input, selectionStart, selectionEnd) {
+	if (input.setSelectionRange) {
+		input.focus();
+		input.setSelectionRange(selectionStart, selectionEnd);
+	} else if (input.createTextRange) {
+		var range = input.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', selectionEnd);
+		range.moveStart('character', selectionStart);
+		range.select();
+	}
+}
+function setCaretToEnd (input) {
+	setSelectionRange(input, input.value.length, input.value.length);
+}
+function setCaretToBegin (input) {
+	setSelectionRange(input, 0, 0);
+}
+function setCaretToPos (input, pos) {
+	setSelectionRange(input, pos, pos);
+}
+function replaceSelection (input, replaceString) {
+	if (input.setSelectionRange) {
+		var selectionStart = input.selectionStart;
+		var selectionEnd = input.selectionEnd;
+		input.value = input.value.substring(0, selectionStart)
+			+ replaceString
+			+ input.value.substring(selectionEnd);
+		if (selectionStart != selectionEnd) // has there been a selection
+			setSelectionRange(input, selectionStart, selectionStart + replaceString.length);
+		else // set caret
+			setCaretToPos(input, selectionStart + replaceString.length);
+	} else if (document.selection) {
+		var range = document.selection.createRange();
+		if (range.parentElement() == input) {
+			var isCollapsed = range.text == '';
+			range.text = replaceString;
+			if (!isCollapsed)  { // there has been a selection
+				//it appears range.select() should select the newly 
+				//inserted text but that fails with IE
+				range.moveStart('character', -replaceString.length);
+				range.select();
+			}
+		}
+	}
+}
 </script>
 <?php
 
@@ -125,7 +173,7 @@ function x_<?php print $text_id; ?>_call_insert ( ) {
 
 function x_<?php print $text_id; ?>_insert_text (v) {
 	//alert('calling <?print $module; ?> <?php print $method; ?> <?php print $param; ?>');
-	document.getElementById('<?php print $text_id; ?>').value += v;
+	replaceSelection(document.getElementById('<?php print $text_id; ?>'),v);
 }
 </script>
 <?php
