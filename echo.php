c18aef27d930ba8abb4fa3831d70d8668d49d561
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2005-07-06 00:07:08 +0000

    Remove some old, useless legacy billing code.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2139 6507a214-7cff-0310-bd63-e3842a560bc8

--- echo.php
+++
@@ -1,9 +0,0 @@
-<?php
- // $Id$
- // desc: echos text/plain file from input
- // lic : GPL, v2
-
-Header ("Content-type: ".( empty($_POST['type']) ? "text/plain" : $_POST['type'] ) );
-print $_POST['text'];
-
-?>
