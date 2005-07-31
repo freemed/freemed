fbaac4bb2f26fb18dcb3a0d2534974b042cb7bbb
rufustfirefly <rufustfirefly@6507a214-7cff-0310-bd63-e3842a560bc8>
2005-07-31 12:48:59 +0000

    Migrate previous operations to its own table. (Volker)
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2157 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/previous_operations.emr.module.php
+++ modules/previous_operations.emr.module.php
@@ -8,68 +8,79 @@ class PreviousOperationsModule extends EMRModule {
 
 	var $MODULE_NAME = "Previous Operations";
 	var $MODULE_AUTHOR = "jeff b (jeff@ourexchange.net)";
-	var $MODULE_VERSION = "0.1.1";
+	var $MODULE_VERSION = "0.2";
 	var $MODULE_FILE = __FILE__;
 
 	var $PACKAGE_MINIMUM_VERSION = '0.6.0';
 
 	var $record_name = "Previous Operations";
+	var $patient_field = "opatient";
+
 	// Dummy array for prototype:
 	var $summary_items = array ( 1,2,3 );
-	var $patient_field = "id";
-	var $date_field = "dummy";
+	var $date_field = 'id';
+	var $table_name = 'previous_operations';
+	var $order_fields = 'odate,operation';
 
 	function PreviousOperationsModule () {
-		// Call parent constructor
+		$this->table_definition = array (
+			'odate'      => SQL__DATE,
+			'operation'  => SQL__VARCHAR(250),
+			'opatient'   => SQL__INT_UNSIGNED(0),
+			'id'         => SQL__SERIAL
+		);
+
+		$this->variables = array (
+			'operation',
+			'opatient' => $_REQUEST['patient'],
+			'odate' => fm_date_assemble('odate')
+		);
+		
+		// call parent constructor
 		$this->EMRModule();
 	} // end constructor PreviousOperationsModule
 
 	// The EMR box; probably the most important part of this module
 	function summary ($patient, $dummy_items) {
-		// Get patient object from global scope (if it exists)
-		if (isset($GLOBALS[this_patient])) {
-			global $this_patient;
-		} else {
-			$this_patient = CreateObject('FreeMED.Patient', $patient);
-		}
-
-		// Extract ops
-		$ops = $this_patient->local_record["ptops"];
+		$my_result = $GLOBALS['sql']->query(
+			"SELECT * FROM ".$this->table_name." ".
+			"WHERE ".$this->patient_field."='".addslashes($patient)."' ".
+			"ORDER BY ".$this->order_fields
+		);
 
 		// Check to see if it's set (show listings if it is)
-		if (strlen($ops)>3) {
-			// Form an array
-			$my_ops = sql_expand($ops);
-			if (!is_array($my_ops)) {
-				$my_ops = array ($my_ops);
-			}
-
-			// Sort by name
-			arsort($my_ops);
-
+		if ($GLOBALS['sql']->results($my_result)) {
 			// Show menu bar
 			$buffer .= "
 			<table BORDER=\"0\" CELLSPACING=\"0\" WIDTH=\"100%\" ".
 			"CELLPADDING=\"2\">
 			<tr CLASS=\"menubar_info\">
+			<td><b>".__("Date")."</b></td>
 			<td><b>".__("Operation")."</b></td>
 			<td><b>".__("Action")."</b></td>
 			</tr>
 			";
 
-			// Loop thru and display ops 
-			foreach ($my_ops AS $k => $v) {
+			// Loop thru and display operations
+			while ($my_r = $GLOBALS['sql']->fetch_array($my_result)) {
 				$buffer .= "
 				<tr>
-				<td ALIGN=\"LEFT\"><small>".prepare($v)."</small></td>
+				<td ALIGN=\"LEFT\"><small>".prepare($my_r['odate'])."</small></td>
+				<td ALIGN=\"LEFT\"><small>".prepare($my_r['operation'])."</small></td>
 				<td ALIGN=\"LEFT\">".
 				template::summary_modify_link($this,
-				"module_loader.php?module=PreviousOperationsModule&action=modform&patient=".urlencode($patient)."&return=manage&id=".urlencode($k)).
+				"module_loader.php?".
+				"module=".get_class($this)."&".
+				"action=modform&patient=".urlencode($patient).
+				"&return=manage&id=".urlencode($my_r['id'])).
 				template::summary_delete_link($this,
-				"module_loader.php?module=PreviousOperationsModule&action=del&patient=".urlencode($patient)."&return=manage&id=".urlencode($k)).
-				"</td></tr>
+				"module_loader.php?".
+				"module=".get_class($this)."&".
+				"action=del&patient=".urlencode($patient).
+				"&return=manage&id=".urlencode($my_r['id']))."</td>
+				</tr>
 				";
-			} // end looping thru ops 
+			} // end looping thru operations
 
 			// End table
 			$buffer .= "
@@ -94,151 +105,71 @@ class PreviousOperationsModule extends EMRModule {
 			"manage\"/>
 			<input TYPE=\"HIDDEN\" NAME=\"patient\" VALUE=\"".
 			prepare($patient)."\"/>
-			".html_form::text_widget("op", 75)."
+			".fm_date_entry('odate')."
+			".html_form::text_widget("operation", 40, 150)."
 			<input TYPE=\"SUBMIT\" VALUE=\"".__("Add")."\" class=\"button\"/>
 			</form>
 			</div>
 			";
 		return $buffer;
-	} // end function PreviousOperationsModule->summary
+	} // end method summary
 
 	function summary_bar() { }
 
-	function form_table () {
-		$r = freemed::get_link_rec($_REQUEST['patient'], 'patient', true);
-		$p = sql_expand($r['ptops']);
-		if (!is_array($p)) { $p = array($p); }
-		global $op;
-		$op = $p[$_REQUEST['id']];
+	function form_table ( ) {
 		return array (
-			__("Previous Operation") =>
-			html_form::text_widget('op', 50)
+			__("Date") =>
+			fm_date_entry('odate'),
+			
+			__("Operation") =>
+			html_form::text_widget('operation', 128)
 		);
 	} // end method form_table
 
-	function add () {
-		global $display_buffer, $return, $patient, $op;
-		reset ($GLOBALS);
-		while (list($k,$v)=each($GLOBALS)) global ${$k};
-
-		// Get patient object
-		$r = freemed::get_link_rec($patient, 'patient', true);
-
-		// Get ops, and extract to an array
-		$ops = $r["ptops"];
-		$my_ops = sql_expand($ops);
-		if (!is_array($my_ops)) {
-			$my_ops = array ($my_ops);
-		}
-
-		// Add a new member to the array
-		$my_ops[] = $op;
-
-		// Remove empties
-		foreach ($my_ops AS $k => $v) {
-			if (empty($v)) unset($my_ops[$k]);
-		}
-
-		// Recombine into a single variable
-		$ops = sql_squash($my_ops);
-
-		$display_buffer .= "
-		<P><CENTER>
-		".__("Adding")." ...
-		";
-
-		// Update the proper table
-		$query = $sql->update_query (
-			"patient",
-			array ( "ptops" => $ops ),
-			array ( "id" => $patient )
-		);
-		$result = $sql->query($query);
-
-		// Check for result, etc
-		if ($result) { $display_buffer .= __("done");  }
-		 else        { $display_buffer .= __("ERROR"); }
-		$display_buffer .= "</CENTER>\n";
-
-		// If we came from patient management (EMR), return there
-		if ($return=="manage") {
-			Header("Location: manage.php?id=".urlencode($patient));
-			die("");
-		}
-	} // end function PreviousOperationsModule->add()
-
-	function del() { $this->delete(); }
-	function delete ($die = true) {
-		global $display_buffer, $return, $patient, $id;
-		reset ($GLOBALS);
-		while (list($k,$v)=each($GLOBALS)) global $$k;
-
-		// Get patient object
-		$this_patient = CreateObject('FreeMED.Patient', $patient);
-
-		// Get ops, and extract to an array
-		$ops = $this_patient->local_record["ptops"];
-		$my_ops = sql_expand($ops);
-		if (!is_array($my_ops)) {
-			$my_ops = array ($my_ops);
-		}
-
-		// Unset the proper member of the array
-		unset ($my_ops[$id]);
-
-		// Recombine into a single variable
-		$ops = sql_squash($my_ops);
-
-		$display_buffer .= "
-		<P><CENTER>
-		".__("Deleting")." ...
-		";
-
-		// Update the proper table
-		$query = $sql->update_query (
-			"patient",
-			array ( "ptops" => $ops ),
-			array ( "id" => $patient )
-		);
-		$result = $sql->query($query);
-
-		// Check for result, etc
-		if ($result) { $display_buffer .= __("done");  }
-		 else        { $display_buffer .= __("ERROR"); }
-		$display_buffer .= "</CENTER>\n";
-
-		// If we came from patient management (EMR), return there
-		if (($return=="manage") and ($die)) {
-			Header("Location: manage.php?id=".urlencode($patient));
-			die("");
-		}
-	} // end method delete
-
-	function mod () {
-		$this->delete(false);
-		$this->add();
-	} // end method mod
-
-	function view() {
-		global $display_buffer;
-		reset ($GLOBALS);
-		while (list($k,$v)=each($GLOBALS)) global $$k;
-		$display_buffer .= "TODO: Listing for operations here\n";
-	} // end method view
-
 	function recent_text ( $patient, $recent_date = NULL ) {
-		// Get patient object
-		$r = freemed::get_link_rec($patient, 'patient', true);
+		// skip recent; need all for this one
+		$query = "SELECT * FROM ".$this->table_name." ".
+			"WHERE ".$this->patient_field."='".addslashes($patient)."' ".
+			"ORDER BY ".$this->order_fields;
+		$res = $GLOBALS['sql']->query($query);
 
-		// Get ops, and extract to an array
-		$ops = $r["ptops"];
-		$my_ops = sql_expand($ops);
-		if (!is_array($my_ops)) {
-			$my_ops = array ($my_ops);
+		// Get operations, and extract to an array
+		while ($r = $GLOBALS['sql']->fetch_array($res)) {
+			$m[] = trim($r['odate'].' '.$r['operation']);
 		}
-
-		return @join(', ', $my_ops);
+		return @join(', ', $m);
 	} // end method recent_text
+
+	function _update ( ) {
+		$version = freemed::module_version($this->MODULE_NAME);
+
+		// Version 0.2
+		//
+		//	Migrated to separate table
+		//
+		if (!version_check($version, '0.2')) {
+			// Create table
+			$GLOBALS['sql']->query($GLOBALS['sql']->create_table_query($this->table_name, $this->table_definition, array('id')));
+
+			// Migrate old entries
+			$q = $GLOBALS['sql']->query('SELECT ptops,id FROM patient WHERE LENGTH(ptops) > 3');
+			while ($r = $GLOBALS['sql']->fetch_array($q)) {
+				$e = sql_expand($r['ptops']);
+				if (!is_array($e)) { $e = array ($e); }
+				foreach ($e AS $a) {
+					$GLOBALS['sql']->query(
+						$GLOBALS['sql']->insert_query(
+							$this->table_name,
+							array(
+								'opatient'  => $r['id'],
+								'operation' => $a
+							)
+						)
+					); // end query
+				} // end foreach $e
+			} // end while
+		}
+	} // end method _update
 
 } // end class PreviousOperationsModule
 
