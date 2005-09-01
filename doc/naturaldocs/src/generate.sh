44fe197beb38b58f86fb372f77db650397e9730b
jbuchbinder <jeff@ourexchange.net>
2005-09-01 17:00:01 +0000

    Add NaturalDocs documentation, initial version.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2182 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ doc/naturaldocs/src/generate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# $Id$
+# $Author$
+#
+#	Script to generate naturaldocs documentation. Assumes that
+#	NaturalDocs is installed in /usr/share/naturaldocs.
+#
+
+if [ ! -e ./doc/naturaldocs/src/generate.sh ]; then \
+	echo "You need to run this script from the REMITT root directory."; \
+	exit; \
+fi
+
+# Get minor version number
+NV=`perl /usr/share/naturaldocs/NaturalDocs -h | grep version | awk '{ print \$4 }'`
+if [ ! "x$NV" = "x1.3" ]; then \
+	echo "You need to have NaturalDocs version 1.3 installed in /usr/share/naturaldocs/"; \
+	exit; \
+fi
+echo "Found Naturaldocs v$NV"
+
+perl /usr/share/naturaldocs/NaturalDocs -i . -p ./doc/naturaldocs/src -o HTML ./doc/naturaldocs -xi doc -xi .svn
+
