3ef8078706df9f4a6d0ca063a40e844a502814c4
jbuchbinder <jeff@ourexchange.net>
2005-09-21 17:34:24 +0000

    Add initial CSV handling class for output.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2212 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/class.CSV.php
@@ -0,0 +1,80 @@
+<?php
+	// $Id$
+	// $Author$
+
+// Class: FreeMED.CSV
+//
+//	Handle comma-separated values format
+//
+class CSV {
+
+	var $_keys;
+	var $_cache;
+
+	// Constructor: CSV
+	//
+	//	Create CSV object
+	//
+	function CSV ( ) { }
+
+	// Method: ImportSQLQuery
+	//
+	//	Import a query from an SQL statement
+	//
+	// Parameters:
+	//
+	//	$query - SQL query text
+	//
+	function ImportSQLQuery ( $query ) {
+		$q = $GLOBALS['sql']->query($query);
+
+		$notset = false;
+		while ($r = $GLOBALS['sql']->fetch_array($q)) {
+			// Unset integer keys
+			foreach($r AS $k=>$v) {
+				if (is_int($k)) {
+					unset($r[$k]); 
+				} else {	
+					$r[$k] = str_replace("\n", "", $r[$k]);
+					$r[$k] = str_replace("\r", "", $r[$k]);
+				}
+			}
+
+			// Get all keys if we don't have them already
+			if (!$notset) {
+				foreach ($r AS $k => $v) { $keys[] = $k; }
+				$notset = true;
+			}
+
+			// Add results
+			$results[] = $r;			
+		}
+
+		$this->_keys  = $keys;
+		$this->_cache = $results;
+	} // end method ImportSQLQuery
+
+	// Method: Export
+	//
+	//	Export as CSV and die
+	//
+	function Export ( ) {
+		if (!isset($this->_cache)) { return false; }
+
+		$CRLF = "\r\n";
+
+		//Header ("Content-type: application/csv");
+		//Header ("Content-Disposition: \"inline; filename=".mktime().".csv\"");
+
+		print join(',', $this->_keys).$CRLF;
+
+		foreach ($this->_cache AS $v) {
+			print join(',', $v).$CRLF;
+		}
+
+		die();
+	}
+
+} // end class CSV
+
+?>
