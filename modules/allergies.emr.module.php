5a6c7d112335b446ec57bcb9fcb97d2de98ee9fe
jbuchbinder <jeff@ourexchange.net>
2005-12-31 17:27:19 +0000

    Add reviewed column to allergies module.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2364 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/allergies.emr.module.php
+++ modules/allergies.emr.module.php
@@ -8,7 +8,7 @@ class AllergiesModule extends EMRModule {
 
 	var $MODULE_NAME = "Allergies";
 	var $MODULE_AUTHOR = "jeff b (jeff@ourexchange.net)";
-	var $MODULE_VERSION = "0.2";
+	var $MODULE_VERSION = "0.2.1";
 	var $MODULE_FILE = __FILE__;
 
 	var $PACKAGE_MINIMUM_VERSION = '0.6.0';
@@ -16,25 +16,32 @@ class AllergiesModule extends EMRModule {
 	var $record_name = "Allergies";
 	var $table_name = 'allergies';
 	var $patient_field = 'patient';
-        var $date_field = 'dummy';
+	var $date_field = 'reviewed';
+	var $widget_hash = '##allergy## (##severity##)';
 
 	function AllergiesModule () {
 		$this->table_definition = array (
 			'allergy' => SQL__VARCHAR(150),
 			'severity' => SQL__VARCHAR(150),
 			'patient' => SQL__INT_UNSIGNED(0),
+			'reviewed' => SQL__TIMESTAMP(14),
 			'id' => SQL__SERIAL
 		);
 
 		$this->variables = array (
 			'allergy' => html_form::combo_assemble('allergy'),
 			'severity' => html_form::combo_assemble('severity'),
-			'patient'
+			'patient',
+			'reviewed' => SQL__NOW
 		);
 
 		$this->summary_vars = array (
 			__("Allergy") => 'allergy',
-			__("Reaction") => 'severity'
+			__("Reaction") => 'severity',
+			__("Reviewed") => '_reviewed'
+		);
+		$this->summary_query = array (
+			"DATE_FORMAT(reviewed, '%m/%d/%Y') AS _reviewed"
 		);
 		$this->summary_options = SUMMARY_DELETE;
 
@@ -74,6 +81,18 @@ class AllergiesModule extends EMRModule {
 		);
 	} // end method view
 
+	function recent_text ( $patient, $recent_date = NULL ) {
+		// skip recent; need all for this one
+		$query = "SELECT * FROM ".$this->table_name." ".
+			"WHERE ".$this->patient_field."='".addslashes($patient)."' ".
+			"ORDER BY ".$this->date_field." DESC";
+		$res = $GLOBALS['sql']->query($query);
+		while ($r = $GLOBALS['sql']->fetch_array($res)) {
+			$m[] = trim($r['allergy']).' ('.trim($r['severity']).')';
+		}
+		return @join(', ', $m);
+	} // end method recent_text
+
 	// Update
 	function _update ( ) {
 		global $sql;
@@ -101,21 +120,17 @@ class AllergiesModule extends EMRModule {
 					} // end foreach entry
 				} // end loop through patient entries
 			} // end checking for results
-		}	
-	} // end method _update
-            
-            function recent_text ( $patient, $recent_date = NULL ) {
-		// skip recent; need all for this one
-		$query = "SELECT * FROM ".$this->table_name." ".
-			"WHERE ".$this->patient_field."='".addslashes($patient)."' ";
-		$res = $GLOBALS['sql']->query($query);
-		$m[] = "\n\nALLERGIES:\n";
-		while ($r = $GLOBALS['sql']->fetch_array($res)) {
-			$m[] = trim($r['allergy']." (".$r['severity'].")");
 		}
-		return @join("\n", $m);
-	} // end method recent_text
 
+		// Version 0.2.1
+		//
+		//	Add "reviewed" field
+		//
+		if (!version_check($version, '0.2.1')) {
+			$sql->query('ALTER TABLE '.$this->table_name.' '.
+				'ADD COLUMN reviewed TIMESTAMP(14) AFTER patient');
+		}
+	} // end method _update
 
 } // end class AllergiesModule
 
