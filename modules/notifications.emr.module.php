9300f84158e58d241537e9a79b3aaa4b87c65b16
jbuchbinder <jeff@ourexchange.net>
2006-01-24 15:41:44 +0000

    Add notification times up to 10 years.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2390 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/notifications.emr.module.php
+++ modules/notifications.emr.module.php
@@ -86,7 +86,16 @@ class Notifications extends EMRModule {
 					__("9 Months") => $scheduler->date_add(date('Y-m-d'), (9*30)),
 					__("10 Months") => $scheduler->date_add(date('Y-m-d'), (10*30.5)),
 					__("11 Months") => $scheduler->date_add(date('Y-m-d'), (11*30)),
-					__("1 Year") => $scheduler->date_add(date('Y-m-d'), 365)
+					__("1 Year") => $scheduler->date_add(date('Y-m-d'), 365),
+					__("2 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 2)),
+					__("3 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 3)),
+					__("4 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 4)),
+					__("5 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 5)),
+					__("6 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 6)),
+					__("7 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 7)),
+					__("8 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 8)),
+					__("9 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 9)),
+					__("10 Years") => $scheduler->date_add(date('Y-m-d'), (365 * 10))
 				)
 			),
 
