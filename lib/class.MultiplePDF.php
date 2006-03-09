51009833145c056617300668c4526648c3fb729e
jbuchbinder <jeff@ourexchange.net>
2006-03-09 16:18:25 +0000

    Add compositing of multiple PDF file capability.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2467 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/class.MultiplePDF.php
@@ -0,0 +1,59 @@
+<?php
+	// $Id$
+	// $Author$
+
+// Class: FreeMED.MultiplePDF
+//
+//	Handle compositing of multiple PDF files
+//
+class MultiplePDF {
+
+	var $stack;
+
+	// Constructor: MultiplePDF
+	function MultiplePDF ( ) {
+	} // end method constructor
+
+	// Method: Add
+	//
+	//	Add an additional PDF to the stack of PDFs to be composited.
+	//
+	// Parameters:
+	//
+	//	$pdffile - PDF file name
+	//
+	function Add ( $pdffile ) {
+		if (file_exists($pdffile)) { $this->stack[] = $pdffile; }
+	} // end method Add
+
+	// Method: Composite
+	//
+	//	Composite all PDF files in the stack to a single PDF file
+	//
+	// Returns:
+	//
+	//	Name of temporary file containing all PDF information.
+	//
+	function Composite ( ) {
+		// If there is nothing on the stack, null filename
+		if (count($this->stack) < 1) { return ''; }
+
+		// Create temporary filename
+		$tempfile ='/tmp/'.mktime().'.pdf';
+
+		// Create command
+		$cmd = "pdfjoin --paper letterpaper --outfile '${tempfile}' ";
+		foreach ($this->stack AS $pdf) {
+			$cmd .= " \"".escapeshellcmd($pdf)."\" ";
+		}
+
+		// Execute the composite command
+		$garbage_collector = system($cmd);
+
+		// Return the temporary file name
+		return $tempfile;
+	} // end method Composite
+
+} // end class MultiplePDF
+
+?>
