8eeee573dec7d343c618ad09ef7e5aca36404c61
jbuchbinder <jeff@ourexchange.net>
2006-06-03 17:10:39 +0000

    Move to Agata 7.x engine for live version. #107
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2547 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/system_reports.report.module.php
+++ modules/system_reports.report.module.php
@@ -8,117 +8,70 @@ class SystemReports extends ReportsModule {
 
 	var $MODULE_NAME = "System Reports";
 	var $MODULE_AUTHOR = "jeff b (jeff@ourexchange.net)";
-	var $MODULE_VERSION = "0.1";
+	var $MODULE_VERSION = "0.2";
 	var $MODULE_FILE = __FILE__;
-	var $MODULE_DESCRIPTION = "Interface to the Agata reporting engine.";
+	var $MODULE_DESCRIPTION = "Interface to the Agata 7.x reporting engine.";
 
-	var $PACKAGE_MINIMUM_VERSION = '0.6.0';
+	var $PACKAGE_MINIMUM_VERSION = '0.8.2';
+	// __("System Reports")
 
 	function display () {
-		$agata = CreateObject('_FreeMED.Agata');
-		$meta = $agata->_ReadMetaInformation($_REQUEST['report'].'.report');
-		// Handle split dates if we have them:
-		if (isset($_REQUEST['by_y'])) {
-			$by = freemed::date_assemble('by');
-		} else {
-			$by = $_REQUEST['by'];
-		}
-		
-		// Serve it up
-		switch ($meta['type']) {
-			case 'merge':
-			$agata->CreateReport(
-				'Merge',
-				$_REQUEST['report'],
-				$meta['name'],
-				array('##BY##' => addslashes($by))
-			);
-			switch ($_REQUEST['format']) {
-				case 'ps':
-				$agata->ServeReport();
-				break;
-			
-				case 'pdf': default:
-				$agata->ServeMergeAsPDF();
-				break;
+		// Import parameters
+		foreach ($_GET AS $k => $v) {
+			switch ($k) {
+				case 'module':
+				case 'action':
+				case 'format':
+				case 'report':
+				case 'submit_action';
+					break;
+	
+				default:
+					$param['$'.$k] = $v;
+					break;
 			}
-			break;
-			
-			case 'standard': default:
-			$agata->CreateReport(
-				$_REQUEST['format'],
-				$_REQUEST['report'],
-				$meta['name'],
-				array('##BY##' => addslashes($by))
-			);
-			$agata->ServeReport();
-			break;
 		}
+
+		$a = CreateObject('_FreeMED.Agata7');
+		if (is_array($param)) {
+			$a->CreateReport( $_REQUEST['format'], $_REQUEST['report'], $param );
+		} else {
+			$a->CreateReport( $_REQUEST['format'], $_REQUEST['report'] );
+		}
+
+		$a->ServeReport();
 	} // end method display
+
+	function form ( ) {
+		global $display_buffer;
+		$a = CreateObject('_FreeMED.Agata7');
+		$f = $a->CreateForm($_REQUEST['report']);
+		if (is_object($f)) { $display_buffer .= $f->toHtml(); }
+		return true;
+	} // end method form
 
 	function view() { 
 		global $display_buffer;
+
+		if ($_REQUEST['action'] == 'form') { return $this->form(); }
+		if ($_REQUEST['action'] == 'view') { return $this->display(); }
 	
 		// Get list from Agata class
-		$agata = CreateObject('_FreeMED.Agata');
+		$agata = CreateObject('_FreeMED.Agata7');
 		$reports = $agata->GetReports();
 		$display_buffer .= "<table width=\"90%\" border=\"0\" ".
 			"cellspacing=\"0\">\n<tr class=\"DataHead\">\n".
 			"<td>".__("Report")."</td>\n".
-			"<td>".__("For")."</td>\n".
-			"<td>".__("Format")."</td>\n".
+			"<td>".__("Description")."</td>\n".
 			"<td>&nbsp;</td>\n".
 			"</tr>\n";
 		foreach ($reports AS $k => $v) {
-			$display_buffer .= "<tr><td>".prepare($v['name']).
-				"</td>\n".
-				"<form method=\"post\" target=\"_report\">\n".
+			$display_buffer .= "<tr><td><b>".prepare($v['Title'])."</b></td>\n".
+				"<td><i>".prepare($v['Description'])."</i></td>\n".
+				"<form method=\"post\">\n".
 				"<input type=\"hidden\" name=\"module\" value=\"".prepare($_REQUEST['module'])."\">\n".
-				"<input type=\"hidden\" name=\"action\" value=\"display\">\n".
-				"<input type=\"hidden\" name=\"report\" value=\"".prepare($k)."\">\n".
-				"<td>";
-			switch ($v['by']) {
-				case 'date':
-				$display_buffer .= fm_date_entry('by');
-				break;
-
-				case 'provider':
-				$display_buffer .= module_function(
-					'ProviderModule',
-					'widget',
-					array (
-						'by',
-						'phyref=\'no\''
-					)
-				);
-				break;
-				
-				default: $display_buffer .= '--'; break;
-			}
-			$display_buffer .= "</td><td>\n";
-			switch ($v['type']) {
-				case 'merge':
-				$display_buffer .= html_form::select_widget(
-					'format',
-					array(
-						'PDF' => 'pdf',
-						'Postscript' => 'ps'
-					)
-				); break;
-				
-				case 'standard': default:
-				$display_buffer .= html_form::select_widget(
-					'format',
-					array(
-						'HTML' => 'Html',
-						'CSV'  => 'Csv',
-						'PDF'  => 'Pdf',
-						'Postscript' => 'Ps',
-						'Plain Text' => 'Txt'
-					)
-				);
-				break;
-			}
+				"<input type=\"hidden\" name=\"action\" value=\"form\">\n".
+				"<input type=\"hidden\" name=\"report\" value=\"".prepare($k)."\">\n";
 			$display_buffer .= "</td><td>".
 				"<input type=\"submit\" value=\"".__("Get Report")."\"/></form></tr>\n";
 		}
