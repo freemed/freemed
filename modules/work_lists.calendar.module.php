8cbbb28692612a5ed38bda6fa5683c6a64778ec7
jbuchbinder <jeff@ourexchange.net>
2006-06-04 10:37:23 +0000

    Atomic scheduling support added to work list module. #106
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2551 6507a214-7cff-0310-bd63-e3842a560bc8

--- modules/work_lists.calendar.module.php
+++ modules/work_lists.calendar.module.php
@@ -47,11 +47,56 @@ class WorkListsModule extends BaseModule {
 	} // end constructor WorkListsModule
 
 	function notify ( ) {
+		global $display_buffer;
+
 		$enable = freemed::config_value( 'worklist_enabled' );
 		$providers = freemed::config_value( 'worklist_providers' );
 
 		// Skip if this is not enabled
 		if (!$enable) { return false; }
+
+		include_once(freemed::template_file('ajax.php'));
+
+		$display_buffer .= "
+		<script language=\"javascript\">
+		var workListElementActive = 0;
+		var workListElementCurrent;
+
+		function workListClick ( id ) {
+			if (workListElementActive) { return false; }
+			workListElementCurrent = id;
+			document.getElementById(id).innerHTML = '<select id=\"worklist_select\" onChange=\"workListProcess(\'worklist_select\', \'' + id + '\'); return true;\">' +
+			'<option value=\"\">-</option>'+
+		";
+
+		$q = $GLOBALS['sql']->query("SELECT * FROM schedulerstatustype ORDER BY id");
+		while ($r = $GLOBALS['sql']->fetch_array($q)) {
+			$display_buffer .= "'<option value=\"${r['id']}\">${r['sname']}</option>'+\n";
+		}
+		$display_buffer .= "
+			'</select>';
+			workListElementActive = 1;
+		}
+
+		function workListProcess ( id, parent ) {
+			if (! document.getElementById(id).value ) {
+				document.getElementById(parent).innerHTML = '&nbsp;';
+				workListElementCurrent = '';
+				workListElementActive = 0;
+				return false;
+			}
+			x_module_html('".get_class($this)."', 'ajax_process', document.getElementById(id).value + workListElementCurrent, workListPopulate);
+		}
+
+		function workListPopulate ( value ) {
+			document.getElementById(workListElementCurrent).style.backgroundColor = value;
+			document.getElementById(workListElementCurrent).innerHTML = '&nbsp;';
+			workListElementCurrent = '';
+			workListElementActive = 0;
+		}
+
+		</script>
+		";
 
 		// Get list of providers
 		$p = explode(',', $providers);
@@ -68,10 +113,49 @@ class WorkListsModule extends BaseModule {
 		);
 	} // end method notify
 
+	function ajax_process ( $v ) {
+		if (!is_object($GLOBALS['this_user'])) {
+			$GLOBALS['this_user'] = CreateObject('FreeMED.User');
+		}
+
+		list( $status, $appt ) = explode('x', $v);
+
+		// Keep funky things from happening
+		if ($status+0 == 0) { return ''; }
+
+		// Actual change
+		$a = freemed::get_link_rec($appt, 'scheduler');
+		$GLOBALS['sql']->query(
+			$GLOBALS['sql']->insert_query(
+				'scheduler_status',
+				array (
+					'csstamp' => SQL__NOW,
+					'cspatient' => $a['calpatient'],
+					'csappt' => $appt,
+					'csstatus' => $status,
+					'csuser' => $GLOBALS['this_user']->user_number
+				)
+			)
+		);
+
+		// Return status color properly
+		$q = $GLOBALS['sql']->query("SELECT * FROM schedulerstatustype WHERE id='".addslashes($status)."'");
+		$r = $GLOBALS['sql']->fetch_array($q);
+		return $r['scolor'];
+	}
+
 	// ----- Internal methods ------------------------------------------------------
 
 	function generate_worklist ( $provider ) {
 		$date = date('Y-m-d');
+		$date = '2006-05-01'; // FIXME FIXME FIXME
+
+		// Load lookup table
+		$q = $GLOBALS['sql']->query( "SELECT * FROM schedulerstatustype" );
+		while ($r = $GLOBALS['sql']->fetch_array( $q )) {
+			$lookup[$r['id']] = $r['scolor'];
+		}
+		unset ($q); unset ($r);
 
 		$pobj = CreateObject( '_FreeMED.Physician', $provider );
 		LoadObjectDependency( '_FreeMED.Scheduler' );
@@ -82,13 +166,15 @@ class WorkListsModule extends BaseModule {
 			'<th>'.__("Time").'</th>'.
 			'<th>'.__("Status").'</th></tr>';
 
-		$query = "SELECT p.id AS s_patient_id, CONCAT(p.ptlname,', ', p.ptfname) AS s_patient, s.calhour AS s_hour, s.calminute AS s_minute, s.calduration AS s_duration FROM scheduler s LEFT OUTER JOIN patient p ON p.id=s.calpatient WHERE s.caldateof='".addslashes($date)."' AND s.calphysician='".addslashes($provider)."' ORDER BY s_hour, s_minute";
+		$query = "SELECT s.id AS id, p.id AS s_patient_id, CONCAT(p.ptlname,', ', p.ptfname) AS s_patient, s.calhour AS s_hour, s.calminute AS s_minute, s.calduration AS s_duration FROM scheduler s LEFT OUTER JOIN patient p ON p.id=s.calpatient WHERE s.caldateof='".addslashes($date)."' AND s.calphysician='".addslashes($provider)."' ORDER BY s_hour, s_minute";
 		$q = $GLOBALS['sql']->query( $query );
 		while ( $r = $GLOBALS['sql']->fetch_array( $q ) ) {
+			$current_status = module_function( 'schedulerpatientstatus', 'getPatientStatus', array( $r['s_patient_id'], $r['id'] ) );
+			
 			$buffer .= "<tr>\n".
-				"<td><a href=\"manage.php?id=".$r['s_patient_id']."\">".prepare($r['s_patient'])."</a></td>\n".
+				"<td><a href=\"manage.php?id=${r['s_patient_id']}\">".prepare($r['s_patient'])."</a></td>\n".
 				"<td>".Scheduler::display_time($r['s_hour'], $r['s_minute'])."</td>\n".
-				"<td>&nbsp;</td>\n".
+				"<td ".( $current_status ? "bgcolor=\"${lookup[$current_status]}\"" : "" )." id=\"x${r['id']}\" onClick=\"workListClick('x${r['id']}'); return true;\">&nbsp;</td>\n".
 				"</tr>\n";
 		} // end fetch_array
 
