765b88f573fdbe78347031e26b54430355aa9615
jbuchbinder <jeff@ourexchange.net>
2006-07-25 04:38:21 +0000

    Fix to handle any valid version 1.x of NaturalDocs.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2583 6507a214-7cff-0310-bd63-e3842a560bc8

--- doc/naturaldocs/src/generate.sh
+++ doc/naturaldocs/src/generate.sh
@@ -12,12 +12,12 @@ if [ ! -e ./doc/naturaldocs/src/generate.sh ]; then \
 fi
 
 # Get minor version number
-NV=`perl /usr/share/naturaldocs/NaturalDocs -h | grep version | awk '{ print \$4 }'`
-if [ ! "x$NV" = "x1.3" ]; then \
-	echo "You need to have NaturalDocs version 1.3 installed in /usr/share/naturaldocs/"; \
+NV=$(perl /usr/share/naturaldocs/NaturalDocs -h | grep version | awk -F'version ' '{ print $2 }')
+if [ "$(echo "${NV}" | grep '1.')" = "" ]; then \
+	echo "You need to have NaturalDocs version >= 1.3 installed in /usr/share/naturaldocs/"; \
 	exit; \
 fi
-echo "Found Naturaldocs v$NV"
+echo "Found Naturaldocs v${NV}"
 
 perl /usr/share/naturaldocs/NaturalDocs -i . -p ./doc/naturaldocs/src -o HTML ./doc/naturaldocs -xi doc -xi .svn
 
