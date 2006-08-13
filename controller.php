a27deec7919f514f4b46cbb32eea85a15408be48
jbuchbinder <jeff@ourexchange.net>
2006-08-13 12:15:25 +0000

    Update comment block, deal with authentication by using default_load() method.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2604 6507a214-7cff-0310-bd63-e3842a560bc8

--- controller.php
+++ controller.php
@@ -1,6 +1,24 @@
 <?php
-	// $Id$
-	// jeff@freemedsoftware.org
+ // $Id$
+ //
+ // Authors:
+ // 	Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 include_once 'lib/freemed.php';
 
@@ -42,7 +60,11 @@ unset ( $controller_name );
 include_once(dirname(__FILE__)."/ui/".strtolower($layout)."/controller/controller.${piece}.php");
 $controller_name = 'controller_'.str_replace('.', '_', $piece);
 $controller = new ${controller_name};
-$controller->action ( );
+if ( CallMethod ( 'org.freemedsoftware.public.Login.LoggedIn' ) ) {
+	$controller->action ( );
+} else {
+	$controller->load_default ( );
+}
 
 //----------------- Functions ----------------------------------------------
 
