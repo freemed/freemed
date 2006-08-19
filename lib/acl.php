772c880feb48f5e147b2bf179f034c22c1ee442d
jbuchbinder <jeff@ourexchange.net>
2006-08-19 13:07:23 +0000

    ACL wrapper updates for new object loader system.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2615 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/acl.php
+++ lib/acl.php
@@ -1,12 +1,30 @@
 <?php
-	// $Id$
-	// $Author$
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 // I don't know what this condition could be, but there's always
 // some reason *not* to load this
 if (1 == 1) {
 	// Load phpgacl object, etc here....
-	$acl = CreateObject('_ACL.gacl', 
+	$acl = CreateObject('org.freemedsoftware.acl.gacl', 
 		array (
 			// Database information from FreeMED
 			'db_type' => 'mysql', // hardcoded for now
