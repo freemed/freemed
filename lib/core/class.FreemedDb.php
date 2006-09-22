c0bb67982f8cbed55e25e46c657135d049028294
jbuchbinder <jeff@ourexchange.net>
2006-09-22 16:08:29 +0000

    Starting attempt to migrate FreeMED's SQL engine from phpwebtools to an extension of PEAR's Db class.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2651 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/core/class.FreemedDb.php
@@ -0,0 +1,37 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+LoadObjectDependency('net.php.pear.Db');
+
+// Class: org.freemedsoftware.core.FreemedDb
+//
+class FreemedDb extends Db {
+
+	public function connect ( ) {
+		PEAR::setErrorHandling ( PEAR_ERROR_RETURN );
+		$uri = "mysql://".DB_USER.":".DB_PASSWORD."@".DB_HOST."/".DB_NAME;
+		return DB::connect ( $uri, false );
+	} // end public function connect
+
+} // end class FreemedDb
+
+?>
