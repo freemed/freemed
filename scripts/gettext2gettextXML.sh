68c8b383b7d6342ebb1f03acd523dab64cd40a8a
jbuchbinder <jeff@ourexchange.net>
2006-09-23 12:49:04 +0000

    Add copyright notices to all shell scripts.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2654 6507a214-7cff-0310-bd63-e3842a560bc8

--- scripts/gettext2gettextXML.sh
+++ scripts/gettext2gettextXML.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 # $Id$
-# $Author$
+#
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
+#
+# Copyright (C) 1999-2006 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 # Quick script for changing _()'s to __()'s for gettextXML functions
 
 for i in *.php; do
--- scripts/svn2cl.sh
+++ scripts/svn2cl.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 # $Id$
-# $Author$
+#
+# Authors:
+#      Arthur de Jong <adejong@debian.org>
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
+#
+# Copyright (C) 2005 Arthur de Jong
+# Copyright (C) 1999-2006 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#	Original copyright notice:
+#
+# Copyright (C) 2005 Arthur de Jong.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+# 3. The name of the author may not be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+
 #
 #	Script to generate Changelog from Subversion XML output.
 #
