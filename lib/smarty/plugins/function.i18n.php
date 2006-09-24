4655413e45b9a9e457e6048c2dce07ab5717ef7b
jbuchbinder <jeff@ourexchange.net>
2006-09-24 05:37:47 +0000

    Add Smarty wrapper for GettextXML as i18n function.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2661 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/smarty/plugins/function.i18n.php
@@ -0,0 +1,38 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+function smarty_function_i18n ( $params, &$smarty ) {
+	LoadObjectDependency ( 'org.freemedsoftware.core.GettextXML' );
+
+	if ( !isset ( $params['s'] ) ) { $smarty->error ( "String not specified" ); }
+
+	// Set namespace if specified
+	if ( isset ( $params['ns'] ) ) { GettextXML::textdomain ( $params['ns'] ); }
+
+	// Set namespace if specified
+	if ( isset ( $params['lang'] ) ) { GettextXML::setlanguage ( $params['lang'] ); }
+
+	// Wrap GettextXML
+	return GettextXML::gettext_xml ( $params['s'] );
+} // end function smarty_function_i18n
+
+?>
