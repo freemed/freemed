e0a24d7cc934650a2b32ea0379fcaf37dc852c24
jbuchbinder <jeff@ourexchange.net>
2006-10-13 10:51:14 +0000

    Basic relay tests for JSON and XML-RPC relays.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2794 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ tests/relay.php
@@ -0,0 +1,33 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+include_once( dirname(__FILE__).'/bootstrap.test.php' );
+
+$xmlrpc = CreateObject('org.freemedsoftware.core.Relay_Xmlrpc');
+$json = CreateObject('org.freemedsoftware.core.Relay_Json');
+$msg = file_get_contents ( dirname(__FILE__)."/xmlrpc.msg.txt" );
+$value = $xmlrpc->deserialize_request ( $msg );
+t( 'org.freemedsoftware.core.Relay_Xmlrpc.deserialize_request', $value );
+t( 'org.freemedsoftware.core.Relay_Json.serialize_response', $json->serialize_response ( $value ) );
+
+?>
