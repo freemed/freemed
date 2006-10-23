b667ddde47ca1c53ebb3e42cba9638f1f37a6f57
jbuchbinder <jeff@ourexchange.net>
2006-10-23 20:01:32 +0000

    Add rebuild_acl rebuilding script for ACLs in case they are destroyed.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2820 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ scripts/rebuild_acl.php
@@ -0,0 +1,67 @@
+#!/usr/bin/env php
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+if (!$_SERVER['argc']) { die ("cannot be called via web"); }
+
+ini_set('include_path', dirname(dirname(__FILE__)).':'.ini_get('include_path'));
+include_once ( 'lib/freemed.php' );
+
+$cache = freemed::module_cache();
+
+print "ACL Rebuild Tool\n";
+print "(c) 2006 FreeMED Software Foundation\n\n";
+
+$tables = array (
+	'acl_acl',
+	'acl_acl_sections',
+	'acl_acl_seq',
+	'acl_aco',
+	'acl_aco_map',
+	'acl_aco_sections',
+	'acl_aro',
+	'acl_aro_groups',
+	'acl_aro_groups_map',
+	'acl_aro_map',
+	'acl_aro_sections',
+	'acl_aro_seq',
+	'acl_axo',
+	'acl_axo_groups',
+	'acl_axo_groups_map',
+	'acl_axo_map',
+	'acl_axo_sections',
+	'acl_axo_seq',
+	'acl_groups_aro_map',
+	'acl_groups_axo_map',
+	'acl_phpgacl'
+);
+
+print " - Dropping tables ... ";
+foreach ($tables AS $table) { $GLOBALS['sql']->query('DROP TABLE '.$table); }
+print "[done]\n";
+
+// Reinitialize db
+print " - Reinitializing ACL tables ... ";
+module_function('acl', '_setup', array());
+print "[done]\n";
+
+?>
