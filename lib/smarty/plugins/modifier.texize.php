2e22c63bab3050cef41e20e9155f9df05f79ab7d
jbuchbinder <jeff@ourexchange.net>
2006-10-23 15:51:48 +0000

    Fix texize, was previously returning null strings due to namespace clobbering.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2810 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/smarty/plugins/modifier.texize.php
+++ lib/smarty/plugins/modifier.texize.php
@@ -1,4 +1,26 @@
 <?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 /**
  * Smarty plugin
  * @package Smarty
@@ -12,11 +34,11 @@
  * Type:     modifier<br>
  * Name:     texize<br>
  * Purpose:  Escape the string according to tex
- * @author   Jeff Buchbinder <jeff at freemedsoftware dot com>
+ * @author   Jeff Buchbinder <jeff at freemedsoftware dot org>
  * @param string
  * @return string
  */
-function smarty_modifier_texize( $text )
+function smarty_modifier_texize( $string )
 {
         // Sanitize all but HTML markers
         $text = str_replace('\\', '\\\\', $string);
