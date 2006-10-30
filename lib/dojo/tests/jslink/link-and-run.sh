2358e4696df14612c6906f15d1a06d4c59fd6e80
jbuchbinder <jeff@ourexchange.net>
2006-10-30 11:10:18 +0000

    Remove 0.3.x dojo series.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2824 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/dojo/tests/jslink/link-and-run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-perl ../../buildscripts/jslink.pl -pre cat -i program.js -l lib.js -o out.js 
-java -jar ../../buildscripts/lib/custom_rhino.jar -e "load('out.js'); load('program.js');"
