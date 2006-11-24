4967e2cddd237802eb28dd43eb75609d2cf73c7c
jbuchbinder <jeff@ourexchange.net>
2006-11-24 15:59:03 +0000

    Add CheckPhpMysqlEnabled(), CreateSettingsFile(), CreateDatabase() methods, initial versions.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@2883 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/public/class.Installation.php
+++ lib/public/class.Installation.php
@@ -52,6 +52,18 @@ class Installation {
 		return mysql_select_db( $name, $link );
 	} // end method CheckDbCredentials
 
+	// Method: CheckPhpMysqlEnabled
+	//
+	//	Determine whether PHP's MySQL extension is properly installed
+	//
+	// Returns:
+	//
+	//	Boolean.
+	//
+	public function CheckPhpMysqlEnabled ( ) {
+		return function_exists ( 'mysql_pconnect' );
+	} // end method CheckPhpMysqlEnabled
+
 	// Method: CreateAdministrationAccount
 	//
 	//	Create system administration account.
@@ -83,13 +95,84 @@ class Installation {
 				'userfac' => '-1',
 				'userphy' => '-1',
 				'userphygrp' => '-1',
-				'userrealphy' => '0'
+				'userrealphy' => '0',
 				'id' => '1'
 			)
 		);
 		$res = $GLOBALS['sql']->query( $query );
 		return $res ? true : false;
 	} // end method CreateAdministrationAccount
+
+	// Method: CreateSettingsFile
+	//
+	//	Generate lib/settings.php file.
+	//
+	// Parameters:
+	//
+	//	$params - Hash containing:
+	//	* host - Database host name
+	//	* name - Database name
+	//	* username - Database user
+	//	* password - Database password
+	//	* installation - Installation name
+	//	* starttime - Start time for scheduler
+	//	* endtime - End time for scheduler
+	//
+	// Returns:
+	//
+	//	Boolean.
+	//
+	public function CreateSettingsFile ( $params ) {
+		// Make sure we don't help out hack attempts
+		if ( file_exists ( PHYSICAL_LOCATION . '/data/cache/healthy' ) ) {
+			return false;
+		}
+
+		include_once( PHYSICAL_LOCATION.'/lib/smarty/Smarty.class.php' );
+		$smarty = new Smarty;
+
+		// Override Smarty defaults
+		$smarty->template_dir = PHYSICAL_LOCATION."/lib/";
+		$smarty->compile_dir = PHYSICAL_LOCATION."/data/cache/smarty/templates_c/";
+		$smarty->cache_dir = PHYSICAL_LOCATION."/data/cache/smarty/cache/";
+
+		// Change delimiters to be something a bit more sane
+		$smarty->left_delimiter = '<{';
+		$smarty->right_delimiter = '}>';
+
+		foreach ( $params AS $k => $v ) { $smarty->assign( $k, $v ); }
+
+		$return = @file_put_contents( $smarty->fetch( 'settings.php.tpl' ), PHYSICAL_LOCATION.'/lib/settings.php' );
+		return $return ? true : false;
+	} // end method CreateSettingsFile
+
+	// Method: CreateDatabase
+	//
+	//	Run actual database creation routines.
+	//
+	// Returns:
+	//
+	//	Boolean.
+	//
+	public function CreateDatabase ( ) {
+		if ( ! ( is_defined('DB_USER') && is_defined('DB_PASSWORD') && is_defined('DB_NAME') ) ) {
+			return false;
+		}
+		// Make sure we don't help out hack attempts
+		if ( file_exists ( PHYSICAL_LOCATION . '/data/cache/healthy' ) ) {
+			return false;
+		}
+	
+		// Create initial modules table	
+		$command = dirname(__FILE__).'/../../scripts/load_schema.sh '.escapeshellarg('mysql').' '.escapeshellarg('modules').' '.escapeshellarg(DB_USER).' '.( DB_PASSWORD ? escapeshellarg(DB_PASSWORD) : '""' ).' '.escapeshellarg(DB_NAME);
+	        system ( $command );
+
+		// Load module index, which should cause complete initialization
+		$modules = CreateObject( 'org.freemedsoftware.core.ModuleIndex', true );
+
+		// Return success status
+		return true;
+	} // end method CreateDatabase
 
 } // end class Installation
 
