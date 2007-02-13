39c624973454d0bc5d83d8827c18910c5b46cebe
jbuchbinder <jeff@ourexchange.net>
2007-02-13 00:45:37 +0000

    Using a built-in function results in throwing nulls, we'll avoid it for now.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3070 6507a214-7cff-0310-bd63-e3842a560bc8

--- lib/core/class.Relay_Json.php
+++ lib/core/class.Relay_Json.php
@@ -49,7 +49,7 @@ class Relay_Json extends Relay {
 				//syslog(LOG_INFO, "deserialize_request [ $k ] = $v");
 			}
 		}
-		if (function_exists( 'json_decode' )) {
+		if (function_exists( 'json_decode' ) && 0==1) {
 			// Try the JSON PECL native function first
 			$return = utf8_decode(json_decode( stripslashes($request), true ));
 			//syslog(LOG_INFO, "json_decode = $return");
