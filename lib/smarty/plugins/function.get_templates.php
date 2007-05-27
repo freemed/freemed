625ed7eac181092c970cd3069b220d08aba08d3d
jbuchbinder <jeff@ourexchange.net>
2007-05-27 13:40:43 +0000

    Add template globbing function for allowing UI "hooks" to be added by other packages by simply adding a file with the glob name specification. This will be in the org.freemedsoftware.hook.* namespace for UI components.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3318 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/smarty/plugins/function.get_templates.php
@@ -0,0 +1,52 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2006 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+function smarty_function_get_templates($params, &$smarty) {
+	if (!isset($params['var'])) {
+		$smarty->trigger_error("get_templates: missing 'var' parameter");
+		return;
+	}
+
+	if($params['var'] == '') {
+		$smarty->trigger_error("get_templates: missing 'var' parameter");
+		return;
+	}
+
+	if (!isset($params['glob'])) {
+		$smarty->trigger_error("get_templates: missing 'glob' parameter");
+		return;
+	}
+
+	if($params['glob'] == '') {
+		$smarty->trigger_error("get_templates: missing 'glob' parameter");
+		return;
+	}
+
+	$files = glob( $smarty->template_dir . "/" . $params['glob'] );
+
+	$smarty->assign($params['var'], $files);
+
+	return '';
+}
+
+?>
