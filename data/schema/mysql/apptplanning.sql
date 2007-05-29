08867908a631a99e3c479b3b1065f9e98fcd066b
jbuchbinder <jeff@ourexchange.net>
2007-05-29 19:56:08 +0000

    Add initial appointment followup support, no methods yet, just basic data model.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3328 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ data/schema/mysql/apptplanning.sql
@@ -0,0 +1,82 @@
+# $Id$
+#
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
+#
+# FreeMED Electronic Medical Record and Practice Management System
+# Copyright (C) 1999-2007 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+CREATE TABLE IF NOT EXISTS `apptplanning` (
+	appatient		BIGINT UNSIGNED NOT NULL,
+	apdatecreated		TIMESTAMP(14) NOT NULL DEFAULT NOW(),
+	apdatetarget		DATE NOT NULL,
+	apreason		VARCHAR (150),
+	apschedulerlink		INT UNSIGNED NOT NULL DEFAULT 0,
+	approvider		BIGINT UNSIGNED NOT NULL DEFAULT 0,
+	apnotifiedon		TIMESTAMP (14),
+	user			INT UNSIGNED NOT NULL,
+	id			SERIAL,
+
+	#	Define keys
+	KEY			( appatient, apdatetarget ),
+	FOREIGN KEY		( appatient ) REFERENCES patient ( id ) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+DROP PROCEDURE IF EXISTS apptplanning_Upgrade;
+DELIMITER //
+CREATE PROCEDURE apptplanning_Upgrade ( )
+BEGIN
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+
+	#----- Remove triggers
+	DROP TRIGGER apptplanning_Delete;
+	DROP TRIGGER apptplanning_Insert;
+	DROP TRIGGER apptplanning_Update;
+
+	#----- Upgrades
+END
+//
+DELIMITER ;
+CALL apptplanning_Upgrade( );
+
+#----- Triggers
+
+DELIMITER //
+
+CREATE TRIGGER apptplanning_Delete
+	AFTER DELETE ON apptplanning
+	FOR EACH ROW BEGIN
+		DELETE FROM `patient_emr` WHERE module='apptplanning' AND oid=OLD.id;
+	END;
+//
+
+CREATE TRIGGER apptplanning_Insert
+	AFTER INSERT ON apptplanning
+	FOR EACH ROW BEGIN
+		INSERT INTO `patient_emr` ( module, patient, oid, stamp, summary, provider, user ) VALUES ( 'apptplanning', NEW.appatient, NEW.id, NEW.apdatecreated, NEW.apreason, NEW.approvider, NEW.user );
+	END;
+//
+
+CREATE TRIGGER apptplanning_Update
+	AFTER UPDATE ON apptplanning
+	FOR EACH ROW BEGIN
+		UPDATE `patient_emr` SET stamp=NEW.apdatecreated, patient=NEW.appatient, summary=NEW.apreason, provider=NEW.approvider, user=NEW.user WHERE module='apptplanning' AND oid=NEW.id;
+	END;
+//
+
+DELIMITER ;
+
