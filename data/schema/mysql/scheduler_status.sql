c7c89e442efb76e750f758f92b5dab6c67b9b326
jbuchbinder <jeff@ourexchange.net>
2007-06-06 14:50:24 +0000

    Support for aggregation table to track deltas between statuses.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3383 6507a214-7cff-0310-bd63-e3842a560bc8

--- data/schema/mysql/scheduler_status.sql
+++ data/schema/mysql/scheduler_status.sql
@@ -38,6 +38,23 @@ CREATE TABLE IF NOT EXISTS `scheduler_status` (
 	FOREIGN KEY		( cspatient ) REFERENCES patient ( id ) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS `scheduler_status_delta` (
+	patient			BIGINT UNSIGNED NOT NULL,
+	appointment		INT UNSIGNED NOT NULL,
+	st_start		INT UNSIGNED,
+	st_end			INT UNSIGNED,
+	stamp_start		TIMESTAMP (14),
+	stamp_end		TIMESTAMP (14),
+	duration		INT UNSIGNED,
+	id_start		INT UNSIGNED,
+	id_end			INT UNSIGNED,
+	id			SERIAL,
+
+	#	Define keys
+	
+	FOREIGN KEY		( patient ) REFERENCES patient ( id ) ON DELETE CASCADE
+);
+
 DROP PROCEDURE IF EXISTS scheduler_status_Upgrade;
 DELIMITER //
 CREATE PROCEDURE scheduler_status_Upgrade ( )
@@ -69,7 +86,13 @@ CREATE TRIGGER scheduler_status_Delete
 CREATE TRIGGER scheduler_status_Insert
 	AFTER INSERT ON scheduler_status
 	FOR EACH ROW BEGIN
+		DECLARE f INT UNSIGNED;
 		INSERT INTO `patient_emr` ( module, patient, oid, stamp, summary, user ) VALUES ( 'scheduler_status', NEW.cspatient, NEW.id, NEW.csstamp, NEW.csnote, NEW.csuser );
+		#----- Update deltas if they exist
+		SELECT scheduler_status_last( NEW.csappt, NEW.id ) INTO f;
+		IF f > 0 THEN
+			CALL scheduler_status_record_delta( f, NEW.id );
+		END IF;
 	END;
 //
 
@@ -79,6 +102,49 @@ CREATE TRIGGER scheduler_status_Update
 		UPDATE `patient_emr` SET stamp=NEW.csstamp, patient=NEW.cspatient, summary=NEW.csnote, user=NEW.csuser WHERE module='scheduler_status' AND oid=NEW.id;
 	END;
 //
+
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS scheduler_status_last;
+
+DELIMITER //
+
+CREATE FUNCTION scheduler_status_last ( appt INT UNSIGNED, tid INT UNSIGNED ) RETURNS INT UNSIGNED
+	READS SQL DATA
+BEGIN
+	DECLARE found INT UNSIGNED DEFAULT 0;
+
+	SELECT s.id INTO found FROM scheduler_status s WHERE s.csappt = appt AND s.id <> tid ORDER BY s.id DESC LIMIT 1;
+	RETURN found;
+END//
+
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS scheduler_status_record_delta;
+
+DELIMITER //
+
+CREATE PROCEDURE scheduler_status_record_delta ( IN beginId INT UNSIGNED, IN endId INT UNSIGNED )
+	CONTAINS SQL
+BEGIN
+	DECLARE b_status INT UNSIGNED;
+	DECLARE e_status INT UNSIGNED;
+	DECLARE b_stamp  TIMESTAMP (14);
+	DECLARE e_stamp  TIMESTAMP (14);
+	DECLARE pt       BIGINT UNSIGNED;
+	DECLARE appt     BIGINT UNSIGNED;
+
+	SELECT csstatus INTO b_status FROM scheduler_status WHERE id=beginId;
+	SELECT csstamp INTO b_stamp FROM scheduler_status WHERE id=beginId;
+	SELECT cspatient INTO pt  FROM scheduler_status WHERE id=beginId;
+	SELECT csappt INTO appt FROM scheduler_status WHERE id=beginId;
+	SELECT csstamp INTO e_stamp FROM scheduler_status WHERE id=endId;
+	SELECT csstatus INTO e_status FROM scheduler_status WHERE id=endId;
+
+	IF beginId > 0 THEN
+		INSERT INTO `scheduler_status_delta` ( patient, appointment, st_start, st_end, stamp_start, stamp_end, duration, id_start, id_end ) VALUES ( pt, appt, b_status, e_status, b_stamp, e_stamp, e_stamp - b_stamp, beginId, endId );
+	END IF;
+END//
 
 DELIMITER ;
 
