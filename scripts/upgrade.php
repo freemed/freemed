cb02f56ed814a87528d66897ec123e11fcba7371
jbuchbinder <jeff@ourexchange.net>
2007-06-09 12:20:06 +0000

    Fix #152, rx dates were being imported from 0.8.x iterations as NOW() instead of rxdatefrom.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3403 6507a214-7cff-0310-bd63-e3842a560bc8

--- scripts/upgrade.php
+++ scripts/upgrade.php
@@ -80,7 +80,7 @@ execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, locked
 execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, locked, provider, status ) SELECT 'pnotes', pnotespat, id, pnotesdt, pnotesdescrip, locked, pnotesdoc, 'active' FROM pnotes;" );
 execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, status ) SELECT 'previous_operations', opatient, id, odate, operation, 'active' FROM previous_operations;" );
 execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, status ) SELECT 'procrec', procpatient, id,procdt, CONCAT( c.cptcode, ' - ', c.cptdescrip), 'active' FROM procrec LEFT OUTER JOIN cpt c ON procrec.proccpt=c.id;" );
-execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, locked, status ) SELECT 'rx', rxpatient, id, NOW(), rxdrug, locked, 'active' FROM rx;" );
+execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, locked, status ) SELECT 'rx', rxpatient, id, rxdatefrom, rxdrug, locked, 'active' FROM rx;" );
 execSql( "INSERT INTO patient_emr ( module, patient, oid, stamp, summary, status ) SELECT 'scheduler', calpatient, id, caldateof, calprenote, 'active' FROM scheduler WHERE caltype='pat';" );
 
 printHeader( "Wipe and upgrade ACL tables" );
