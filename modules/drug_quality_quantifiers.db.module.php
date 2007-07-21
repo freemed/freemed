c496622f12b407e46c6f976e3424fa54f1e43177
jbuchbinder <jeff@ourexchange.net>
2007-07-21 13:38:58 +0000

    Add drug form and quantity qualifier tables used by NCPDP SCRIPT standard.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3596 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ modules/drug_quality_quantifiers.db.module.php
@@ -0,0 +1,48 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ // 	Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2007 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+LoadObjectDependency('org.freemedsoftware.core.SupportModule');
+
+class DrugQuantityQualifiers extends SupportModule {
+
+	var $MODULE_NAME = "Drug Quantity Qualifiers";
+	var $MODULE_VERSION = "0.1";
+	var $MODULE_FILE = __FILE__;
+	var $MODULE_UID = "48fc41bf-3812-4adb-ac64-5499256ef90f";
+	var $MODULE_HIDDEN = true;
+
+	var $PACKAGE_MINIMUM_VERSION = '0.8.0';
+
+	var $table_name = "drugquantityqual";
+	var $widget_hash = '##description## (##code##)';
+
+	public function __construct () {
+		// Call parent constructor
+		parent::__construct();
+	} // end constructor
+
+} // end class DrugQuantityQualifiers
+
+register_module("DrugQuantityQualifiers");
+
+?>
