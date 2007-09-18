e416b67fc334c1fdc7636f87fb7c54ffde6a597d
jbuchbinder <jeff@ourexchange.net>
2007-09-18 17:41:12 +0000

    Add data model and module backend for patient linking. #94
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3824 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ data/schema/mysql/patientlinks.sql
@@ -0,0 +1,94 @@
+# $Id$
+#
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
+#
+# FreeMED Electronic Medical Record and Practice Management System
+# Copyright (C) 1999-2007 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+SOURCE data/schema/mysql/patient.sql
+SOURCE data/schema/mysql/patient_emr.sql
+
+CREATE TABLE IF NOT EXISTS `patientlinks` (
+	stamp			TIMESTAMP (14) NOT NULL DEFAULT NOW(),
+	srcpatient		BIGINT (20) NOT NULL DEFAULT 0,
+	destpatient		BIGINT (20) NOT NULL DEFAULT 0,
+	linktype		VARCHAR (50) NOT NULL DEFAULT '',
+	linkdetails		TEXT,
+	user			INT UNSIGNED NOT NULL DEFAULT 0,
+	active			ENUM ( 'active', 'inactive' ) NOT NULL DEFAULT 'active',
+	id			SERIAL
+
+	#	Define keys
+
+	, KEY			( srcpatient, linktype )
+	, KEY			( destpatient, linktype )
+	, FOREIGN KEY		( srcpatient ) REFERENCES patient.id ON DELETE CASCADE
+	, FOREIGN KEY		( destpatient ) REFERENCES patient.id ON DELETE CASCADE
+);
+
+DROP PROCEDURE IF EXISTS patientlinks_Upgrade;
+DELIMITER //
+CREATE PROCEDURE patientlinks_Upgrade ( )
+BEGIN
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+
+	#----- Remove triggers
+	DROP TRIGGER patientlinks_Delete;
+	DROP TRIGGER patientlinks_Insert;
+	DROP TRIGGER patientlinks_Update;
+
+	#----- Upgrades
+	CALL FreeMED_Module_GetVersion( 'patientlinks', @V );
+
+	CALL FreeMED_Module_UpdateVersion( 'patientlinks', 1 );
+END
+//
+DELIMITER ;
+CALL patientlinks_Upgrade( );
+
+#----- Triggers
+
+DELIMITER //
+
+CREATE TRIGGER patientlinks_Delete
+	AFTER DELETE ON patientlinks
+	FOR EACH ROW BEGIN
+		DELETE FROM `patient_emr` WHERE module='patientlinks' AND oid=OLD.id;
+	END;
+//
+
+CREATE TRIGGER patientlinks_Insert
+	AFTER INSERT ON patientlinks
+	FOR EACH ROW BEGIN
+		DECLARE dP VARCHAR(250);
+		SELECT CONCAT( ptlname, ', ', ptfname, ' ', ptmname, ' (', ptid, ')' ) INTO dP FROM patient WHERE id=NEW.destpatient;
+		INSERT INTO `patient_emr` ( module, patient, oid, stamp, summary, user, status ) VALUES ( 'patientlinks', NEW.srcpatient, NEW.id, NEW.stamp, CONCAT( NEW.linktype, ' ', dP ), NEW.user, NEW.active );
+	END;
+//
+
+CREATE TRIGGER patientlinks_Update
+	AFTER UPDATE ON patientlinks
+	FOR EACH ROW BEGIN
+		DECLARE dP VARCHAR(250);
+		SELECT CONCAT( ptlname, ', ', ptfname, ' ', ptmname, ' (', ptid, ')' ) INTO dP FROM patient WHERE id=NEW.destpatient;
+		UPDATE `patient_emr` SET stamp=NEW.stamp, patient=NEW.srcpatient, summary=CONCAT( NEW.linktype, ' ', dP), user=NEW.user, status=NEW.active WHERE module='patientlinks' AND oid=NEW.id;
+	END;
+//
+
+DELIMITER ;
+
---
+++ modules/patient_link.emr.module.php
@@ -0,0 +1,81 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ // 	Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2007 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+LoadObjectDependency('org.freemedsoftware.core.SupportModule');
+
+class PatientLink extends SupportModule {
+
+	var $MODULE_NAME = "Patient Tag";
+	var $MODULE_VERSION = "0.1";
+	var $MODULE_FILE = __FILE__;
+	var $MODULE_UID = "28e6a139-9d8d-4532-b054-43fe9d0aae01";
+	var $MODULE_HIDDEN = true;
+
+	var $PACKAGE_MINIMUM_VERSION = '0.8.0';
+
+	var $record_name = "Patient Link";
+	var $table_name  = "patientlinks";
+	var $order_field = "stamp";
+	var $patient_field = "srcpatient";
+
+	//var $widget_hash = "##tag## (##datecreate## - ##dateexpire##)";
+	var $widget_hash = "##linktype## ##linkdetails##";
+
+	public function __construct ( ) {
+		// __("Patient Link")
+	
+		$this->list_view = array (
+			__("Date") => 'stamp',
+			__("Link Type") => 'linktype',
+			__("Link Details") => 'linkdetails'
+		);
+
+		$this->variables = array (
+			"srcpatient",
+			"destpatient",
+			"linktype",
+			"linkdetails",
+			"user"
+		);
+
+		$this->_SetAssociation('EmrModule');
+
+		// Run parent constructor
+		parent::__construct();
+	} // end constructor
+
+	protected function add_pre ( &$data ) {
+		unset( $date['stamp'] );
+		$data['user'] = freemed::user_cache()->user_number;
+	}
+
+	protected function mod_pre ( &$data ) {
+		unset( $date['stamp'] );
+		$data['user'] = freemed::user_cache()->user_number;
+	}
+
+} // end class PatientLink
+
+register_module ("PatientLink");
+
+?>
