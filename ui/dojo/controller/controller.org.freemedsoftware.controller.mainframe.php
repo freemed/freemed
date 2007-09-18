2c35666474575391e189cdfac06e2feff59c014b
jbuchbinder <jeff@ourexchange.net>
2007-09-18 18:34:31 +0000

    Small comment beautification.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3827 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/dojo/controller/controller.org.freemedsoftware.controller.mainframe.php
+++ ui/dojo/controller/controller.org.freemedsoftware.controller.mainframe.php
@@ -20,7 +20,6 @@
  // You should have received a copy of the GNU General Public License
  // along with this program; if not, write to the Free Software
  // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-	// jeff@freemedsoftware.org
 
 include_once (dirname(__FILE__).'/../class.Controller.php');
 
