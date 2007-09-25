11df4ff4ce0e16f87930ac1f496e27e28552461e
jbuchbinder <jeff@ourexchange.net>
2007-09-25 18:09:14 +0000

    Add Patient List report.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@3872 6507a214-7cff-0310-bd63-e3842a560bc8

--- data/schema/mysql/reporting.sql
+++ data/schema/mysql/reporting.sql
@@ -63,5 +63,6 @@ SOURCE data/schema/mysql/reporting/report_OutstandingPatientAccounts.sql
 SOURCE data/schema/mysql/reporting/report_OutstandingPatientAccountsByProvider.sql
 SOURCE data/schema/mysql/reporting/report_PatientAccountActivity.sql
 SOURCE data/schema/mysql/reporting/report_PatientAgingReport.sql
+SOURCE data/schema/mysql/reporting/report_PatientList.sql
 SOURCE data/schema/mysql/reporting/report_PatientZipCodeDistribution.sql
 
---
+++ data/schema/mysql/reporting/report_PatientList.sql
@@ -0,0 +1,64 @@
+# $Id$
+#
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
+#
+# FreeMED Electronic Medical Record and Practice Management System
+# Copyright (C) 1999-2007 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+# File: Report - Patient List
+
+DROP PROCEDURE IF EXISTS report_PatientList_en_US ;
+DELIMITER //
+
+# Function: report_PatientList_en_US
+#
+#	List of patients in the system.
+#
+CREATE PROCEDURE report_PatientList_en_US ( )
+BEGIN
+	SET @sql = CONCAT(
+		"SELECT ptlname AS 'Last Name', ptfname AS 'First Name', ptmname AS 'MI', ptaddr1 AS 'Address', CONCAT(ptcity, ', ', ptstate,' ', ptzip) AS 'City/State/Zip', ptid AS 'Patient ID', ptdob AS 'DOB' FROM patient WHERE ptarchive=0 ORDER BY ptlname, ptfname, ptmname, ptdob"
+	) ;
+
+	PREPARE s FROM @sql ;
+	EXECUTE s ;
+	DEALLOCATE PREPARE s ;
+END //
+
+DELIMITER ;
+
+#	Add indices
+
+DELETE FROM `reporting` WHERE report_sp = 'report_PatientList_en_US';
+
+INSERT INTO `reporting` (
+		report_name,
+		report_uuid,
+		report_locale,
+		report_desc,
+		report_sp,
+		report_param_count
+	) VALUES (
+		'Patient List',
+		'771722fb-914c-44b1-a8da-3f22299905cf',
+		'en_US',
+		'List of patients in the system.',
+		'report_PatientList_en_US',
+		0
+	);
+
