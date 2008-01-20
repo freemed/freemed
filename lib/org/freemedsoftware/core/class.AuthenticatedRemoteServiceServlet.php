054be9326dadf5bd80f92462ddef97cb0ce6066f
jbuchbinder <jeff@ourexchange.net>
2008-01-20 21:46:53 +0000

    Implemented AuthenticatedRemoteServiceServlet for allowing authenticated access via GWTRPC relay.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4068 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/org/freemedsoftware/core/class.AuthenticatedRemoteServiceServlet.php
@@ -0,0 +1,62 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2008 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+require_once(GWTPHP_DIR.'/RemoteServiceServlet.class.php');
+require_once(GWTPHP_DIR.'/exceptions/AuthenticationException.class.php');
+
+// Class: org.freemedsoftware.core.AuthenticatedRemoteServiceServlet
+//
+//	Class to extend GWTPHP's RemoteServiceServlet to handle FreeMED's
+//	authentication scheme
+//
+
+class AuthenticatedRemoteServiceServlet extends RemoteServiceServlet {
+
+    /**
+     * Override this method to examine the decoded request that will be
+     * processing by RemoteServiceServlet. The default implementation does nothing and need
+     * not be called by subclasses.
+     * FOCUS: this method is used only in PHP implementation of GWT RemoteServiceServlet
+     */
+    protected function onAfterRequestDecoded(RPCReqest $rpcRequest) {
+        if ( ! $this->checkAuthenticationPolicy( $rpcRequest->getMethod() ) ) {
+            $ex = new AuthenticationException( );
+            throw new AuthenticationException( );
+        }
+    }
+
+    protected function checkAuthenticationPolicy( string $methodName ) {
+        // Return false if attempting to access core namespace
+        if ( strpos( $methodName, 'org.freemedsoftware.core.' ) !== false ) {
+            return false;
+        }
+
+        // FIXME: Check authentication here
+
+        // Default to returning "true"
+        return true;
+    } // end method checkAuthenticationPolicy
+
+}
+
+?>
