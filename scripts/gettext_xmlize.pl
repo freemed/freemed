8b499399ab506d043fa645926f7c47d1c86675ca
jbuchbinder <jeff@ourexchange.net>
2008-03-09 10:54:14 +0000

    Update xml/dexml-ize scripts to have proper comments. Added Phil's patch to allow passing of locale info to xmlize.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/branches/0.8.x@4137 6507a214-7cff-0310-bd63-e3842a560bc8

--- scripts/gettext_xmlize.pl
+++ scripts/gettext_xmlize.pl
@@ -1,8 +1,25 @@
 #!/usr/bin/perl
+# $Id$
 #
-#	$Id$
-#	$Author$
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
 #
+# FreeMED Electronic Medical Record and Practice Management System
+# Copyright (C) 1999-2008 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 # Auto-detect the path for libraries and the FreeMED install
 use FindBin;
@@ -22,6 +39,11 @@ my @buf;
 print "<?xml version=\"1.0\"?>\n";
 print "<gettextXML>\n";
 print "<information>\n";
+
+if ($ARGV[0] && $ARGV[1]) {
+	print "<Locale>" . $ARGV[0] ."</Locale>\n";
+	print "<LocaleName>" . $ARGV[1] ."</LocaleName>\n";
+} 
 
 while (<INPUT>) {
 	my $line = $_;
