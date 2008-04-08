32d26ba622f16e4e5b16069506ca084c5ec55d78
jbuchbinder <jeff@ourexchange.net>
2008-04-08 22:49:17 +0000

    Update with corrections for type problems for WIP widget.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4156 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/gwt/src/org/freemedsoftware/gwt/client/widget/PatientWidget.java
+++ ui/gwt/src/org/freemedsoftware/gwt/client/widget/PatientWidget.java
@@ -29,11 +29,13 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.SuggestOracle;
 import com.google.gwt.user.client.ui.Widget;
-import java.util.HashMap;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import java.util.*;
+import java.lang.StringBuffer;
 
 public class PatientWidget extends Composite {
 
-	protected Integer value = 0;
+	protected int value = 0;
     /**
      * @gwt.typeArgs <java.lang.String, java.lang.String>
      */
@@ -42,6 +44,79 @@ public class PatientWidget extends Composite {
 	public PatientWidget() {
 
 		final SuggestOracle suggestOracle = new SuggestOracle() {
+			ArrayList items = new ArrayList();
+
+		     final class StartsWithSuggestion implements Suggestion, IsSerializable {
+		             private String _value;
+		             private String _displayString;
+
+		             /**
+		              * Constructor used by RPC.
+		              */
+		             public StartsWithSuggestion() { }
+
+		             /**
+		              * Constructor for <code>StartsWithSuggestion</code>.
+		              */
+		             public StartsWithSuggestion(String value, String displayString) {
+		                 _value = value;
+		                 _displayString = displayString;
+		             }
+
+		             public String getDisplayString() {
+		                 return _displayString;
+		             }
+
+		             public String getReplacementString() {
+		            	 return _displayString;
+		             }
+		             
+		             public Object getValue() {
+		                 return _value;
+		             }
+		         } 
+			
+		    private StartsWithSuggestion getFormattedSuggestion(String query,String suggestion) {
+		    	StringBuffer formattedSuggestion = new StringBuffer()
+		    		            .append("<strong>")
+		    		            .append(suggestion.substring(0, query.length()))
+		    		            .append("</strong>")
+		    		            .append(suggestion.substring(query.length()));
+		    	return new StartsWithSuggestion(suggestion, formattedSuggestion.toString());
+		    }
+			
+			/**
+			 * 
+			 * @param query
+			 * @param limit
+			 * @return
+			 * @gwt.typeArgs <java.lang.String>
+			 */
+		    private List getItems(String query, int limit)
+		    {
+		    	/**
+		    	 * @gwt.typeArgs <java.lang.String>
+		    	 */
+		        ArrayList/*<StartsWithSuggestion>*/ matches = new ArrayList();
+		        for (int i = 0; i < items.size() && matches.size() < limit; i++) {
+		            if (query.matches(((String) items.get(i)).substring(0, query.length()).toLowerCase())) {
+		                matches.add(getFormattedSuggestion(query, (String) items.get(i)));
+		            }
+		        }
+		        //Log.debug("found " + matches.size() + " matches for query " + query);
+		        return matches;
+		    }
+
+
+			public void requestSuggestions(SuggestOracle.Request request, SuggestOracle.Callback callback) {
+				/**
+				 * @gwt.typeArgs <StartsWithSuggestion>
+				 */
+		        final List/*<StartsWithSuggestion>*/ suggestions =
+		        	getItems(request.getQuery().toLowerCase(), request.getLimit());
+		        	        Response response = new Response(suggestions);
+		        	        callback.onSuggestionsReady(request, response); 
+			}
 			
 		};
 		final SuggestBox suggestBox = new SuggestBox(suggestOracle);
@@ -56,9 +131,8 @@ public class PatientWidget extends Composite {
 	 * Get integer value of currently selected patient.
 	 * @return Current selected patient value
 	 */
-	public Integer getValue() {
-		return value;	
-	}
+	public int getValue() {
+		return value;
 	}
 	
 }
