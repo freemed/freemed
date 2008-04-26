a1293d4f9d537c9acc28b51bc30f4da3385fc43d
jbuchbinder <jeff@ourexchange.net>
2008-04-26 21:47:27 +0000

    Update with copyright notice and move to ScreenInterface from Composite.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4196 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/gwt/src/org/freemedsoftware/gwt/client/screen/DocumentManager.java
+++ ui/gwt/src/org/freemedsoftware/gwt/client/screen/DocumentManager.java
@@ -1,17 +1,37 @@
-package org.freemedsoftware.gwt.client.widget;
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Jeff Buchbinder <jeff@freemedsoftware.org>
+ *      Jeremy Allen <ieziar.jeremy <--at--> gmail.com>
+ *
+ * FreeMED Electronic Medical Record and Practice Management System
+ * Copyright (C) 1999-2008 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.freemedsoftware.gwt.client.screen;
+
+import org.freemedsoftware.gwt.client.ScreenInterface;
 
 import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.CheckBox;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class DocumentManager extends Composite {
+public class DocumentManager extends ScreenInterface {
 
 	public DocumentManager() {
 
