1fab9eeb3fba1876ac1155e33600322b3141ce9c
jbuchbinder <jeff@ourexchange.net>
2008-05-02 14:15:40 +0000

    Add ModuleInterface RPC interface and async class.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4212 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ ui/gwt/src/org/freemedsoftware/gwt/client/Api/ModuleInterfaceAsync.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Jeff Buchbinder <jeff@freemedsoftware.org>
+ *
+ * FreeMED Electronic Medical Record and Practice Management System
+ * Copyright (C) 1999-2008 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.freemedsoftware.gwt.client.Api;
+
+import java.util.HashMap;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface ModuleInterfaceAsync {
+	/**
+	 * 
+	 * @param data
+	 */
+	public void ModuleAddMethod(String module, HashMap data, AsyncCallback callback);
+
+	public void ModuleDeleteMethod(String module, Integer id, AsyncCallback callback);
+
+	/**
+	 * 
+	 * @param data
+	 */
+	public void ModuleModifyMethod(String module, HashMap data, AsyncCallback callback);
+
+}
