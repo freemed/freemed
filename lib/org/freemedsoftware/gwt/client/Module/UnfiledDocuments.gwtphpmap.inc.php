29b5355bcad1e9ef234c7409cc91c27a96b9216f
jbuchbinder <jeff@ourexchange.net>
2008-06-23 14:48:03 +0000

    Add UnfiledDocuments RPC interface.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4245 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ lib/org/freemedsoftware/gwt/client/Module/UnfiledDocuments.gwtphpmap.inc.php
@@ -0,0 +1,129 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ // 	Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2008 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+$gwtphpmap = array (
+	array (
+		  'className' => 'org.freemedsoftware.gwt.client.Module.UnfiledDocuments'
+		, 'mappedBy' => 'org.freemedsoftware.module.UnfiledDocuments'
+		, 'methods' => array (
+
+			// Method: NumberOfPages
+			//
+			//	Expose the number of pages of a Djvu document
+			//
+			// Parameters:
+			//
+			//	$id - Table record id
+			//
+			// Returns:
+			//
+			//	Integer, number of pages in the specified document
+			//
+			  array (
+				  'name' => 'NumberOfPages'
+				, 'mappedName' => 'NumberOfPages'
+				, 'returnType' => 'java.lang.Integer'
+				, 'params' => array (
+					  array ( 'type' => 'java.lang.Integer' )
+				)
+				, 'throws' => array ( )
+			)
+
+			// Method: batchSplit
+			//
+			//	Split multiple faxed documents.
+			//
+			// Parameters:
+			//
+			//	$id - Record id
+			//
+			//	$splitafter - Array of "splits"
+			//
+			, array (
+				  'name' => 'BatchSplit'
+				, 'mappedName' => 'BatchSplit'
+				, 'returnType' => TypeSignatures::$NULL
+				, 'params' => array (
+					  array ( 'type' => 'java.lang.Integer' )
+					, array ( 'type' => '[Ljava.lang.Integer;' )
+				)
+				, 'throws' => array ( )
+			)
+
+			// Method: GetAll
+			//
+			//	Get all records.
+			//
+			// Returns:
+			//
+			//	Array of hashes.
+			//
+			, array (
+				  'name' => 'GetAll'
+				, 'mappedName' => 'GetAll'
+				, 'returnType' => '[Ljava.util.HashMap;'
+				, 'returnTypeCRC' => '3558356060[L962170901;'
+				, 'params' => array ( )
+				, 'throws' => array ( )
+			)
+
+			// Method: Faxback
+			//
+			// Parameters:
+			//
+			//	$id - Record id
+			//
+			//	$faxback - Fax number to send faxback to
+			//
+			, array (
+				  'name' => 'Faxback'
+				, 'mappedName' => 'faxback'
+				, 'returnType' => TypeSignatures::$NULL
+				, 'params' => array (
+					  array ( 'type' => 'java.lang.Integer' )
+					, array ( 'type' => 'java.lang.String' )
+				)
+				, 'throws' => array ( )
+			)
+
+			// Method: GetCount
+			//
+			//	Retrieve number of unfiled documents in the system.
+			//
+			// Returns:
+			//
+			//	Current number of unfiled documents in the system.
+			//
+			, array (
+				  'name' => 'GetCount'
+				, 'mappedName' => 'GetCount'
+				, 'returnType' => 'java.lang.Integer'
+				, 'params' => array ( )
+				, 'throws' => array ( )
+			)
+
+		)
+	)
+);
+
+?>
---
+++ ui/gwt/src/org/freemedsoftware/gwt/client/Module/UnfiledDocuments.java
@@ -0,0 +1,45 @@
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Jeff Buchbinder <jeff@freemedsoftware.org>
+ *
+ * FreeMED Electronic Medical Record and Practice Management System
+ * Copyright (C) 1999-2008 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.freemedsoftware.gwt.client.Module;
+
+import java.util.HashMap;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+public interface UnfiledDocuments extends RemoteService {
+	public Integer NumberOfPages(Integer id);
+
+	public void BatchSplit(Integer id, Integer[] splitAfter);
+
+	/**
+	 * @gwt.typeArgs <java.lang.String,java.lang.String>
+	 * @return
+	 */
+	public HashMap[] GetAll();
+
+	public void Faxback(Integer id, String faxback);
+
+	public Integer GetCount();
+}
