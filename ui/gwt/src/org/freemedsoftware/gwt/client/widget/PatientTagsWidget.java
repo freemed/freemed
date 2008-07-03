1406880e869922d34c611cb39724dd598ca9cf50
jbuchbinder <jeff@ourexchange.net>
2008-07-03 18:39:03 +0000

    Add RPC functionality to tags widget.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4252 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/gwt/src/org/freemedsoftware/gwt/client/widget/PatientTagsWidget.java
+++ ui/gwt/src/org/freemedsoftware/gwt/client/widget/PatientTagsWidget.java
@@ -26,6 +26,11 @@ package org.freemedsoftware.gwt.client.widget;
 
 import java.util.ArrayList;
 
+import org.freemedsoftware.gwt.client.Util;
+import org.freemedsoftware.gwt.client.Module.PatientTagAsync;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.ChangeListener;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Composite;
@@ -39,23 +44,24 @@ import com.google.gwt.user.client.ui.Widget;
 public class PatientTagsWidget extends Composite {
 
 	protected Integer patientId = new Integer(0);
-	
+
 	/**
 	 * @gwt.typeArgs <java.lang.String>
 	 */
 	protected ArrayList tags = null;
-	
+
 	protected final FlowPanel flowPanel;
+
 	protected final TextBox wEntry;
-	
-	public PatientTagsWidget( ) {
+
+	public PatientTagsWidget() {
 		flowPanel = new FlowPanel();
 		initWidget(flowPanel);
 
 		wEntry = new TextBox();
 		flowPanel.add(wEntry);
 		wEntry.addChangeListener(new ChangeListener() {
-			public void onChange( Widget w ) {
+			public void onChange(Widget w) {
 				TextBox t = (TextBox) w;
 				if (t.getText().length() > 2) {
 					addTag(t.getText());
@@ -63,22 +69,36 @@ public class PatientTagsWidget extends Composite {
 			}
 		});
 	}
-	
+
 	/**
 	 * Update database to set new tag.
 	 * 
 	 * @param tag
 	 */
-	public void addTag( String tag ) {
-		wEntry.setText("");		
+	public void addTag(final String tag) {
+		if (Util.isStubbedMode()) {
+			wEntry.setText("");
+			addTagToDisplay(tag);
+		} else {
+			getProxy().CreateTag(patientId, tag, new AsyncCallback() {
+				public void onSuccess(Object o) {
+					wEntry.setText("");
+					addTagToDisplay(tag);
+				}
+
+				public void onFailure(Throwable t) {
+					GWT.log("Exception", t);
+				}
+			});
+		}
 	}
-	
+
 	/**
 	 * Actual addition of tag to display.
 	 * 
 	 * @param tag
 	 */
-	protected void addTagToDisplay( String tag ) {
+	protected void addTagToDisplay(String tag) {
 		HorizontalPanel p = new HorizontalPanel();
 		p.setTitle(tag);
 		HTML r = new HTML("<sup>X</sup>");
@@ -86,31 +106,75 @@ public class PatientTagsWidget extends Composite {
 		p.add(r);
 		p.setStyleName("freemed-PatientTag");
 		r.addClickListener(new ClickListener() {
-			public void onClick( Widget w ) {
+			public void onClick(Widget w) {
 				HorizontalPanel container = (HorizontalPanel) w.getParent();
-				removeTag(container.getTitle());
-				container.removeFromParent();
+				removeTag(container.getTitle(), container);
 			}
 		});
 		flowPanel.add(p);
 	}
+
 	/**
 	 * Update database to remove tag.
 	 * 
 	 * @param tag
 	 */
-	public void removeTag( String tag ) {
+	public void removeTag(String tag, final HorizontalPanel hp) {
+		if (Util.isStubbedMode()) {
+			hp.removeFromParent();
+		} else {
+			getProxy().ExpireTag(patientId, tag, new AsyncCallback() {
+				public void onSuccess(Object o) {
+					hp.removeFromParent();
+				}
+
+				public void onFailure(Throwable t) {
+					GWT.log("Exception", t);
+				}
+			});
+		}
 	}
-	
-	
+
 	/**
 	 * Set internal patient id representation.
 	 * 
 	 * @param patient
 	 */
-	public void setPatient( Integer patient ) {
+	public void setPatient(Integer patient) {
 		patientId = patient;
+		populate();
 	}
-	
-}
 
+	protected void populate() {
+		if (Util.isStubbedMode()) {
+			addTagToDisplay("testTag1");
+			addTagToDisplay("Diabetes");
+			addTagToDisplay("LatePayment");
+		} else {
+			getProxy().TagsForPatient(patientId, new AsyncCallback() {
+				public void onSuccess(Object o) {
+					String[] tags = (String[]) o;
+					for (int iter = 0; iter < tags.length; iter++) {
+						addTagToDisplay(tags[iter]);
+					}
+				}
+
+				public void onFailure(Throwable t) {
+					GWT.log("Exception", t);
+				}
+			});
+		}
+	}
+
+	protected PatientTagAsync getProxy() {
+		PatientTagAsync p = null;
+		try {
+			p = (PatientTagAsync) Util
+					.getProxy("org.freemedsoftware.gwt.client.Module.PatientTag");
+		} catch (Exception ex) {
+			GWT.log("Exception", ex);
+		}
+		return p;
+	}
+
+}
