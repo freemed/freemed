e3ea1884124718c029967835f66a7597b890091a
jbuchbinder <jeff@ourexchange.net>
2008-09-14 00:14:21 +0000

    Add copyright notice (left out accidentally). Add additional convenience methods, should be ready for initial tests.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4323 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/gwt/src/org/freemedsoftware/gwt/client/widget/SimpleUIBuilder.java
+++ ui/gwt/src/org/freemedsoftware/gwt/client/widget/SimpleUIBuilder.java
@@ -1,6 +1,31 @@
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Jeff Buchbinder <jeff@freemedsoftware.org>
+ *
+ * FreeMED Electronic Medical Record and Practice Management System
+ * Copyright (C) 1999-2008 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 package org.freemedsoftware.gwt.client.widget;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
@@ -78,6 +103,28 @@ public class SimpleUIBuilder extends Composite {
 			if (value != null) {
 				((TextBox) w).setText(value);
 			}
+		} else if (type.compareToIgnoreCase("module") == 0) {
+			w = new SupportModuleWidget(options);
+			if (value != null) {
+				((SupportModuleWidget) w).setValue(new Integer(value));
+			}
+		} else if (type.compareToIgnoreCase("select") == 0) {
+			w = new CustomListBox();
+
+			// Push in all "options" values
+			String[] o = options.split(",");
+			for (int iter = 0; iter < o.length; iter++) {
+				// Check for "description" pairs
+				if (o[iter].contains("|")) {
+					String[] i = o[iter].split("|");
+					((CustomListBox) w).addItem(i[0], i[1]);
+				}
+				((CustomListBox) w).addItem(o[iter]);
+			}
+
+			if (value != null) {
+				((CustomListBox) w).setWidgetValue(value);
+			}
 		} else if (type.compareToIgnoreCase("patient") == 0) {
 			w = new PatientWidget();
 			if (value != null) {
@@ -95,6 +142,46 @@ public class SimpleUIBuilder extends Composite {
 		widgets.put(name, w);
 		table.setText(widgets.size() - 1, 0, title);
 		table.setWidget(widgets.size() - 1, 1, w);
+	}
+
+	/**
+	 * Form HashMap containing all values contained in this widget.
+	 * 
+	 * @return
+	 */
+	public HashMap<String, String> getValues() {
+		HashMap<String, String> c = new HashMap<String, String>();
+		Iterator<String> iter = widgets.keySet().iterator();
+		while (iter.hasNext()) {
+			String k = iter.next();
+			c.put(k, getWidgetValue(k));
+		}
+		return c;
+	}
+
+	/**
+	 * Convenience method for extracting value from a contained widget given the
+	 * widget's name in the widgets hashmap.
+	 * 
+	 * @param name
+	 *            "name" key in the widgets hashmap
+	 * @return Value of the specified widget, or null if none is found.
+	 */
+	protected String getWidgetValue(String name) {
+		Widget w = widgets.get(name);
+		if (w instanceof TextBox) {
+			return ((TextBox) w).getText();
+		}
+		if (w instanceof CustomListBox) {
+			return ((CustomListBox) w).getWidgetValue();
+		}
+		if (w instanceof SupportModuleWidget) {
+			return ((SupportModuleWidget) w).getValue().toString();
+		}
+		if (w instanceof PatientWidget) {
+			return ((PatientWidget) w).getValue().toString();
+		}
+		return null;
 	}
 
 }
