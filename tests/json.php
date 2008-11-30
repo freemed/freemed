db3e3a54c9f1c70a64ad0fc22ad01a87c0bd2eb8
jbuchbinder <jeff@ourexchange.net>
2008-11-30 13:49:38 +0000

    Add additional test scripts.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4401 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ tests/json.php
@@ -0,0 +1,30 @@
+<?php
+ // $Id$
+ //
+ // Authors:
+ //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //
+ // FreeMED Electronic Medical Record and Practice Management System
+ // Copyright (C) 1999-2008 FreeMED Software Foundation
+ //
+ // This program is free software; you can redistribute it and/or modify
+ // it under the terms of the GNU General Public License as published by
+ // the Free Software Foundation; either version 2 of the License, or
+ // (at your option) any later version.
+ //
+ // This program is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ // GNU General Public License for more details.
+ //
+ // You should have received a copy of the GNU General Public License
+ // along with this program; if not, write to the Free Software
+ // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+include_once( dirname(__FILE__).'/bootstrap.test.php' );
+
+t ( 'json_encode("test")', json_encode("test") );
+t ( 'json_encode(array("test","test2"))', json_encode(array("test", "test2")) );
+t ( 'json_encode(array("test" => 1,"test2" => "a"))', json_encode(array("test" => 1, "test2" => "a")) );
+
+?>
