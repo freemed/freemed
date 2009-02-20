e98abbba05d288992cf3f2c04d49af436581ffea
jbuchbinder <jeff@ourexchange.net>
2009-02-20 15:11:10 +0000

    CLI installer changes from Phil.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4470 6507a214-7cff-0310-bd63-e3842a560bc8

--- scripts/install.php
+++ scripts/install.php
@@ -4,6 +4,7 @@
  //
  // Authors:
  //      Jeff Buchbinder <jeff@freemedsoftware.org>
+ //      Philipp Meng <pmeng@freemedsoftware.org>
  //
  // FreeMED Electronic Medical Record and Practice Management System
  // Copyright (C) 1999-2009 FreeMED Software Foundation
@@ -25,22 +26,56 @@
 if (!$_SERVER['argc']) { die ("cannot be called via web"); }
 
 ini_set('include_path', dirname(dirname(__FILE__)).':'.ini_get('include_path'));
-include_once ( 'lib/freemed.php' );
+
+define ('CLI_INSTALL', true );
+define ('SKIP_SQL_INIT', true );
 
 print "FreeMED CLI Installation Tool\n";
-print "(c) 2007 FreeMED Software Foundation\n\n";
-
-function getInput ( $mask ) { fscanf(STDIN, "${mask}\n", $x); return $x; }
-function execSql  ( $s    ) { print " - Executing \"$s\" : "; $GLOBALS['sql']->query( $s ); print " ... [done]\n"; }
-function printHeader ( $x ) { print "\n\n ----->> ${x} <<-----\n\n"; }
-function loadSchema ( $s ) { $c="./scripts/load_schema.sh 'mysql' '${s}' '".DB_USER."' '".DB_PASSWORD."' '".DB_NAME."'"; print `$c`; print "\n\n"; }
+print "(c) 2009 FreeMED Software Foundation\n\n";
 
 if ( ! file_exists ( './scripts/install.php' ) ) {
 	print "You must run this from the root directory of your FreeMED install.\n\n";
 	die();
 }
 
-print "Please type 'yes' if you're *sure* you want to do this : ";
+if ( ! file_exists ( 'lib/settings.php' ) ) {
+	print "Please create the file lib/settings.php form the template file lib/settings.php.tpl and enter the correct values.\nWhen finished, please execute this script again.\nABORTED.\n\n";
+	die();
+}
+
+function getInput ( $mask ) { fscanf(STDIN, "${mask}\n", $x); return $x; }
+
+//Check for MySQL-Version and inform the user about the necessarity of SUPER-rights for MySQL
+//When the user likes, he can enter his root-user/pass to override declaration in lib/settings.php
+$output = shell_exec('mysql -V');
+preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
+if ($version[0] >= "5.0.4") {
+print "You have MySQL Version ".$version[0]." installed. There are problems with creating triggers on the Tables with non-root privleges (creating triggers requires SUPER privileges). Does the username/password specified in lib/settings.php have SUPER privileges? (yes/no?  Default = no) ";
+if (getInput('%s') != 'yes') {
+	print "\n\nWithout the correct rights, this script won't work. You can enter the username and password of a user with SUPER privileges here. These settings will not be saved, they are only used by this script. The settings in lib/settings.php are kept and used by all transactions of FreeMED in the future. Do you want to do that? (yes/no  Default = no) ";
+	if (getInput('%s') != 'yes') {
+		print "Installation interrupted by user.\n";			
+		die();
+	}
+	print "MySQL Username: ";
+	$username = getInput('%s');
+	print "\n";
+	print "MySQL Password: ";
+	$password = getInput('%s');
+	print "\n\n";
+	//Define the DB-User and Password here to override settings in lib/settings.php	
+	define ('DB_USER', $username); // SQL server username
+	define ('DB_PASSWORD', $password); // SQL server password
+}
+}
+
+include_once ( 'lib/freemed.php' );
+
+function execSql  ( $s    ) { print " - Executing \"$s\" : "; $GLOBALS['sql']->query( $s ); print " ... [done]\n"; }
+function printHeader ( $x ) { print "\n\n ----->> ${x} <<-----\n\n"; }
+function loadSchema ( $s ) { $c="./scripts/load_schema.sh 'mysql' '${s}' '".DB_USER."' '".DB_PASSWORD."' '".DB_NAME."'"; print `$c`; print "\n\n"; }
+
+print "\nPlease type 'yes' if you're *sure* you want to do this : ";
 if ( getInput( '%s' ) != 'yes' ) {
 	print "\nI didn't think so. :(\n";
 	die();
@@ -74,7 +109,7 @@ loadSchema( 'scheduler' );
 
 printHeader( "Install ACL tables" );
 loadSchema( 'acl' );
-include_once( dirname(__FILE__).'/../modules/acl.module.php' );
+include_once( dirname(__FILE__).'/../lib/org/freemedsoftware/module/ACL.class.php' );
 $a = new ACL();
 $q = "SELECT username, id FROM user WHERE id > 0";
 $r = $GLOBALS['sql']->queryAll( $q );
@@ -91,5 +126,7 @@ $modules = CreateObject( 'org.freemedsoftware.core.ModuleIndex', true, false );
 
 printHeader( "Force module definition upgrades (round 2)" );
 $modules = CreateObject( 'org.freemedsoftware.core.ModuleIndex', true, false );
+
+print "\n\nfinished.\n";
 
 ?>
