2205dd7460e4c25d1cbeb2dcfe08136c2a9cf8fe
pmeng <pmeng@6507a214-7cff-0310-bd63-e3842a560bc8>
2009-03-25 20:53:06 +0000

    Updated Copyright Notice.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4516 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/gwt/src/org/freemedsoftware/gwt/client/widget/NoInsertAtEndIndexedDropController.java
+++ ui/gwt/src/org/freemedsoftware/gwt/client/widget/NoInsertAtEndIndexedDropController.java
@@ -1,19 +1,30 @@
 /*
  * $Id$
  * 
- * Copyright 2008 Fred Sauer
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ *
+ * Authors:
+ * 			Fred Sauer
+ *
+ * FreeMED Electronic Medical Record and Practice Management System
+ * Copyright (C) 1999-2009 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * The code in this file has originally been written by Mr. Fred Sauer.
+ * We, the FreeMED Software Foundation, thank him a lot for the right to use his code!
+ *
  */
 package org.freemedsoftware.gwt.client.widget;
 
