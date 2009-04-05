beac70bd83b8a432f19c90b705335c12af897f25
pmeng <pmeng@6507a214-7cff-0310-bd63-e3842a560bc8>
2009-04-05 16:26:08 +0000

    Little Enhancements to the Prescriptions Screen. But it is still broken due to Flyspray Bug #22
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4548 6507a214-7cff-0310-bd63-e3842a560bc8

--- ui/gwt/src/org/freemedsoftware/gwt/client/screen/patient/PrescriptionsScreen.java
+++ ui/gwt/src/org/freemedsoftware/gwt/client/screen/patient/PrescriptionsScreen.java
@@ -24,19 +24,38 @@
 
 package org.freemedsoftware.gwt.client.screen.patient;
 
-import org.freemedsoftware.gwt.client.PatientScreenInterface;
-import org.freemedsoftware.gwt.client.widget.SupportModuleWidget;
+import java.util.HashMap;
 
+import org.freemedsoftware.gwt.client.JsonUtil;
+import org.freemedsoftware.gwt.client.PatientScreenInterface;
+import org.freemedsoftware.gwt.client.Util;
+import org.freemedsoftware.gwt.client.Util.ProgramMode;
+import org.freemedsoftware.gwt.client.widget.SupportModuleWidget;
+import org.freemedsoftware.gwt.client.widget.Toaster;
+
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.i18n.client.HasDirection.Direction;
+import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
+import eu.future.earth.gwt.client.DateEditFieldWithPicker;
+
 public class PrescriptionsScreen extends PatientScreenInterface {
 
+	final SupportModuleWidget wProvider = new SupportModuleWidget("ProviderModule");
+	final SupportModuleWidget wDrug = new SupportModuleWidget("MultumDrugLexicon");
+	final SupportModuleWidget wQuantity = new SupportModuleWidget("DrugQuantityQualifiers");
 	public PrescriptionsScreen() {
 
 		final FlexTable flexTable = new FlexTable();
@@ -45,7 +64,7 @@ public class PrescriptionsScreen extends PatientScreenInterface {
 		final Label dateLabel = new Label("Starting Date");
 		flexTable.setWidget(0, 0, dateLabel);
 		dateLabel.setDirection(Direction.RTL);
-
+		
 		final Label providerLabel = new Label("Provider");
 		flexTable.setWidget(1, 0, providerLabel);
 		providerLabel.setDirection(Direction.RTL);
@@ -86,35 +105,33 @@ public class PrescriptionsScreen extends PatientScreenInterface {
 		flexTable.setWidget(10, 0, noteLabel);
 		noteLabel.setDirection(Direction.RTL);
 
-		final SupportModuleWidget wProvider = new SupportModuleWidget();
+		final DateEditFieldWithPicker wDate = new DateEditFieldWithPicker();
+		flexTable.setWidget(0, 1, wDate);
+		flexTable.getFlexCellFormatter().setColSpan(0, 1, 2);
+		
+
 		flexTable.setWidget(1, 1, wProvider);
 		flexTable.getFlexCellFormatter().setColSpan(1, 1, 2);
-		wProvider.setModuleName("ProviderModule");
 
-		final SupportModuleWidget wDrug = new SupportModuleWidget();
 		flexTable.setWidget(2, 1, wDrug);
 		flexTable.getFlexCellFormatter().setColSpan(2, 1, 2);
-		wDrug.setModuleName("MultumDrugLexicon");
-		// TODO
 
-		final SupportModuleWidget wQuantity = new SupportModuleWidget();
 		flexTable.setWidget(4, 1, wQuantity);
 		flexTable.getFlexCellFormatter().setColSpan(4, 1, 2);
-		wQuantity.setModuleName("DrugQuantityQualifiers");
 
-		final SupportModuleWidget wInterval = new SupportModuleWidget();
+		final TextBox wInterval = new TextBox();
 		flexTable.setWidget(5, 1, wInterval);
 		flexTable.getFlexCellFormatter().setColSpan(5, 1, 2);
 
-		final SupportModuleWidget wSubstitutions = new SupportModuleWidget();
+		final TextBox wSubstitutions = new TextBox();
 		flexTable.setWidget(6, 1, wSubstitutions);
 		flexTable.getFlexCellFormatter().setColSpan(6, 1, 2);
 
-		final SupportModuleWidget wCoverageStatus = new SupportModuleWidget();
+		final TextBox wCoverageStatus = new TextBox();
 		flexTable.setWidget(7, 1, wCoverageStatus);
 		flexTable.getFlexCellFormatter().setColSpan(7, 1, 2);
 
-		final SupportModuleWidget wRefills = new SupportModuleWidget();
+		final TextBox wRefills = new TextBox();
 		flexTable.setWidget(8, 1, wRefills);
 		flexTable.getFlexCellFormatter().setColSpan(8, 1, 2);
 
@@ -122,13 +139,9 @@ public class PrescriptionsScreen extends PatientScreenInterface {
 		flexTable.setWidget(9, 1, tSignature);
 		flexTable.getFlexCellFormatter().setColSpan(9, 1, 2);
 		tSignature.setWidth("100%");
-
-		final TextBox tNote = new TextBox();
-		flexTable.setWidget(10, 1, tNote);
 		flexTable.getFlexCellFormatter().setColSpan(10, 1, 2);
-		tNote.setWidth("100%");
 
-		final SupportModuleWidget wDosage = new SupportModuleWidget();
+		final TextBox wDosage = new TextBox();
 		flexTable.setWidget(3, 1, wDosage);
 		flexTable.getFlexCellFormatter().setColSpan(3, 1, 2);
 
@@ -147,10 +160,92 @@ public class PrescriptionsScreen extends PatientScreenInterface {
 				resetForm();
 			}
 		});
+
+		final TextArea textArea = new TextArea();
+		flexTable.setWidget(10, 1, textArea);
+		textArea.setWidth("100%");
+
+
 	}
 	
 	public void savePrescription() {
-		//TODO
+		HashMap<String,String> data = new HashMap<String,String>();
+		data.put("rxphy",Integer.toString(wProvider.getValue()));
+		data.put("rxpatient", Integer.toString(patientId));
+		data.put("rxdrug", wDrug.getText());
+		data.put("rxdrugmultum", Integer.toString(wDrug.getValue()));
+		//rxform
+		//rxdosage
+		//rxquantity
+		data.put("rxquantityqual", Integer.toString(wQuantity.getValue()));
+		//rxsize
+		//rxunit
+		//rxinterval
+		//rxsubstitute
+		//rxrefills
+		//rxrefillinterval
+		//rxperrefill
+		//rxorigrx
+		//rxdx
+		//rxcovstatus
+		//rxsig
+		//rxnote
+		
+		if (Util.getProgramMode() == ProgramMode.STUBBED) {
+			// TODO: STUBBED
+		} else if (Util.getProgramMode() == ProgramMode.JSONRPC) {
+			// JSON-RPC
+			String[] params = {JsonUtil.jsonify(data)};
+			RequestBuilder builder = new RequestBuilder(
+					RequestBuilder.POST,
+					URL
+							.encode(Util
+									.getJsonRequest(
+											"org.freemedsoftware.module.Prescription.add",
+											params)));
+			try {
+				builder.sendRequest(null, new RequestCallback() {
+					public void onError(Request request, Throwable ex) {
+						state.getToaster().addItem("PrescriptionScreen",
+								"Failed to add Prescription",
+								Toaster.TOASTER_ERROR);
+					}
+
+					@SuppressWarnings("unchecked")
+					public void onResponseReceived(Request request,
+							Response response) {
+						if (200 == response.getStatusCode()) {
+							if (response.getText().compareToIgnoreCase(
+									"false") != 0) {
+								HashMap<String, String>[] r = (HashMap<String, String>[]) JsonUtil
+										.shoehornJson(JSONParser
+												.parse(response.getText()),
+												"HashMap<String,String>");
+								if (r != null) {
+									//Successful
+									state.getToaster().addItem("PrescriptionScreen", "Successfully added prescription", Toaster.TOASTER_INFO);
+								}
+							} else {
+								JsonUtil
+										.debug("Received dummy response from JSON backend");
+							}
+						} else {
+							state.getToaster().addItem("PrescriptionScreen",
+									"Failed to add Prescription",
+									Toaster.TOASTER_ERROR);
+						}
+					}
+				});
+			} catch (RequestException e) {
+				state.getToaster().addItem("PrescriptionScreen",
+						"Failed to add Prescription",
+						Toaster.TOASTER_ERROR);
+			}
+		} else {
+			// GWT-RPC
+		}
+		
+		
 	}
 	
 	public void resetForm() {
