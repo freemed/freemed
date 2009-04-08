ea3d050cb821c2eff21ac8f73420654505ee73e1
pmeng <pmeng@6507a214-7cff-0310-bd63-e3842a560bc8>
2009-04-08 13:46:14 +0000

    Added a ListBox that gets values from SupportModule.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@4555 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ ui/gwt/src/org/freemedsoftware/gwt/client/widget/SupportModuleListBox.java
@@ -0,0 +1,159 @@
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Philipp Meng <pmeng@freemedsoftware.org>
+ *
+ * FreeMED Electronic Medical Record and Practice Management System
+ * Copyright (C) 1999-2009 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.freemedsoftware.gwt.client.widget;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.freemedsoftware.gwt.client.JsonUtil;
+import org.freemedsoftware.gwt.client.Util;
+import org.freemedsoftware.gwt.client.WidgetInterface;
+import org.freemedsoftware.gwt.client.Util.ProgramMode;
+
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class SupportModuleListBox extends WidgetInterface {
+
+	protected String moduleName = null;
+
+	protected VerticalPanel layout;
+
+	protected ListBox listBox;
+
+	protected String selectText = "Select an Item";
+
+	public SupportModuleListBox(String s) {
+		moduleName = s;
+		buildForm();
+	}
+
+	public SupportModuleListBox(String s, String text) {
+		selectText = text;
+		moduleName = s;
+		buildForm();
+	}
+
+	public void buildForm() {
+		layout = new VerticalPanel();
+		listBox = new ListBox();
+		listBox.addItem(selectText);
+		layout.add(listBox);
+		initWidget(layout);
+		loadValues();
+	}
+
+	public void loadValues() {
+		if (Util.getProgramMode() == ProgramMode.STUBBED) {
+			listBox.addItem("Item 1", "i1");
+		} else if (Util.getProgramMode() == ProgramMode.JSONRPC) {
+			listBox.setEnabled(false);
+			String[] params = { moduleName, "" };
+			RequestBuilder builder = new RequestBuilder(
+					RequestBuilder.POST,
+					URL
+							.encode(Util
+									.getJsonRequest(
+											"org.freemedsoftware.api.ModuleInterface.ModuleSupportPicklistMethod",
+											params)));
+			try {
+				builder.sendRequest(null, new RequestCallback() {
+					public void onError(Request request, Throwable ex) {
+						JsonUtil
+								.debug("SupportModuleListBox: Error retrieving Data");
+						listBox.setEnabled(false);
+					}
+
+					@SuppressWarnings("unchecked")
+					public void onResponseReceived(Request request,
+							Response response) {
+						if (Util.checkValidSessionResponse(response.getText(),
+								state)) {
+							if (200 == response.getStatusCode()) {
+								HashMap<String, String> result = (HashMap<String, String>) JsonUtil
+										.shoehornJson(JSONParser.parse(response
+												.getText()),
+												"HashMap<String,String>");
+
+								if (result != null) {
+									Set<String> keys = result.keySet();
+									Iterator<String> iter = keys.iterator();
+									while (iter.hasNext()) {
+										final String key = (String) iter.next();
+										final String val = (String) result
+												.get(key);
+										listBox.addItem(val, key);
+									}
+
+								}
+								listBox.setEnabled(true);
+							} else {
+								Window.alert(response.toString());
+							}
+						}
+					}
+				});
+			} catch (RequestException e) {
+				listBox.setEnabled(true);
+				JsonUtil
+						.debug("Exception in SupportModuleListBox.loadValues; Message:"
+								+ e.getMessage());
+			}
+		}
+	}
+
+	public String getSelectedText() {
+		Integer index = listBox.getSelectedIndex();
+		if (index > 1) {
+			String s = listBox.getItemText(index);
+			return s;
+		}
+		return null;
+	}
+
+	/**
+	 * This method gives the Value of the Selected Item in the Listbox.
+	 * 
+	 * @return The String Value, null if there was an error.
+	 */
+	public String getSelectedValue() {
+		Integer index = listBox.getSelectedIndex();
+		if (index > 1) {
+			String s = listBox.getValue(index);
+			return s;
+		}
+		return null;
+	}
+
+}
