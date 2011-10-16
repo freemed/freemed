e4011afe9e275320ae81bbb9ea025e8a357bdaa3
jbuchbinder <jeff@ourexchange.net>
2011-10-16 22:29:49 +0000

    Dump GWT gettext messages appropriately.
    git-svn-id: https://svn.master.freemedsoftware.org/freemed/trunk@6147 6507a214-7cff-0310-bd63-e3842a560bc8

---
+++ ui/gwt/dump-messages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# $Id$
+#
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
+#
+# FreeMED Electronic Medical Record and Practice Management System
+# Copyright (C) 1999-2011 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#
+#	Dump gettext messages
+#
+
+D="$( cd "$(dirname "$0")"; pwd )"
+
+( cd "$D" ; xgettext -c -k'_' -o ../../locale/freemed-gwt.pot $(find src | grep '\.java$' | grep -v svn) )
+
