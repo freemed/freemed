f95ab72f771c81dcf5fc7e5494ec1afeb7c13273
Jeff Buchbinder <jeff@ourexchange.net>
2015-02-27 14:50:02 -0500

    Remove superfluous FOREIGN KEY which stops system-wide notifications from working.

--- data/schema/mysql/systemnotification.sql
+++ data/schema/mysql/systemnotification.sql
@@ -4,7 +4,7 @@
 #      Jeff Buchbinder <jeff@freemedsoftware.org>
 #
 # FreeMED Electronic Medical Record and Practice Management System
-# Copyright (C) 1999-2012 FreeMED Software Foundation
+# Copyright (C) 1999-2015 FreeMED Software Foundation
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -33,8 +33,6 @@ CREATE TABLE IF NOT EXISTS `systemnotification` (
 
 	, PRIMARY KEY		( id )
 	, KEY			( stamp, nuser )
-
-	, FOREIGN KEY		( nuser ) REFERENCES user ( id ) ON DELETE CASCADE
 );
 
 DROP PROCEDURE IF EXISTS systemnotification_Upgrade;
